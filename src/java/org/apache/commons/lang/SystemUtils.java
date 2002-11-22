858927268d783737f42503fa2ec868618e0c70ff
Henri Yandell <bayard@apache.org>
2002-11-22 23:32:45 +0000

    Surprisingly the isJavaVersionAtLeast was not static. I can't see any reason for this and have fixed it.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137156 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/SystemUtils.java
+++ src/java/org/apache/commons/lang/SystemUtils.java
@@ -61,7 +61,7 @@ package org.apache.commons.lang;
  * @author Based on code from Lucene
  * @author <a href="mailto:scolebourne@apache.org">Stephen Colebourne</a>
  * @author <a href="mailto:sdowney@panix.com">Steve Downey</a>
- * @version $Id: SystemUtils.java,v 1.4 2002/11/16 10:41:03 scolebourne Exp $
+ * @version $Id: SystemUtils.java,v 1.5 2002/11/22 23:32:45 bayard Exp $
  */
 public class SystemUtils {
     
@@ -442,7 +442,7 @@ public class SystemUtils {
      * @return <code>true</code> if the actual version is equal or greater
      *  than the required version
      */
-    public boolean isJavaVersionAtLeast(float requiredVersion) {
+    public static boolean isJavaVersionAtLeast(float requiredVersion) {
         return (getJavaVersion() >= requiredVersion);
     }
     
