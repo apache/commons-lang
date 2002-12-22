2561da0544ad16a26038a4a8603c9956faf7bc3a
Stephen Colebourne <scolebourne@apache.org>
2002-12-22 19:54:58 +0000

    Add since tag
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137199 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/SerializationException.java
+++ src/java/org/apache/commons/lang/SerializationException.java
@@ -1,5 +1,3 @@
-package org.apache.commons.lang;
-
 /* ====================================================================
  * The Apache Software License, Version 1.1
  *
@@ -53,6 +51,7 @@ package org.apache.commons.lang;
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
+package org.apache.commons.lang;
 
 import org.apache.commons.lang.exception.NestableRuntimeException;
 
@@ -61,8 +60,9 @@ import org.apache.commons.lang.exception.NestableRuntimeException;
  *
  * <p>The original error is wrapped within this one.</p>
  *
- * @author <a href="mailto:scolebourne@joda.org">Stephen Colebourne</a>
- * @version $Id: SerializationException.java,v 1.3 2002/11/16 10:41:03 scolebourne Exp $
+ * @author Stephen Colebourne
+ * @since 1.0
+ * @version $Id: SerializationException.java,v 1.4 2002/12/22 19:54:58 scolebourne Exp $
  */
 public class SerializationException extends NestableRuntimeException {
 
