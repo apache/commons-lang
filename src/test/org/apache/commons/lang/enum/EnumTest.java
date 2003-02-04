c0d11a776236558a4f73845c723952cd2c949334
Stephen Colebourne <scolebourne@apache.org>
2003-02-04 18:30:08 +0000

    Update Enum classes to support hierarchy Enum definitions
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137247 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/enum/EnumTest.java
+++ src/test/org/apache/commons/lang/enum/EnumTest.java
@@ -66,7 +66,7 @@ import org.apache.commons.lang.SerializationUtils;
  * Test cases for the {@link Enum} class.
  *
  * @author Stephen Colebourne
- * @version $Id: EnumTest.java,v 1.5 2002/12/31 22:39:39 scolebourne Exp $
+ * @version $Id: EnumTest.java,v 1.6 2003/02/04 18:30:08 scolebourne Exp $
  */
 
 public final class EnumTest extends TestCase {
@@ -222,6 +222,131 @@ public final class EnumTest extends TestCase {
     public void testOperationCalculation() {
         assertEquals(3, OperationEnum.PLUS.eval(1, 2));
         assertEquals(-1, OperationEnum.MINUS.eval(1, 2));
+    }
+    
+    //-----------------------------------------------------------------------
+    public void testExtended1Get() {
+        assertSame(Extended1Enum.ALPHA, Extended1Enum.getEnum("Alpha"));
+        assertSame(Extended1Enum.BETA, Extended1Enum.getEnum("Beta"));
+        assertSame(null, Extended1Enum.getEnum("Gamma"));
+        assertSame(null, Extended1Enum.getEnum("Delta"));
+    }
+            
+    public void testExtended2Get() {
+        assertSame(Extended1Enum.ALPHA, Extended2Enum.ALPHA);
+        assertSame(Extended1Enum.BETA, Extended2Enum.BETA);
+        
+        assertSame(Extended2Enum.ALPHA, Extended2Enum.getEnum("Alpha"));
+        assertSame(Extended2Enum.BETA, Extended2Enum.getEnum("Beta"));
+        assertSame(Extended2Enum.GAMMA, Extended2Enum.getEnum("Gamma"));
+        assertSame(null, Extended2Enum.getEnum("Delta"));
+    }
+
+    public void testExtended3Get() {
+        assertSame(Extended2Enum.ALPHA, Extended3Enum.ALPHA);
+        assertSame(Extended2Enum.BETA, Extended3Enum.BETA);
+        assertSame(Extended2Enum.GAMMA, Extended3Enum.GAMMA);
+        
+        assertSame(Extended3Enum.ALPHA, Extended3Enum.getEnum("Alpha"));
+        assertSame(Extended3Enum.BETA, Extended3Enum.getEnum("Beta"));
+        assertSame(Extended3Enum.GAMMA, Extended3Enum.getEnum("Gamma"));
+        assertSame(Extended3Enum.DELTA, Extended3Enum.getEnum("Delta"));
+    }
+
+    public void testExtendedSerialization() {
+        assertSame(Extended1Enum.ALPHA, SerializationUtils.clone(Extended1Enum.ALPHA));
+        assertSame(Extended1Enum.BETA, SerializationUtils.clone(Extended1Enum.BETA));
+        assertSame(Extended2Enum.GAMMA, SerializationUtils.clone(Extended2Enum.GAMMA));
+        assertSame(Extended3Enum.DELTA, SerializationUtils.clone(Extended3Enum.DELTA));
+    }
+
+    public void testExtendedToString() {
+        assertEquals("Extended1Enum[Alpha]", Extended1Enum.ALPHA.toString());
+        assertEquals("Extended1Enum[Beta]", Extended1Enum.BETA.toString());
+        
+        assertEquals("Extended1Enum[Alpha]", Extended2Enum.ALPHA.toString());
+        assertEquals("Extended1Enum[Beta]", Extended2Enum.BETA.toString());
+        assertEquals("Extended2Enum[Gamma]", Extended2Enum.GAMMA.toString());
+        
+        assertEquals("Extended1Enum[Alpha]", Extended3Enum.ALPHA.toString());
+        assertEquals("Extended1Enum[Beta]", Extended3Enum.BETA.toString());
+        assertEquals("Extended2Enum[Gamma]", Extended3Enum.GAMMA.toString());
+        assertEquals("Extended3Enum[Delta]", Extended3Enum.DELTA.toString());
+    }
+
+    public void testExtended1List() {
+        List list = Extended1Enum.getEnumList();
+        assertNotNull(list);
+        assertEquals(2, list.size());
+        assertEquals(list.size(), Extended1Enum.getEnumMap().keySet().size());
+        
+        Iterator it = list.iterator();
+        assertSame(Extended1Enum.ALPHA, it.next());
+        assertSame(Extended1Enum.BETA, it.next());
+    }
+
+    public void testExtended2List() {
+        List list = Extended2Enum.getEnumList();
+        assertNotNull(list);
+        assertEquals(3, list.size());
+        assertEquals(list.size(), Extended2Enum.getEnumMap().keySet().size());
+        
+        Iterator it = list.iterator();
+        assertSame(Extended2Enum.ALPHA, it.next());
+        assertSame(Extended2Enum.BETA, it.next());
+        assertSame(Extended2Enum.GAMMA, it.next());
+    }
+
+    public void testExtended3List() {
+        List list = Extended3Enum.getEnumList();
+        assertNotNull(list);
+        assertEquals(4, list.size());
+        assertEquals(list.size(), Extended3Enum.getEnumMap().keySet().size());
+        
+        Iterator it = list.iterator();
+        assertSame(Extended3Enum.ALPHA, it.next());
+        assertSame(Extended3Enum.BETA, it.next());
+        assertSame(Extended3Enum.GAMMA, it.next());
+        assertSame(Extended3Enum.DELTA, it.next());
+    }
+
+    public void testExtended1Map() {
+        Map map = Extended1Enum.getEnumMap();
+        assertNotNull(map);
+        assertEquals(map.keySet().size(), Extended1Enum.getEnumList().size());
+        
+        assertTrue(map.containsValue(Extended1Enum.ALPHA));
+        assertTrue(map.containsValue(Extended1Enum.BETA));
+        assertSame(Extended1Enum.ALPHA, map.get("Alpha"));
+        assertSame(Extended1Enum.BETA, map.get("Beta"));
+    }
+
+    public void testExtended2Map() {
+        Map map = Extended2Enum.getEnumMap();
+        assertNotNull(map);
+        assertEquals(map.keySet().size(), Extended2Enum.getEnumList().size());
+        
+        assertTrue(map.containsValue(Extended2Enum.ALPHA));
+        assertTrue(map.containsValue(Extended2Enum.BETA));
+        assertTrue(map.containsValue(Extended2Enum.GAMMA));
+        assertSame(Extended2Enum.ALPHA, map.get("Alpha"));
+        assertSame(Extended2Enum.BETA, map.get("Beta"));
+        assertSame(Extended2Enum.GAMMA, map.get("Gamma"));
+    }
+
+    public void testExtended3Map() {
+        Map map = Extended3Enum.getEnumMap();
+        assertNotNull(map);
+        assertEquals(map.keySet().size(), Extended3Enum.getEnumList().size());
+        
+        assertTrue(map.containsValue(Extended3Enum.ALPHA));
+        assertTrue(map.containsValue(Extended3Enum.BETA));
+        assertTrue(map.containsValue(Extended3Enum.GAMMA));
+        assertTrue(map.containsValue(Extended3Enum.DELTA));
+        assertSame(Extended3Enum.ALPHA, map.get("Alpha"));
+        assertSame(Extended3Enum.BETA, map.get("Beta"));
+        assertSame(Extended3Enum.GAMMA, map.get("Gamma"));
+        assertSame(Extended3Enum.DELTA, map.get("Delta"));
     }
 
 }
