69cc0e40e6a67e9a9d843f97350a5031bb6e8ab1
Alex Chaffee <alex@apache.org>
2003-04-09 20:55:33 +0000

    Extracted Entities class (instead of inner class) Added support for full HTML 4.0 entity set Separated XML, ISO8859-1 (HTML 3.2), and HTML 4.0 entities inside Entities object Added escapeXml and unescapeXml public API methods Escape now uses entities for values 0x80 or greater (previously 0x100)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137292 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/StringEscapeUtils.java
+++ src/java/org/apache/commons/lang/StringEscapeUtils.java
@@ -75,9 +75,14 @@ import org.apache.commons.lang.exception.NestableRuntimeException;
  * @author Helge Tesgaard
  * @author <a href="sean@boohai.com">Sean Brown</a>
  * @since 2.0
- * @version $Id: StringEscapeUtils.java,v 1.5 2003/04/09 18:45:28 alex Exp $
+ * @version $Id: StringEscapeUtils.java,v 1.6 2003/04/09 20:55:32 alex Exp $
  */
 public class StringEscapeUtils {
+
+    /**
+     * The entity set to use when escaping and unescaping HTML
+     */
+    protected static Entities DEFAULT_ENTITIES = Entities.HTML40;
 
     /**
      * <p><code>StringEscapeUtils</code> instances should NOT be constructed in
@@ -351,167 +356,72 @@ public class StringEscapeUtils {
     // HTML and XML
     //--------------------------------------------------------------------------
 
-    private static class Entities {
-        // see http://hotwired.lycos.com/webmonkey/reference/special_characters/
-        // see http://www.w3.org/TR/REC-html40/sgml/entities.html
-        static Object[][] entities = {
-            // {"#39", new Integer(39)},       // ' - apostrophe
-            {"quot", new Integer(34)}, // " - double-quote
-            {"amp", new Integer(38)}, // & - ampersand
-            {"lt", new Integer(60)}, // < - less-than
-            {"gt", new Integer(62)}, // > - greater-than
-            {"nbsp", new Integer(160)}, // non-breaking space
-            {"iexcl", new Integer(161)}, //inverted exclamation mark
-            {"cent", new Integer(162)}, //cent sign
-            {"pound", new Integer(163)}, //pound sign
-            {"curren", new Integer(164)}, //currency sign
-            {"yen", new Integer(165)}, //yen sign = yuan sign
-            {"brvbar", new Integer(166)}, //broken bar = broken vertical bar
-            {"sect", new Integer(167)}, //section sign
-            {"uml", new Integer(168)}, //diaeresis = spacing diaeresis
-            {"copy", new Integer(169)}, // © - copyright sign
-            {"ordf", new Integer(170)}, //feminine ordinal indicator
-            {"laquo", new Integer(171)}, //left-pointing double angle quotation mark = left pointing guillemet
-            {"not", new Integer(172)}, //not sign
-            {"shy", new Integer(173)}, //soft hyphen = discretionary hyphen
-            {"reg", new Integer(174)}, // ® - registered trademark sign
-            {"macr", new Integer(175)}, //macron = spacing macron = overline = APL overbar
-            {"deg", new Integer(176)}, //degree sign
-            {"plusmn", new Integer(177)}, //plus-minus sign = plus-or-minus sign
-            {"sup2", new Integer(178)}, //superscript two = superscript digit two = squared
-            {"sup3", new Integer(179)}, //superscript three = superscript digit three = cubed
-            {"acute", new Integer(180)}, //acute accent = spacing acute
-            {"micro", new Integer(181)}, //micro sign
-            {"para", new Integer(182)}, //pilcrow sign = paragraph sign
-            {"middot", new Integer(183)}, //middle dot = Georgian comma = Greek middle dot
-            {"cedil", new Integer(184)}, //cedilla = spacing cedilla
-            {"sup1", new Integer(185)}, //superscript one = superscript digit one
-            {"ordm", new Integer(186)}, //masculine ordinal indicator
-            {"raquo", new Integer(187)}, //right-pointing double angle quotation mark = right pointing guillemet
-            {"frac14", new Integer(188)}, //vulgar fraction one quarter = fraction one quarter
-            {"frac12", new Integer(189)}, //vulgar fraction one half = fraction one half
-            {"frac34", new Integer(190)}, //vulgar fraction three quarters = fraction three quarters
-            {"iquest", new Integer(191)}, //inverted question mark = turned question mark
-            {"Agrave", new Integer(192)}, // À - uppercase A, grave accent
-            {"Aacute", new Integer(193)}, // Á - uppercase A, acute accent
-            {"Acirc", new Integer(194)}, // Â - uppercase A, circumflex accent
-            {"Atilde", new Integer(195)}, // Ã - uppercase A, tilde
-            {"Auml", new Integer(196)}, // Ä - uppercase A, umlaut
-            {"Aring", new Integer(197)}, // Å - uppercase A, ring
-            {"AElig", new Integer(198)}, // Æ - uppercase AE
-            {"Ccedil", new Integer(199)}, // Ç - uppercase C, cedilla
-            {"Egrave", new Integer(200)}, // È - uppercase E, grave accent
-            {"Eacute", new Integer(201)}, // É - uppercase E, acute accent
-            {"Ecirc", new Integer(202)}, // Ê - uppercase E, circumflex accent
-            {"Euml", new Integer(203)}, // Ë - uppercase E, umlaut
-            {"Igrave", new Integer(204)}, // Ì - uppercase I, grave accent
-            {"Iacute", new Integer(205)}, // Í - uppercase I, acute accent
-            {"Icirc", new Integer(206)}, // Î - uppercase I, circumflex accent
-            {"Iuml", new Integer(207)}, // Ï - uppercase I, umlaut
-            {"ETH", new Integer(208)}, // Ð - uppercase Eth, Icelandic
-            {"Ntilde", new Integer(209)}, // Ñ - uppercase N, tilde
-            {"Ograve", new Integer(210)}, // Ò - uppercase O, grave accent
-            {"Oacute", new Integer(211)}, // Ó - uppercase O, acute accent
-            {"Ocirc", new Integer(212)}, // Ô - uppercase O, circumflex accent
-            {"Otilde", new Integer(213)}, // Õ - uppercase O, tilde
-            {"Ouml", new Integer(214)}, // Ö - uppercase O, umlaut
-            {"times", new Integer(215)}, //multiplication sign
-            {"Oslash", new Integer(216)}, // Ø - uppercase O, slash
-            {"Ugrave", new Integer(217)}, // Ù - uppercase U, grave accent
-            {"Uacute", new Integer(218)}, // Ú - uppercase U, acute accent
-            {"Ucirc", new Integer(219)}, // Û - uppercase U, circumflex accent
-            {"Uuml", new Integer(220)}, // Ü - uppercase U, umlaut
-            {"Yacute", new Integer(221)}, // Ý - uppercase Y, acute accent
-            {"THORN", new Integer(222)}, // Þ - uppercase THORN, Icelandic
-            {"szlig", new Integer(223)}, // ß - lowercase sharps, German
-            {"agrave", new Integer(224)}, // à - lowercase a, grave accent
-            {"aacute", new Integer(225)}, // á - lowercase a, acute accent
-            {"acirc", new Integer(226)}, // â - lowercase a, circumflex accent
-            {"atilde", new Integer(227)}, // ã - lowercase a, tilde
-            {"auml", new Integer(228)}, // ä - lowercase a, umlaut
-            {"aring", new Integer(229)}, // å - lowercase a, ring
-            {"aelig", new Integer(230)}, // æ - lowercase ae
-            {"ccedil", new Integer(231)}, // ç - lowercase c, cedilla
-            {"egrave", new Integer(232)}, // è - lowercase e, grave accent
-            {"eacute", new Integer(233)}, // é - lowercase e, acute accent
-            {"ecirc", new Integer(234)}, // ê - lowercase e, circumflex accent
-            {"euml", new Integer(235)}, // ë - lowercase e, umlaut
-            {"igrave", new Integer(236)}, // ì - lowercase i, grave accent
-            {"iacute", new Integer(237)}, // í - lowercase i, acute accent
-            {"icirc", new Integer(238)}, // î - lowercase i, circumflex accent
-            {"iuml", new Integer(239)}, // ï - lowercase i, umlaut
-            {"eth", new Integer(240)}, // ð - lowercase eth, Icelandic
-            {"ntilde", new Integer(241)}, // ñ - lowercase n, tilde
-            {"ograve", new Integer(242)}, // ò - lowercase o, grave accent
-            {"oacute", new Integer(243)}, // ó - lowercase o, acute accent
-            {"ocirc", new Integer(244)}, // ô - lowercase o, circumflex accent
-            {"otilde", new Integer(245)}, // õ - lowercase o, tilde
-            {"ouml", new Integer(246)}, // ö - lowercase o, umlaut
-            {"divide", new Integer(247)}, // division sign
-            {"oslash", new Integer(248)}, // ø - lowercase o, slash
-            {"ugrave", new Integer(249)}, // ù - lowercase u, grave accent
-            {"uacute", new Integer(250)}, // ú - lowercase u, acute accent
-            {"ucirc", new Integer(251)}, // û - lowercase u, circumflex accent
-            {"uuml", new Integer(252)}, // ü - lowercase u, umlaut
-            {"yacute", new Integer(253)}, // ý - lowercase y, acute accent
-            {"thorn", new Integer(254)}, // þ - lowercase thorn, Icelandic
-            {"yuml", new Integer(255)}, // ÿ - lowercase y, umlaut
-            {"euro", new Integer(8364)}, // Euro symbol
-        };
-
-        private Map mapNameToValue;
-        private Map mapValueToName;
-
-        public Entities() {
-            mapNameToValue = new HashMap();
-            mapValueToName = new HashMap();
-            for (int i = 0; i < entities.length; ++i) {
-                mapNameToValue.put(entities[i][0], entities[i][1]);
-                mapValueToName.put(entities[i][1], entities[i][0]);
-            }
-        }
-
-        public String entityName(int value) {
-            return (String) mapValueToName.get(new Integer(value));
-        }
-
-        public Integer entityValue(String name) {
-            return (Integer) mapNameToValue.get(name);
-        }
-    }
-
-    private static Entities entities;
-
-    private static void initEntities() {
-        if (entities == null)
-            entities = new Entities();
-    }
-
-    private static String entityName(char ch) {
-        initEntities();
-        return entities.entityName(ch);
-    }
-
-    private static Integer entityValue(String entity) {
-        initEntities();
-        return entities.entityValue(entity);
-    }
-
     /**
      * <p>Turns funky characters into HTML entity equivalents.</p>
+     * <p>
      * For example: <tt>"bread" & "butter"</tt> => <tt>&amp;quot;bread&amp;quot; &amp;amp; &amp;quot;butter&amp;quot;</tt>.
-     * Supports all known HTML entities, including funky accents. See the source code for more detail.
-     * see http://hotwired.lycos.com/webmonkey/reference/special_characters/
+     * </p>
+     * <p>Supports all known HTML 4.0 entities, including funky accents.
+     * See the source code for more detail.
+     * </p>
+     * @see http://hotwired.lycos.com/webmonkey/reference/special_characters/
+     * @see Entities
      * @see #unescapeHtml(String)
      **/
     public static String escapeHtml(String str) {
+        return escapeEntities(str, Entities.HTML40);
+    }
+
+    /**
+     * Given a string containing entity escapes, returns a string
+     * containing the actual Unicode characters corresponding to the
+     * escapes.
+     *
+     * @see #escapeHtml(String)
+     **/
+    public static String unescapeHtml(String str) {
+        return unescapeEntities(str, Entities.HTML40);
+    }
+
+    /**
+     * <p>Turns funky characters into XML entity equivalents.</p>
+     * <p>
+     * For example: <tt>"bread" & "butter"</tt> => <tt>&amp;quot;bread&amp;quot; &amp;amp; &amp;quot;butter&amp;quot;</tt>.
+     * </p>
+     * <p>
+     * Supports only the four basic XML entities (gt, lt, quot, amp).
+     * Does not support DTDs or external entities.
+     * </p>
+     * @see #unescapeXml(java.lang.String)
+     **/
+    public static String escapeXml(String str) {
+        return escapeEntities(str, Entities.XML);
+    }
+
+    /**
+     * <p>Given a string containing XML entity escapes, returns a string
+     * containing the actual Unicode characters corresponding to the
+     * escapes.
+     * </p>
+     * <p>
+     * Supports only the four basic XML entities (gt, lt, quot, amp).
+     * Does not support DTDs or external entities.
+     * </p>
+     *
+     * @see #escapeXml(String)
+     **/
+    public static String unescapeXml(String str) {
+        return unescapeEntities(str, Entities.XML);
+    }
+
+    private static String escapeEntities(String str, Entities entities) {
         StringBuffer buf = new StringBuffer(str.length() * 2);
         int i;
         for (i = 0; i < str.length(); ++i) {
             char ch = str.charAt(i);
-            String entity = entityName(ch);
+            String entity = entities.entityName(ch);
             if (entity == null) {
-                if (((int) ch) > 128) {   // should this be 127 or 128?
+                if (((int) ch) > 0x7F) {
                     int intValue = ((int) ch);
                     buf.append("&#" + intValue + ";");
                 } else {
@@ -524,14 +434,7 @@ public class StringEscapeUtils {
         return buf.toString();
     }
 
-    /**
-     * Given a string containing entity escapes, returns a string
-     * containing the actual Unicode characters corresponding to the
-     * escapes.
-     *
-     * @see #escapeHtml(String)
-     **/
-    public static String unescapeHtml(String str) {
+    private static String unescapeEntities(String str, Entities entities) {
         StringBuffer buf = new StringBuffer(str.length());
         int i;
         for (i = 0; i < str.length(); ++i) {
@@ -547,7 +450,7 @@ public class StringEscapeUtils {
                 if (entity.charAt(0) == '#') {
                     iso = new Integer(entity.substring(1));
                 } else {
-                    iso = entityValue(entity);
+                    iso = entities.entityValue(entity);
                 }
                 if (iso == null) {
                     buf.append("&" + entity + ";");
