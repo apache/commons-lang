9345b31d1511906438eeb796b26ee65dbe964561
Stephen Colebourne <scolebourne@apache.org>
2003-07-26 14:22:21 +0000

    Fix bug in isNestedThrowable, shown on JDK<1.4
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137511 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/exception/ExceptionUtils.java
+++ src/java/org/apache/commons/lang/exception/ExceptionUtils.java
@@ -80,7 +80,7 @@ import org.apache.commons.lang.SystemUtils;
  * @author Stephen Colebourne
  * @author <a href="mailto:ggregory@seagullsw.com">Gary Gregory</a>
  * @since 1.0
- * @version $Id: ExceptionUtils.java,v 1.29 2003/07/26 13:05:21 scolebourne Exp $
+ * @version $Id: ExceptionUtils.java,v 1.30 2003/07/26 14:22:21 scolebourne Exp $
  */
 public class ExceptionUtils {
     
@@ -354,7 +354,7 @@ public class ExceptionUtils {
         for (int i = 0, isize = CAUSE_METHOD_NAMES.length; i < isize; i++) {
             try {
                 Method method = cls.getMethod(CAUSE_METHOD_NAMES[i], null);
-                if (method != null) {
+                if (method != null && Throwable.class.isAssignableFrom(method.getReturnType())) {
                     return true;
                 }
             } catch (NoSuchMethodException ignored) {
