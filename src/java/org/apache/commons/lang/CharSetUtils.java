0da950654112a5185e2601bf44be2b6ffb17c169
Stephen Colebourne <scolebourne@apache.org>
2003-07-31 21:32:47 +0000

    Fix CharSetUtils to not throw NPE all the time
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137538 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/CharSetUtils.java
+++ src/java/org/apache/commons/lang/CharSetUtils.java
@@ -56,14 +56,14 @@ package org.apache.commons.lang;
 /**
  * <p>Numerous routines to manipulate a <code>CharSet</code>.</p>
  *
- * <p>This class tries to handle <code>null</code> input gracefully.
- * An exception will generally not be thrown for a <code>null</code> input.
+ * <p>This class handles <code>null</code> input gracefully.
+ * An exception will not be thrown for a <code>null</code> input.
  * Each method documents its behaviour in more detail.</p>
  * 
  * @author <a href="bayard@generationjava.com">Henri Yandell</a>
  * @author Stephen Colebourne
  * @since 1.0
- * @version $Id: CharSetUtils.java,v 1.17 2003/07/31 20:38:26 scolebourne Exp $
+ * @version $Id: CharSetUtils.java,v 1.18 2003/07/31 21:32:47 scolebourne Exp $
  */
 public class CharSetUtils {
 
@@ -77,6 +77,7 @@ public class CharSetUtils {
     public CharSetUtils() {
     }
 
+    //-----------------------------------------------------------------------
     /**
      * <p>Creates a <code>CharSetUtils</code> object which allows a certain amount of
      * set logic to be performed.</p>
@@ -87,33 +88,72 @@ public class CharSetUtils {
      *   a set in itself due to the size of that set in unicode.</li>
      *  <li>&quot;ej-m&quot; implies e,j->m. e,j,k,l,m.</li>
      * </ul>
+     * 
+     * <pre>
+     * CharSetUtils.evaluateSet(null)  = null
+     * CharSetUtils.evaluateSet("")    = CharSet matching nothing
+     * CharSetUtils.evaluateSet("a-e") = CharSet matching a,b,c,d,e
+     * </pre>
      *
-     * @param set  the set, must not be null
-     * @return a CharSet instance
-     * @throws NullPointerException if any of set[i] is null or if set is null
+     * @param set  the set, may be null
+     * @return a CharSet instance, <code>null</code> if null input
+     */
+    public static CharSet evaluateSet(String set) {
+        if (set == null) {
+            return null;
+        }
+        return new CharSet(new String[] {set}); 
+    }
+
+    /**
+     * <p>Creates a <code>CharSetUtils</code> object which allows a certain amount of
+     * set logic to be performed.</p>
+     * <p>The syntax is:</p>
+     * <ul>
+     *  <li>&quot;aeio&quot; which implies 'a','e',..</li>
+     *  <li>&quot;^e&quot; implies not e. However it only negates, it's not
+     *   a set in itself due to the size of that set in unicode.</li>
+     *  <li>&quot;ej-m&quot; implies e,j->m. e,j,k,l,m.</li>
+     * </ul>
+     * 
+     * <pre>
+     * CharSetUtils.evaluateSet(null)    = null
+     * CharSetUtils.evaluateSet([])      = CharSet matching nothing
+     * CharSetUtils.evaluateSet(["a-e"]) = CharSet matching a,b,c,d,e
+     * </pre>
+     *
+     * @param set  the set, may be null
+     * @return a CharSet instance, <code>null</code> if null input
      */
     public static CharSet evaluateSet(String[] set) {
+        if (set == null) {
+            return null;
+        }
         return new CharSet(set); 
     }
 
+    //-----------------------------------------------------------------------
     /**
      * <p>Squeezes any repititions of a character that is mentioned in the
      * supplied set.</p>
      *
-     * <p>An example is:</p>
-     * <ul>
-     *  <li>squeeze(&quot;hello&quot;, &quot;el&quot;) => &quot;helo&quot;</li>
-     * </ul>
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * <pre>
+     * CharSetUtils.squeeze(null, *)        = null
+     * CharSetUtils.squeeze("", *)          = ""
+     * CharSetUtils.squeeze(*, null)        = *
+     * CharSetUtils.squeeze(*, "")          = *
+     * CharSetUtils.squeeze("hello", "k-p") = "helo"
+     * CharSetUtils.squeeze("hello", "a-e") = "hello"
+     * </pre>
      *
+     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * @param str  the string to squeeze, may be null
-     * @param set  the character set to use for manipulation, must not be null
+     * @param set  the character set to use for manipulation, may be null
      * @return modified String, <code>null</code> if null string input
-     * @throws NullPointerException if <code>set</code> is <code>null</code>
      */
     public static String squeeze(String str, String set) {
-        if (str == null) {
-            return null;
+        if (str == null || str.length() == 0 || set == null || set.length() == 0) {
+            return str;
         }
         String[] strs = new String[1];
         strs[0] = set;
@@ -128,17 +168,15 @@ public class CharSetUtils {
      * <ul>
      *   <li>squeeze(&quot;hello&quot;, {&quot;el&quot;}) => &quot;helo&quot;</li>
      * </ul>
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * 
+     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * @param str  the string to squeeze, may be null
-     * @param set  the character set to use for manipulation, must not be null
+     * @param set  the character set to use for manipulation, may be null
      * @return modified String, <code>null</code> if null string input
-     * @throws NullPointerException if <code>set</code> is <code>null</code>
-     *  or any element is <code>null</code>
      */
     public static String squeeze(String str, String[] set) {
-        if (str == null) {
-            return null;
+        if (str == null || str.length() == 0 || set == null || set.length == 0) {
+            return str;
         }
         CharSet chars = evaluateSet(set);
         StringBuffer buffer = new StringBuffer(str.length());
@@ -159,22 +197,27 @@ public class CharSetUtils {
         return buffer.toString();
     }
 
+    //-----------------------------------------------------------------------
     /**
      * <p>Takes an argument in set-syntax, see evaluateSet,
      * and returns the number of characters present in the specified string.</p>
      *
-     * <p>An example would be:</p>
-     * <ul>
-     *   <li>count(&quot;hello&quot;, {&quot;c-f&quot;, &quot;o&quot;}) returns 2.</li>
-     * </ul>
+     * <pre>
+     * CharSetUtils.count(null, *)        = 0
+     * CharSetUtils.count("", *)          = 0
+     * CharSetUtils.count(*, null)        = 0
+     * CharSetUtils.count(*, "")          = 0
+     * CharSetUtils.count("hello", "k-p") = 3
+     * CharSetUtils.count("hello", "a-e") = 1
+     * </pre>
      *
+     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * @param str  String to count characters in, may be null
-     * @param set  String set of characters to count, must not be null
+     * @param set  String set of characters to count, may be null
      * @return character count, zero if null string input
-     * @throws NullPointerException if <code>set</code> is <code>null</code>
      */
     public static int count(String str, String set) {
-        if (str == null) {
+        if (str == null || str.length() == 0 || set == null || set.length() == 0) {
             return 0;
         }
         String[] strs = new String[1];
@@ -191,14 +234,13 @@ public class CharSetUtils {
      *  <li>count(&quot;hello&quot;, {&quot;c-f&quot;, &quot;o&quot;}) returns 2.</li>
      * </ul>
      *
+     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * @param str  String to count characters in, may be null
-     * @param set  String[] set of characters to count, must not be null
+     * @param set  String[] set of characters to count, may be null
      * @return character count, zero if null string input
-     * @throws NullPointerException if <code>set</code> is <code>null</code>
-     *  or any element is <code>null</code>
      */
     public static int count(String str, String[] set) {
-        if (str == null) {
+        if (str == null || str.length() == 0 || set == null || set.length == 0) {
             return 0;
         }
         CharSet chars = evaluateSet(set);
@@ -213,23 +255,31 @@ public class CharSetUtils {
         return count;
     }
 
+    //-----------------------------------------------------------------------
     /**
      * <p>Takes an argument in set-syntax, see evaluateSet,
      * and keeps any of characters present in the specified string.</p>
      *
-     * <p>An example would be:</p>
-     * <ul>
-     *   <li>keep(&quot;hello&quot;, {&quot;c-fo&quot;}) returns &quot;hll&quot;</li>
-     * </ul>
+     * <pre>
+     * CharSetUtils.keep(null, *)        = null
+     * CharSetUtils.keep("", *)          = ""
+     * CharSetUtils.keep(*, null)        = ""
+     * CharSetUtils.keep(*, "")          = ""
+     * CharSetUtils.keep("hello", "hl") = "hll"
+     * CharSetUtils.keep("hello", "le") = "ell"
+     * </pre>
      *
+     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * @param str  String to keep characters from, may be null
-     * @param set  String set of characters to keep, must not be null
+     * @param set  String set of characters to keep, may be null
      * @return modified String, <code>null</code> if null string input
-     * @throws NullPointerException if <code>set</code> is <code>null</code>
      */
     public static String keep(String str, String set) {
         if (str == null) {
             return null;
+        }
+        if (str.length() == 0 || set == null || set.length() == 0) {
+            return "";
         }
         String[] strs = new String[1];
         strs[0] = set;
@@ -246,36 +296,43 @@ public class CharSetUtils {
      *   returns &quot;hll&quot;</li>
      * </ul>
      *
+     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * @param str  String to keep characters from, may be null
-     * @param set  String[] set of characters to keep, must not be null
+     * @param set  String[] set of characters to keep, may be null
      * @return modified String, <code>null</code> if null string input
-     * @throws NullPointerException if <code>set</code> is <code>null</code>
-     *  or any element is <code>null</code>
      */
     public static String keep(String str, String[] set) {
         if (str == null) {
             return null;
         }
+        if (str.length() == 0 || set == null || set.length == 0) {
+            return "";
+        }
         return modify(str, set, true);
     }
 
+    //-----------------------------------------------------------------------
     /**
      * <p>Takes an argument in set-syntax, see evaluateSet,
      * and deletes any of characters present in the specified string.</p>
      *
-     * <p>An example would be:</p>
-     * <ul>
-     *   <li>delete(&quot;hello&quot;, {&quot;c-fo&quot;}) returns &quot;hll&quot;</li>
-     * </ul>
+     * <pre>
+     * CharSetUtils.delete(null, *)        = null
+     * CharSetUtils.delete("", *)          = ""
+     * CharSetUtils.delete(*, null)        = *
+     * CharSetUtils.delete(*, "")          = *
+     * CharSetUtils.delete("hello", "hl") = "hll"
+     * CharSetUtils.delete("hello", "le") = "ell"
+     * </pre>
      *
+     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * @param str  String to delete characters from, may be null
-     * @param set  String set of characters to delete, must not be null
+     * @param set  String set of characters to delete, may be null
      * @return modified String, <code>null</code> if null string input
-     * @throws NullPointerException if <code>set</code> is <code>null</code>
      */
     public static String delete(String str, String set) {
-        if (str == null) {
-            return null;
+        if (str == null || str.length() == 0 || set == null || set.length() == 0) {
+            return str;
         }
         String[] strs = new String[1];
         strs[0] = set;
@@ -292,19 +349,19 @@ public class CharSetUtils {
      *   &quot;hll&quot;</li>
      * </ul>
      *
+     * @see #evaluateSet(java.lang.String[]) for set-syntax.
      * @param str  String to delete characters from, may be null
-     * @param set  String[] set of characters to delete, must not be null
+     * @param set  String[] set of characters to delete, may be null
      * @return modified String, <code>null</code> if null string input
-     * @throws NullPointerException if <code>set</code> is <code>null</code>
-     *  or any element is <code>null</code>
      */
     public static String delete(String str, String[] set) {
-        if (str == null) {
-            return null;
+        if (str == null || str.length() == 0 || set == null || set.length == 0) {
+            return str;
         }
         return modify(str, set, false);
     }
 
+    //-----------------------------------------------------------------------
     // Implementation of delete and keep
     private static String modify(String str, String[] set, boolean expect) {
         CharSet chars = evaluateSet(set);
@@ -319,6 +376,7 @@ public class CharSetUtils {
         return buffer.toString();
     }
 
+    //-----------------------------------------------------------------------
     /**
      * <p>Translate characters in a String.
      * This is a multi character search and replace routine.</p>
