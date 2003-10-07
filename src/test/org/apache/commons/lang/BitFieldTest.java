77f44ba2713f1e8faad5f6dd056832f446392814
Stephen Colebourne <scolebourne@apache.org>
2003-10-07 20:36:26 +0000

    Update licence Tidy file
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137679 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/BitFieldTest.java
+++ src/test/org/apache/commons/lang/BitFieldTest.java
@@ -1,13 +1,7 @@
-/*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//lang/src/test/org/apache/commons/lang/BitFieldTest.java,v 1.2 2003/10/05 15:46:55 rdonkin Exp $
- * $Revision: 1.2 $
- * $Date: 2003/10/05 15:46:55 $
- *
- * ====================================================================
- * 
+/* ====================================================================
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
+ * Copyright (c) 2002-2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -15,28 +9,28 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
  *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgement:  
- *       "This product includes software developed by the 
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowledgement:
+ *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
  *
- * 4. The names "Apache", "The Jakarta Project", "Commons", and "Apache Software
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
  *    Foundation" must not be used to endorse or promote products derived
- *    from this software without prior written permission. For written 
+ *    from this software without prior written permission. For written
  *    permission, please contact apache@apache.org.
  *
- * 5. Products derived from this software may not be called "Apache",
- *    "Apache" nor may "Apache" appear in their names without prior 
- *    written permission of the Apache Software Foundation.
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Software Foundation.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -56,9 +50,7 @@
  * individuals on behalf of the Apache Software Foundation.  For more
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
- *
- */ 
-
+ */
 package org.apache.commons.lang;
 
 import junit.framework.Test;
@@ -69,15 +61,12 @@ import junit.textui.TestRunner;
 /**
  * Class to test BitField functionality
  *
- * @author Scott Sanders (sanders at apache dot org)
+ * @author Scott Sanders
  * @author Marc Johnson
- * @author Glen Stampoultzis (gstamp@iprimus.com.au)
- * @version $Id: BitFieldTest.java,v 1.2 2003/10/05 15:46:55 rdonkin Exp $
+ * @author Glen Stampoultzis
+ * @version $Id: BitFieldTest.java,v 1.3 2003/10/07 20:36:26 scolebourne Exp $
  */
-
-public class BitFieldTest
-    extends TestCase
-{
+public class BitFieldTest extends TestCase {
 
     public static void main(String[] args) {
         TestRunner.run(suite());
@@ -97,18 +86,14 @@ public class BitFieldTest
      *
      * @param name
      */
-
-    public BitFieldTest(String name)
-    {
+    public BitFieldTest(String name) {
         super(name);
     }
 
     /**
      * test the getValue() method
      */
-
-    public void testGetValue()
-    {
+    public void testGetValue() {
         assertEquals(bf_multi.getValue(-1), 127);
         assertEquals(bf_multi.getValue(0), 0);
         assertEquals(bf_single.getValue(-1), 1);
@@ -118,21 +103,17 @@ public class BitFieldTest
     /**
      * test the getShortValue() method
      */
-
-    public void testGetShortValue()
-    {
-        assertEquals(bf_multi.getShortValue(( short ) -1), ( short ) 127);
-        assertEquals(bf_multi.getShortValue(( short ) 0), ( short ) 0);
-        assertEquals(bf_single.getShortValue(( short ) -1), ( short ) 1);
-        assertEquals(bf_single.getShortValue(( short ) 0), ( short ) 0);
+    public void testGetShortValue() {
+        assertEquals(bf_multi.getShortValue((short) - 1), (short) 127);
+        assertEquals(bf_multi.getShortValue((short) 0), (short) 0);
+        assertEquals(bf_single.getShortValue((short) - 1), (short) 1);
+        assertEquals(bf_single.getShortValue((short) 0), (short) 0);
     }
 
     /**
      * test the getRawValue() method
      */
-
-    public void testGetRawValue()
-    {
+    public void testGetRawValue() {
         assertEquals(bf_multi.getRawValue(-1), 0x3F80);
         assertEquals(bf_multi.getRawValue(0), 0);
         assertEquals(bf_single.getRawValue(-1), 0x4000);
@@ -142,26 +123,19 @@ public class BitFieldTest
     /**
      * test the getShortRawValue() method
      */
-
-    public void testGetShortRawValue()
-    {
-        assertEquals(bf_multi.getShortRawValue(( short ) -1),
-                     ( short ) 0x3F80);
-        assertEquals(bf_multi.getShortRawValue(( short ) 0), ( short ) 0);
-        assertEquals(bf_single.getShortRawValue(( short ) -1),
-                     ( short ) 0x4000);
-        assertEquals(bf_single.getShortRawValue(( short ) 0), ( short ) 0);
+    public void testGetShortRawValue() {
+        assertEquals(bf_multi.getShortRawValue((short) - 1), (short) 0x3F80);
+        assertEquals(bf_multi.getShortRawValue((short) 0), (short) 0);
+        assertEquals(bf_single.getShortRawValue((short) - 1), (short) 0x4000);
+        assertEquals(bf_single.getShortRawValue((short) 0), (short) 0);
     }
 
     /**
      * test the isSet() method
      */
-
-    public void testIsSet()
-    {
+    public void testIsSet() {
         assertTrue(!bf_multi.isSet(0));
-        for (int j = 0x80; j <= 0x3F80; j += 0x80)
-        {
+        for (int j = 0x80; j <= 0x3F80; j += 0x80) {
             assertTrue(bf_multi.isSet(j));
         }
         assertTrue(!bf_single.isSet(0));
@@ -171,11 +145,8 @@ public class BitFieldTest
     /**
      * test the isAllSet() method
      */
-
-    public void testIsAllSet()
-    {
-        for (int j = 0; j < 0x3F80; j += 0x80)
-        {
+    public void testIsAllSet() {
+        for (int j = 0; j < 0x3F80; j += 0x80) {
             assertTrue(!bf_multi.isAllSet(j));
         }
         assertTrue(bf_multi.isAllSet(0x3F80));
@@ -186,19 +157,15 @@ public class BitFieldTest
     /**
      * test the setValue() method
      */
-
-    public void testSetValue()
-    {
-        for (int j = 0; j < 128; j++)
-        {
+    public void testSetValue() {
+        for (int j = 0; j < 128; j++) {
             assertEquals(bf_multi.getValue(bf_multi.setValue(0, j)), j);
             assertEquals(bf_multi.setValue(0, j), j << 7);
         }
 
         // verify that excess bits are stripped off
         assertEquals(bf_multi.setValue(0x3f80, 128), 0);
-        for (int j = 0; j < 2; j++)
-        {
+        for (int j = 0; j < 2; j++) {
             assertEquals(bf_single.getValue(bf_single.setValue(0, j)), j);
             assertEquals(bf_single.setValue(0, j), j << 14);
         }
@@ -210,58 +177,42 @@ public class BitFieldTest
     /**
      * test the setShortValue() method
      */
-
-    public void testSetShortValue()
-    {
-        for (int j = 0; j < 128; j++)
-        {
-            assertEquals(bf_multi
-                .getShortValue(bf_multi
-                    .setShortValue(( short ) 0, ( short ) j)), ( short ) j);
-            assertEquals(bf_multi.setShortValue(( short ) 0, ( short ) j),
-                         ( short ) (j << 7));
+    public void testSetShortValue() {
+        for (int j = 0; j < 128; j++) {
+            assertEquals(bf_multi.getShortValue(bf_multi.setShortValue((short) 0, (short) j)), (short) j);
+            assertEquals(bf_multi.setShortValue((short) 0, (short) j), (short) (j << 7));
         }
 
         // verify that excess bits are stripped off
-        assertEquals(bf_multi.setShortValue(( short ) 0x3f80, ( short ) 128),
-                     ( short ) 0);
-        for (int j = 0; j < 2; j++)
-        {
-            assertEquals(bf_single
-                .getShortValue(bf_single
-                    .setShortValue(( short ) 0, ( short ) j)), ( short ) j);
-            assertEquals(bf_single.setShortValue(( short ) 0, ( short ) j),
-                         ( short ) (j << 14));
+        assertEquals(bf_multi.setShortValue((short) 0x3f80, (short) 128), (short) 0);
+        for (int j = 0; j < 2; j++) {
+            assertEquals(bf_single.getShortValue(bf_single.setShortValue((short) 0, (short) j)), (short) j);
+            assertEquals(bf_single.setShortValue((short) 0, (short) j), (short) (j << 14));
         }
 
         // verify that excess bits are stripped off
-        assertEquals(bf_single.setShortValue(( short ) 0x4000, ( short ) 2),
-                     ( short ) 0);
+        assertEquals(bf_single.setShortValue((short) 0x4000, (short) 2), (short) 0);
     }
 
-    public void testByte()
-    {
-        assertEquals(1, new BitField(1).setByteBoolean(( byte ) 0, true));
-        assertEquals(2, new BitField(2).setByteBoolean(( byte ) 0, true));
-        assertEquals(4, new BitField(4).setByteBoolean(( byte ) 0, true));
-        assertEquals(8, new BitField(8).setByteBoolean(( byte ) 0, true));
-        assertEquals(16, new BitField(16).setByteBoolean(( byte ) 0, true));
-        assertEquals(32, new BitField(32).setByteBoolean(( byte ) 0, true));
-        assertEquals(64, new BitField(64).setByteBoolean(( byte ) 0, true));
-        assertEquals(-128,
-                     new BitField(128).setByteBoolean(( byte ) 0, true));
-        assertEquals(0, new BitField(1).setByteBoolean(( byte ) 1, false));
-        assertEquals(0, new BitField(2).setByteBoolean(( byte ) 2, false));
-        assertEquals(0, new BitField(4).setByteBoolean(( byte ) 4, false));
-        assertEquals(0, new BitField(8).setByteBoolean(( byte ) 8, false));
-        assertEquals(0, new BitField(16).setByteBoolean(( byte ) 16, false));
-        assertEquals(0, new BitField(32).setByteBoolean(( byte ) 32, false));
-        assertEquals(0, new BitField(64).setByteBoolean(( byte ) 64, false));
-        assertEquals(0, new BitField(128).setByteBoolean(( byte ) 128,
-                                     false));
-        assertEquals(-2, new BitField(1).setByteBoolean(( byte ) 255, false));
-        byte clearedBit = new BitField(0x40).setByteBoolean(( byte ) -63,
-                                       false);
+    public void testByte() {
+        assertEquals(1, new BitField(1).setByteBoolean((byte) 0, true));
+        assertEquals(2, new BitField(2).setByteBoolean((byte) 0, true));
+        assertEquals(4, new BitField(4).setByteBoolean((byte) 0, true));
+        assertEquals(8, new BitField(8).setByteBoolean((byte) 0, true));
+        assertEquals(16, new BitField(16).setByteBoolean((byte) 0, true));
+        assertEquals(32, new BitField(32).setByteBoolean((byte) 0, true));
+        assertEquals(64, new BitField(64).setByteBoolean((byte) 0, true));
+        assertEquals(-128, new BitField(128).setByteBoolean((byte) 0, true));
+        assertEquals(0, new BitField(1).setByteBoolean((byte) 1, false));
+        assertEquals(0, new BitField(2).setByteBoolean((byte) 2, false));
+        assertEquals(0, new BitField(4).setByteBoolean((byte) 4, false));
+        assertEquals(0, new BitField(8).setByteBoolean((byte) 8, false));
+        assertEquals(0, new BitField(16).setByteBoolean((byte) 16, false));
+        assertEquals(0, new BitField(32).setByteBoolean((byte) 32, false));
+        assertEquals(0, new BitField(64).setByteBoolean((byte) 64, false));
+        assertEquals(0, new BitField(128).setByteBoolean((byte) 128, false));
+        assertEquals(-2, new BitField(1).setByteBoolean((byte) 255, false));
+        byte clearedBit = new BitField(0x40).setByteBoolean((byte) - 63, false);
 
         assertEquals(false, new BitField(0x40).isSet(clearedBit));
     }
@@ -269,9 +220,7 @@ public class BitFieldTest
     /**
      * test the clear() method
      */
-
-    public void testClear()
-    {
+    public void testClear() {
         assertEquals(bf_multi.clear(-1), 0xFFFFC07F);
         assertEquals(bf_single.clear(-1), 0xFFFFBFFF);
     }
@@ -279,19 +228,15 @@ public class BitFieldTest
     /**
      * test the clearShort() method
      */
-
-    public void testClearShort()
-    {
-        assertEquals(bf_multi.clearShort(( short ) -1), ( short ) 0xC07F);
-        assertEquals(bf_single.clearShort(( short ) -1), ( short ) 0xBFFF);
+    public void testClearShort() {
+        assertEquals(bf_multi.clearShort((short) - 1), (short) 0xC07F);
+        assertEquals(bf_single.clearShort((short) - 1), (short) 0xBFFF);
     }
 
     /**
      * test the set() method
      */
-
-    public void testSet()
-    {
+    public void testSet() {
         assertEquals(bf_multi.set(0), 0x3F80);
         assertEquals(bf_single.set(0), 0x4000);
     }
@@ -299,19 +244,15 @@ public class BitFieldTest
     /**
      * test the setShort() method
      */
-
-    public void testSetShort()
-    {
-        assertEquals(bf_multi.setShort(( short ) 0), ( short ) 0x3F80);
-        assertEquals(bf_single.setShort(( short ) 0), ( short ) 0x4000);
+    public void testSetShort() {
+        assertEquals(bf_multi.setShort((short) 0), (short) 0x3F80);
+        assertEquals(bf_single.setShort((short) 0), (short) 0x4000);
     }
 
     /**
      * test the setBoolean() method
      */
-
-    public void testSetBoolean()
-    {
+    public void testSetBoolean() {
         assertEquals(bf_multi.set(0), bf_multi.setBoolean(0, true));
         assertEquals(bf_single.set(0), bf_single.setBoolean(0, true));
         assertEquals(bf_multi.clear(-1), bf_multi.setBoolean(-1, false));
@@ -321,17 +262,11 @@ public class BitFieldTest
     /**
      * test the setShortBoolean() method
      */
-
-    public void testSetShortBoolean()
-    {
-        assertEquals(bf_multi.setShort(( short ) 0),
-                     bf_multi.setShortBoolean(( short ) 0, true));
-        assertEquals(bf_single.setShort(( short ) 0),
-                     bf_single.setShortBoolean(( short ) 0, true));
-        assertEquals(bf_multi.clearShort(( short ) -1),
-                     bf_multi.setShortBoolean(( short ) -1, false));
-        assertEquals(bf_single.clearShort(( short ) -1),
-                     bf_single.setShortBoolean(( short ) -1, false));
+    public void testSetShortBoolean() {
+        assertEquals(bf_multi.setShort((short) 0), bf_multi.setShortBoolean((short) 0, true));
+        assertEquals(bf_single.setShort((short) 0), bf_single.setShortBoolean((short) 0, true));
+        assertEquals(bf_multi.clearShort((short) - 1), bf_multi.setShortBoolean((short) - 1, false));
+        assertEquals(bf_single.clearShort((short) - 1), bf_single.setShortBoolean((short) - 1, false));
     }
 
 }
