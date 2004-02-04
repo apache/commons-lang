c8f42adee295e7305dc8f9ac090e505c36f2d6b7
Fredrik Westermarck <fredrik@apache.org>
2004-02-04 18:52:59 +0000

    Fixed bug in getDateInstance(int, TimeZone, Locale). See bz #26646.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137772 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/time/FastDateFormat.java
+++ src/java/org/apache/commons/lang/time/FastDateFormat.java
@@ -1,7 +1,7 @@
 /* ====================================================================
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2002-2003 The Apache Software Foundation.  All rights
+ * Copyright (c) 2002-2004 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -99,7 +99,7 @@ import java.util.TimeZone;
  * @author Stephen Colebourne
  * @author Nikolay Metchev
  * @since 2.0
- * @version $Id: FastDateFormat.java,v 1.17 2003/11/29 13:52:34 scolebourne Exp $
+ * @version $Id: FastDateFormat.java,v 1.18 2004/02/04 18:52:59 fredrik Exp $
  */
 public class FastDateFormat extends Format {
     // A lot of the speed in this class comes from caching, but some comes
@@ -307,7 +307,7 @@ public class FastDateFormat extends Format {
         if (timeZone != null) {
             key = new Pair(key, timeZone);
         }
-        if (locale == null) {
+        if (locale != null) {
             key = new Pair(key, locale);
         }
 
