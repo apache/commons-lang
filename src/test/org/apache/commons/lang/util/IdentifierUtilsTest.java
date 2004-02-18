43e94f47ba2fe243f28b68193418878843fec392
Gary D. Gregory <ggregory@apache.org>
2004-02-18 23:06:54 +0000

    Apply Apache License Version 2.0 by following the instructions at http://www.apache.org/dev/apply-license.html.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137800 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/util/IdentifierUtilsTest.java
+++ src/test/org/apache/commons/lang/util/IdentifierUtilsTest.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2002-2004 The Apache Software Foundation
+ * Copyright 2002-2004 The Apache Software Foundation
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.commons.lang.util;
 
@@ -27,7 +27,7 @@ import junit.framework.TestSuite;
  *
  * @deprecated WILL BE REMOVED SOON
  * @author Stephen Colebourne
- * @version $Id: IdentifierUtilsTest.java,v 1.5 2004/02/03 22:34:44 scolebourne Exp $
+ * @version $Id: IdentifierUtilsTest.java,v 1.6 2004/02/18 23:03:51 ggregory Exp $
  */
 public class IdentifierUtilsTest extends junit.framework.TestCase {
 
