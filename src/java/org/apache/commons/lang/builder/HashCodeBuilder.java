178e241ff9d28a51720ca3e3235d651242b4a50c
Henri Yandell <bayard@apache.org>
2004-08-15 02:17:13 +0000

    added comment in javadoc for #30328
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137892 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/builder/HashCodeBuilder.java
+++ src/java/org/apache/commons/lang/builder/HashCodeBuilder.java
@@ -74,7 +74,7 @@ import java.lang.reflect.Modifier;
  * @author Gary Gregory
  * @author Pete Gieser
  * @since 1.0
- * @version $Id: HashCodeBuilder.java,v 1.21 2004/02/18 22:53:24 ggregory Exp $
+ * @version $Id: HashCodeBuilder.java,v 1.22 2004/08/15 02:17:13 bayard Exp $
  */
 public class HashCodeBuilder {
 
@@ -459,6 +459,9 @@ public class HashCodeBuilder {
 
     /**
      * <p>Append a <code>hashCode</code> for a <code>boolean</code>.</p>
+     * <p>This adds <code>iConstant * 1</code> to the <code>hashCode</code>
+     * and not a <code>1231</code> or <code>1237</code> as done in java.lang.Boolean. 
+     * This is in accordance with the Effective Java design. </p>
      *
      * @param value  the boolean to add to the <code>hashCode</code>
      * @return this
