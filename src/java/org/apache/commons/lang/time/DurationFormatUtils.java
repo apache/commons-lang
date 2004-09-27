da87a8669b475ce648553ade59882f21f5245eed
Henri Yandell <bayard@apache.org>
2004-09-27 03:31:59 +0000

    fixing the optimisation separation
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137937 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/time/DurationFormatUtils.java
+++ src/java/org/apache/commons/lang/time/DurationFormatUtils.java
@@ -41,7 +41,7 @@ import java.util.TimeZone;
  * @author <a href="mailto:ggregory@seagullsw.com">Gary Gregory</a>
  * @author Henri Yandell
  * @since 2.1
- * @version $Id: DurationFormatUtils.java,v 1.16 2004/09/27 03:30:10 bayard Exp $
+ * @version $Id: DurationFormatUtils.java,v 1.17 2004/09/27 03:31:59 bayard Exp $
  */
 public class DurationFormatUtils {
 
@@ -119,7 +119,7 @@ public class DurationFormatUtils {
      */
     public static String format(long millis, String format, boolean padWithZeros, TimeZone timezone) {
 
-        if(millis > 28 * DateUtils.MILLIS_PER_DAY) {
+        if(millis >= 28 * DateUtils.MILLIS_PER_DAY) {
             Calendar c = Calendar.getInstance(timezone);
             c.set(1970, 0, 1, 0, 0, 0);
             c.set(Calendar.MILLISECOND, 0);
