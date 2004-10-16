2a41c12c95bb41f7b07a270927b27f3bc5098621
Stephen Colebourne <scolebourne@apache.org>
2004-10-16 17:08:42 +0000

    Add methods to compare dates and calendars in different ways based on bug 26922
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@137982 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/time/DateUtils.java
+++ src/java/org/apache/commons/lang/time/DateUtils.java
@@ -31,7 +31,7 @@ import java.util.TimeZone;
  * @author <a href="mailto:ggregory@seagullsw.com">Gary Gregory</a>
  * @author Phil Steitz
  * @since 2.0
- * @version $Id: DateUtils.java,v 1.32 2004/10/13 21:48:00 scolebourne Exp $
+ * @version $Id: DateUtils.java,v 1.33 2004/10/16 17:08:42 scolebourne Exp $
  */
 public class DateUtils {
     
@@ -125,8 +125,8 @@ public class DateUtils {
      * 28 Mar 2002 13:45 and 12 Mar 2002 13:45 would return false.
      * </p>
      * 
-     * @param date1  the first date, not null
-     * @param date2  the second date, not null
+     * @param date1  the first date, not altered, not null
+     * @param date2  the second date, not altered, not null
      * @return true if they represent the same day
      * @throws IllegalArgumentException if either date is <code>null</code>
      * @since 2.1
@@ -142,7 +142,6 @@ public class DateUtils {
         return isSameDay(cal1, cal2);
     }
 
-    //-----------------------------------------------------------------------
     /**
      * <p>Checks if two calendar objects are on the same day ignoring time.</p>
      *
@@ -150,8 +149,8 @@ public class DateUtils {
      * 28 Mar 2002 13:45 and 12 Mar 2002 13:45 would return false.
      * </p>
      * 
-     * @param cal1  the first calendar, not null
-     * @param cal2  the second calendar, not null
+     * @param cal1  the first calendar, not altered, not null
+     * @param cal2  the second calendar, not altered, not null
      * @return true if they represent the same day
      * @throws IllegalArgumentException if either calendar is <code>null</code>
      * @since 2.1
@@ -163,6 +162,70 @@ public class DateUtils {
         return (cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA) &&
                 cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
                 cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR));
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * <p>Checks if two date objects represent the same instant in time.</p>
+     *
+     * <p>This method compares the long millisecond time of the two objects.</p>
+     * 
+     * @param date1  the first date, not altered, not null
+     * @param date2  the second date, not altered, not null
+     * @return true if they represent the same millisecond instant
+     * @throws IllegalArgumentException if either date is <code>null</code>
+     * @since 2.1
+     */
+    public static boolean isSameInstant(Date date1, Date date2) {
+        if (date1 == null || date2 == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+        return (date1.getTime() == date2.getTime());
+    }
+
+    /**
+     * <p>Checks if two calendar objects represent the same instant in time.</p>
+     *
+     * <p>This method compares the long millisecond time of the two objects.</p>
+     * 
+     * @param cal1  the first calendar, not altered, not null
+     * @param cal2  the second calendar, not altered, not null
+     * @return true if they represent the same millisecond instant
+     * @throws IllegalArgumentException if either date is <code>null</code>
+     * @since 2.1
+     */
+    public static boolean isSameInstant(Calendar cal1, Calendar cal2) {
+        if (cal1 == null || cal2 == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+        return (cal1.getTime().getTime() == cal2.getTime().getTime());
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * <p>Checks if two calendar objects represent the same local time.</p>
+     *
+     * <p>This method compares the values of the fields of the two objects.
+     * In addition, both calendars must be the same of the same type.</p>
+     * 
+     * @param cal1  the first calendar, not altered, not null
+     * @param cal2  the second calendar, not altered, not null
+     * @return true if they represent the same millisecond instant
+     * @throws IllegalArgumentException if either date is <code>null</code>
+     * @since 2.1
+     */
+    public static boolean isSameLocalTime(Calendar cal1, Calendar cal2) {
+        if (cal1 == null || cal2 == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+        return (cal1.get(Calendar.MILLISECOND) == cal2.get(Calendar.MILLISECOND) &&
+                cal1.get(Calendar.SECOND) == cal2.get(Calendar.SECOND) &&
+                cal1.get(Calendar.MINUTE) == cal2.get(Calendar.MINUTE) &&
+                cal1.get(Calendar.HOUR) == cal2.get(Calendar.HOUR) &&
+                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR) &&
+                cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA) &&
+                cal1.getClass() == cal2.getClass());
     }
 
     //-----------------------------------------------------------------------
