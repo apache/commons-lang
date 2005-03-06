325c03b6c378aeb25539bd737b8dc4123528d35b
Henri Yandell <bayard@apache.org>
2005-03-06 19:05:25 +0000

    also commenting out import statement for text so this builds :)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@156333 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/AllLangTestSuite.java
+++ src/test/org/apache/commons/lang/AllLangTestSuite.java
@@ -25,7 +25,7 @@ import org.apache.commons.lang.enums.EnumTestSuite;
 import org.apache.commons.lang.exception.ExceptionTestSuite;
 import org.apache.commons.lang.math.MathTestSuite;
 import org.apache.commons.lang.mutable.MutableTestSuite;
-import org.apache.commons.lang.text.TextTestSuite;
+//import org.apache.commons.lang.text.TextTestSuite;
 import org.apache.commons.lang.time.TimeTestSuite;
 
 /**
