63a4c5f02e172b87271d65c4222075d4f5eeb859
Fredrik Westermarck <fredrik@apache.org>
2006-05-05 15:23:31 +0000

    Resolved a few checkstyle issues.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@400092 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/builder/ReflectionToStringBuilder.java
+++ src/java/org/apache/commons/lang/builder/ReflectionToStringBuilder.java
@@ -23,10 +23,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
-
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.ClassUtils;
 
