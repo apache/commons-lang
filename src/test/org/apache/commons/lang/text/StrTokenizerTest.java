829e72a10e66d6ba411884fcd8adba1a05afa5fe
Stephen Colebourne <scolebourne@apache.org>
2006-07-22 18:12:41 +0000

    Copyright dates
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@424613 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/text/StrTokenizerTest.java
+++ src/test/org/apache/commons/lang/text/StrTokenizerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2005 The Apache Software Foundation.
+ * Copyright 2003-2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
