ce70915ae9e41820fcb49793044cc7360f7f7faf
Gary D. Gregory <ggregory@apache.org>
2006-08-26 22:07:15 +0000

    Updated Copyrights to 2006.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@437239 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/LocaleUtilsTest.java
+++ src/test/org/apache/commons/lang/LocaleUtilsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005 The Apache Software Foundation.
+ * Copyright 2005-2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import junit.textui.TestRunner;
  *
  * @author Chris Hyzer
  * @author Stephen Colebourne
- * @version $Id: LocaleUtilsTest.java 307282 2005-10-08 10:04:20Z scolebourne $
+ * @version $Id$
  */
 public class LocaleUtilsTest extends TestCase {
 
