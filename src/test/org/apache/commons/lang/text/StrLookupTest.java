a5a4f9067a0b22e629463b1ef059818ccda6f37e
Henri Yandell <bayard@apache.org>
2006-08-28 06:21:41 +0000

    Fixing the ASF copyright and placing it in the NOTICE file
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@437554 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/text/StrLookupTest.java
+++ src/test/org/apache/commons/lang/text/StrLookupTest.java
@@ -1,9 +1,10 @@
 /*
- * Copyright 2005-2006 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  * 
  *      http://www.apache.org/licenses/LICENSE-2.0
  * 
@@ -27,7 +28,7 @@ import junit.textui.TestRunner;
 /**
  * Test class for StrLookup.
  *
- * @version $Id: StrSubstitutorTest.java 231316 2005-08-10 20:36:26Z ggregory $
+ * @version $Id$
  */
 public class StrLookupTest extends TestCase {
 
