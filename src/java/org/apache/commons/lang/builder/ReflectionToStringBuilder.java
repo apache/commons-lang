bfeac15f064b62c29b8a0989e5d4d121797b76e7
Henri Yandell <bayard@apache.org>
2007-01-31 20:54:26 +0000

    Removing unused imports
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@501986 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/builder/ReflectionToStringBuilder.java
+++ src/java/org/apache/commons/lang/builder/ReflectionToStringBuilder.java
@@ -23,8 +23,6 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.ClassUtils;
 
