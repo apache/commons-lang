b16235ad108c4fcdb7117493394d26d2f0faeace
Henri Yandell <bayard@apache.org>
2007-02-06 22:49:50 +0000

    Applying test and fix for LANG-315
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@504351 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/time/StopWatch.java
+++ src/java/org/apache/commons/lang/time/StopWatch.java
@@ -115,7 +115,9 @@ public class StopWatch {
         if(this.runningState != STATE_RUNNING && this.runningState != STATE_SUSPENDED) {
             throw new IllegalStateException("Stopwatch is not running. ");
         }
-        stopTime = System.currentTimeMillis();
+        if(this.runningState == STATE_RUNNING) {
+            stopTime = System.currentTimeMillis();
+        }
         this.runningState = STATE_STOPPED;
     }
 
