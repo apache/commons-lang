3ef8a796b5ddf87e3e9afec9ba94aac62681b394
Henri Yandell <bayard@apache.org>
2007-02-06 22:49:50 +0000

    Applying test and fix for LANG-315
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@504351 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/time/StopWatchTest.java
+++ src/test/org/apache/commons/lang/time/StopWatchTest.java
@@ -108,6 +108,18 @@ public class StopWatchTest extends TestCase {
         assertTrue(totalTime < 1300);
     }
 
+    public void testLang315() {
+        StopWatch watch = new StopWatch();
+        watch.start();
+            try {Thread.sleep(200);} catch (InterruptedException ex) {}
+        watch.suspend();
+        long suspendTime = watch.getTime();
+            try {Thread.sleep(200);} catch (InterruptedException ex) {}
+        watch.stop();
+        long totalTime = watch.getTime();
+        assertTrue( suspendTime == totalTime );
+    }
+
     // test bad states
     public void testBadStates() {
         StopWatch watch = new StopWatch();
