dc55ab2697912ecc8f6453a8ceda373851fa419a
Hanson Char <hchar@apache.org>
2007-04-20 05:06:03 +0000

    removed pointless synchronized
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/lang/trunk@530645 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/builder/ToStringStyle.java
+++ src/java/org/apache/commons/lang/builder/ToStringStyle.java
@@ -103,7 +103,7 @@ public abstract class ToStringStyle implements Serializable {
      * </p>
      */
     private static ThreadLocal registry = new ThreadLocal() {
-        protected synchronized Object initialValue() {
+        protected Object initialValue() {
             // The HashSet implementation is not synchronized,
             // which is just what we need here.
             return new HashSet();
