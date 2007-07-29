bb2ca1dfb2e69d15686c65dbcaebb02f37f24e60
Henri Yandell <bayard@apache.org>
2007-07-29 20:03:50 +0000

    Fixing email addresses
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@560800 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/SystemUtils.java
+++ src/java/org/apache/commons/lang/SystemUtils.java
@@ -890,7 +890,7 @@ public class SystemUtils {
     // These MUST be declared after those above as they depend on the
     // values being set up
     // OS names from http://www.vamphq.com/os.html
-    // Selected ones included - please advise commons-dev@jakarta.apache.org
+    // Selected ones included - please advise dev@commons.apache.org
     // if you want another added or a mistake corrected
 
     /**
