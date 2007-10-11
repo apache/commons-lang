ee512dcc927ef3261b78fbe1dc9e97485f14a2f3
Gary D. Gregory <ggregory@apache.org>
2007-10-11 01:32:11 +0000

    Organize imports.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@583663 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/builder/MultiLineToStringStyleTest.java
+++ src/test/org/apache/commons/lang/builder/MultiLineToStringStyleTest.java
@@ -23,6 +23,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
+
 import org.apache.commons.lang.SystemUtils;
 
 /**
