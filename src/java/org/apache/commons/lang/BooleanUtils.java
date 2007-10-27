a5589b39e985e64861bd920aa7b13ab32c215b06
Henri Yandell <bayard@apache.org>
2007-10-27 05:07:45 +0000

    Applying Sebb's test and fix for LANG-365
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@589050 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/BooleanUtils.java
+++ src/java/org/apache/commons/lang/BooleanUtils.java
@@ -679,6 +679,7 @@ public class BooleanUtils {
                         (str.charAt(1) == 'E' || str.charAt(1) == 'e') &&
                         (str.charAt(2) == 'S' || str.charAt(2) == 's');
                 }
+                return false;
             }
             case 4: {
                 char ch = str.charAt(0);
