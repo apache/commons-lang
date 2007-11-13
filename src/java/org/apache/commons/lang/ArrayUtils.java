ecc7608fafef69637d46e60e32d93814353b8880
Henri Yandell <bayard@apache.org>
2007-11-13 14:35:33 +0000

    Moved toClass from ArrayUtils to ClassUtils as per Joerg's comment in LANG-333
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@594551 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/ArrayUtils.java
+++ src/java/org/apache/commons/lang/ArrayUtils.java
@@ -4414,27 +4414,4 @@ public class ArrayUtils {
         return result;
     }
 
-    /**
-     * <p>Converts an array of <code>Object</code> in to an Array of <code>Class</code> objects.</p>
-     *
-     * <p>This method returns <code>null</code> for a <code>null</code> input array.</p>
-     *
-     * @param array an <code>Object</code> array
-     * @return a <code>Class</code> array, <code>null</code> if null array input
-     * @since 2.4
-     */
-    public static Class[] toClass(Object[] array)
-    {
-        if (array == null) {
-            return null;
-        } else if (array.length == 0) {
-            return EMPTY_CLASS_ARRAY;
-        }
-        Class[] classes = new Class[array.length];
-        for (int i = 0; i < array.length; i++) {
-            classes[i] = array[i].getClass();
-        }
-        return classes;
-    }
-
 }
