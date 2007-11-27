7e51285c6364585624c45b92bd5a58c744047be5
Matthew Jason Benson <mbenson@apache.org>
2007-11-27 17:26:28 +0000

    remove unused variables
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@598706 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/math/FractionTest.java
+++ src/test/org/apache/commons/lang/math/FractionTest.java
@@ -403,7 +403,6 @@ public class FractionTest extends TestCase {
         
         // normal
         Fraction f2 = null;
-        int remainder, number1, number2 = 0;
         for (int i = 1; i <= 100; i++) {  // denominator
             for (int j = 1; j <= i; j++) {  // numerator
                 try {
