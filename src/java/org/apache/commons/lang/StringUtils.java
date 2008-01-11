0268232db1a6663d12f82ae2f5f83e921e53358e
Matthew Jason Benson <mbenson@apache.org>
2008-01-11 20:13:02 +0000

    [LANG-192] addSplitByCharacterType and camelCase variant
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@611288 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/StringUtils.java
+++ src/java/org/apache/commons/lang/StringUtils.java
@@ -2607,6 +2607,85 @@ public class StringUtils {
         return (String[]) list.toArray(new String[list.size()]);
     }
 
+    /**
+     * <p>Splits a String by Character type as returned by
+     * <code>java.lang.Character.getType(char)</code>. Groups of contiguous
+     * characters of the same type are returned as complete tokens. 
+     * <pre>
+     * StringUtils.splitByCamelCase(null)         = null
+     * StringUtils.splitByCamelCase("")           = []
+     * StringUtils.splitByCamelCase("ab de fg")   = ["ab", " ", "de", " ", "fg"]
+     * StringUtils.splitByCamelCase("ab   de fg") = ["ab", "   ", "de", " ", "fg"]
+     * StringUtils.splitByCamelCase("ab:cd:ef")   = ["ab", ":", "cd", ":", "ef"]
+     * StringUtils.splitByCamelCase("fooBar")     = ["foo", "B", "ar"]
+     * StringUtils.splitByCamelCase("foo200Bar")  = ["foo", "200", "B", "ar"]
+     * StringUtils.splitByCamelCase("ASFRules")   = ["ASFR", "ules"]
+     * </pre>
+     * @param str the String to split, may be <code>null</code>
+     * @return an array of parsed Strings, <code>null</code> if null String input
+     * @since 2.4
+     */
+    public static String[] splitByCharacterType(String str) {
+        return splitByCharacterType(str, false);
+    }
+
+    /**
+     * <p>Splits a String by Character type as returned by
+     * <code>java.lang.Character.getType(char)</code>. Groups of contiguous
+     * characters of the same type are returned as complete tokens, with the
+     * following exception: if <code>camelCase</code> is <code>true</code>,
+     * the character of type <code>Character.UPPERCASE_LETTER</code>, if any,
+     * immediately preceding a token of type <code>Character.LOWERCASE_LETTER</code>
+     * will belong to the following token rather than to the preceding, if any,
+     * <code>Character.UPPERCASE_LETTER</code> token. 
+     * <pre>
+     * StringUtils.splitByCamelCase(null)         = null
+     * StringUtils.splitByCamelCase("")           = []
+     * StringUtils.splitByCamelCase("ab de fg")   = ["ab", " ", "de", " ", "fg"]
+     * StringUtils.splitByCamelCase("ab   de fg") = ["ab", "   ", "de", " ", "fg"]
+     * StringUtils.splitByCamelCase("ab:cd:ef")   = ["ab", ":", "cd", ":", "ef"]
+     * StringUtils.splitByCamelCase("fooBar")     = ["foo", "Bar"]
+     * StringUtils.splitByCamelCase("foo200Bar")  = ["foo", "200", "Bar"]
+     * StringUtils.splitByCamelCase("ASFRules")   = ["ASF", "Rules"]
+     * </pre>
+     * @param str the String to split, may be <code>null</code>
+     * @param camelCase whether to use so-called "camel-case" for letter types
+     * @return an array of parsed Strings, <code>null</code> if null String input
+     * @since 2.4
+     */
+    public static String[] splitByCharacterType(String str, boolean camelCase) {
+        if (str == null) {
+            return null;
+        }
+        if (str.length() == 0) {
+            return ArrayUtils.EMPTY_STRING_ARRAY;
+        }
+        char[] c = str.toCharArray();
+        List list = new ArrayList();
+        int tokenStart = 0;
+        int currentType = Character.getType(c[tokenStart]);
+        for (int pos = tokenStart + 1; pos < c.length; pos++) {
+            int type = Character.getType(c[pos]);
+            if (type == currentType) {
+                continue;
+            }
+            if (camelCase && type == Character.LOWERCASE_LETTER
+                    && currentType == Character.UPPERCASE_LETTER) {
+                int newTokenStart = pos - 1;
+                if (newTokenStart != tokenStart) {
+                    list.add(new String(c, tokenStart, newTokenStart - tokenStart));
+                    tokenStart = newTokenStart;
+                }
+            } else {
+                list.add(new String(c, tokenStart, pos - tokenStart));
+                tokenStart = pos;
+            }
+            currentType = type;
+        }
+        list.add(new String(c, tokenStart, c.length - tokenStart));
+        return (String[]) list.toArray(new String[list.size()]);
+    }
+
     // Joining
     //-----------------------------------------------------------------------
     /**
