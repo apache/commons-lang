e266303c56334b7e9b040b36892c41b91fcc4cd9
Niall Pemberton <niallp@apache.org>
2008-01-25 15:20:32 +0000

    LANG-404 Add Calendar flavour format methods to DateFormatUtils
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@615243 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/time/DateFormatUtilsTest.java
+++ src/test/org/apache/commons/lang/time/DateFormatUtilsTest.java
@@ -85,6 +85,29 @@ public class DateFormatUtilsTest extends TestCase {
         assertEquals(buffer.toString(), DateFormatUtils.format(c.getTime().getTime(), "yyyyMdH", Locale.US));
     }
     
+    //-----------------------------------------------------------------------
+    public void testFormatCalendar() {
+        Calendar c = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        c.set(2005,0,1,12,0,0);
+        c.setTimeZone(TimeZone.getDefault());
+        StringBuffer buffer = new StringBuffer ();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        buffer.append (year);
+        buffer.append(month);
+        buffer.append(day);
+        buffer.append(hour);
+        assertEquals(buffer.toString(), DateFormatUtils.format(c, "yyyyMdH"));
+        
+        assertEquals(buffer.toString(), DateFormatUtils.format(c.getTime(), "yyyyMdH"));
+        
+        assertEquals(buffer.toString(), DateFormatUtils.format(c, "yyyyMdH", Locale.US));
+        
+        assertEquals(buffer.toString(), DateFormatUtils.format(c.getTime(), "yyyyMdH", Locale.US));
+    }
+    
     public void testFormatUTC() {
         Calendar c = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
         c.set(2005,0,1,12,0,0);
