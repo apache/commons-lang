a5ff053fd8444556c869f9c8cbcd1f55ae48f924
Matthew Jason Benson <mbenson@apache.org>
2008-02-26 15:36:55 +0000

    improve documentation
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@631256 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/text/FormatFactory.java
+++ src/java/org/apache/commons/lang/text/FormatFactory.java
@@ -15,7 +15,9 @@ public interface FormatFactory {
      * Create or retrieve a format instance.
      *
      * @param name The format type name
-     * @param arguments Arguments used to create the format instance
+     * @param arguments Arguments used to create the format instance. This allows the
+     *                  <code>FormatFactory</code> to implement the "format style"
+     *                  concept from <code>java.text.MessageFormat</code>.
      * @param locale The locale, may be null
      * @return The format instance
      */
