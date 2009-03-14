6637e145e683fff24a9342f35dcf38471e9fd197
Sebastian Bazley <sebb@apache.org>
2009-03-14 16:04:17 +0000

    Make private fields final
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@754471 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/builder/ToStringStyle.java
+++ src/java/org/apache/commons/lang/builder/ToStringStyle.java
@@ -133,7 +133,7 @@ public abstract class ToStringStyle implements Serializable {
      * to detect cyclical object references and avoid infinite loops.
      * </p>
      */
-    private static ThreadLocal registry = new ThreadLocal() {
+    private static final ThreadLocal registry = new ThreadLocal() {
         @Override
         protected Object initialValue() {
             // The HashSet implementation is not synchronized,
