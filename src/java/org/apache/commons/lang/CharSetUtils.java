d85758d68870e524fb62e68957f1eb2fff6c3fae
Sebastian Bazley <sebb@apache.org>
2009-03-15 15:06:03 +0000

    LANG-438 - replace links to Javadoc that was deleted with the deprecated methods
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@754677 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang/CharSetUtils.java
+++ src/java/org/apache/commons/lang/CharSetUtils.java
@@ -58,7 +58,7 @@ public class CharSetUtils {
      * CharSetUtils.squeeze("hello", "a-e") = "hello"
      * </pre>
      *
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * @see CharSet#getInstance(java.lang.String) for set-syntax.
      * @param str  the string to squeeze, may be null
      * @param set  the character set to use for manipulation, may be null
      * @return modified String, <code>null</code> if null string input
@@ -81,7 +81,7 @@ public class CharSetUtils {
      *   <li>squeeze(&quot;hello&quot;, {&quot;el&quot;}) => &quot;helo&quot;</li>
      * </ul>
      * 
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * @see CharSet#getInstance(java.lang.String) for set-syntax.
      * @param str  the string to squeeze, may be null
      * @param set  the character set to use for manipulation, may be null
      * @return modified String, <code>null</code> if null string input
@@ -124,7 +124,7 @@ public class CharSetUtils {
      * CharSetUtils.count("hello", "a-e") = 1
      * </pre>
      *
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * @see CharSet#getInstance(java.lang.String) for set-syntax.
      * @param str  String to count characters in, may be null
      * @param set  String set of characters to count, may be null
      * @return character count, zero if null string input
@@ -147,7 +147,7 @@ public class CharSetUtils {
      *  <li>count(&quot;hello&quot;, {&quot;c-f&quot;, &quot;o&quot;}) returns 2.</li>
      * </ul>
      *
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * @see CharSet#getInstance(java.lang.String) for set-syntax.
      * @param str  String to count characters in, may be null
      * @param set  String[] set of characters to count, may be null
      * @return character count, zero if null string input
@@ -183,7 +183,7 @@ public class CharSetUtils {
      * CharSetUtils.keep("hello", "le")  = "ell"
      * </pre>
      *
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * @see CharSet#getInstance(java.lang.String) for set-syntax.
      * @param str  String to keep characters from, may be null
      * @param set  String set of characters to keep, may be null
      * @return modified String, <code>null</code> if null string input
@@ -211,7 +211,7 @@ public class CharSetUtils {
      *   returns &quot;eo&quot;</li>
      * </ul>
      *
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * @see CharSet#getInstance(java.lang.String) for set-syntax.
      * @param str  String to keep characters from, may be null
      * @param set  String[] set of characters to keep, may be null
      * @return modified String, <code>null</code> if null string input
@@ -242,7 +242,7 @@ public class CharSetUtils {
      * CharSetUtils.delete("hello", "le")  = "ho"
      * </pre>
      *
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * @see CharSet#getInstance(java.lang.String) for set-syntax.
      * @param str  String to delete characters from, may be null
      * @param set  String set of characters to delete, may be null
      * @return modified String, <code>null</code> if null string input
@@ -266,7 +266,7 @@ public class CharSetUtils {
      *   &quot;hll&quot;</li>
      * </ul>
      *
-     * @see #evaluateSet(java.lang.String[]) for set-syntax.
+     * @see CharSet#getInstance(java.lang.String) for set-syntax.
      * @param str  String to delete characters from, may be null
      * @param set  String[] set of characters to delete, may be null
      * @return modified String, <code>null</code> if null string input
