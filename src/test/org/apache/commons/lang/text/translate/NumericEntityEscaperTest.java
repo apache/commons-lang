cc6281fc3eac8fb781be34f38a36b12d5ccc718e
Henri Yandell <bayard@apache.org>
2009-07-20 23:28:45 +0000

    Improving code coverage with more tests
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@796062 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/lang/text/translate/NumericEntityEscaperTest.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.lang.text.translate;
+
+import junit.framework.TestCase;
+
+import java.io.StringWriter;
+import java.io.IOException;
+
+/**
+ * Unit tests for {@link org.apache.commons.lang.text.translate.NumericEntityEscaper}.
+ */
+public class NumericEntityEscaperTest extends TestCase {
+
+    public void testBelow() throws IOException {
+        NumericEntityEscaper nee = NumericEntityEscaper.below('F');
+
+        String input = "ADFGZ";
+        String result = nee.translate(input);
+        assertEquals("Failed to escape numeric entities via the below method", "&#65;&#68;FGZ", result);
+    }
+
+    public void testBetween() throws IOException {
+        NumericEntityEscaper nee = NumericEntityEscaper.between('F', 'L');
+
+        String input = "ADFGZ";
+        String result = nee.translate(input);
+        assertEquals("Failed to escape numeric entities via the between method", "AD&#70;&#71;Z", result);
+    }
+
+    public void testAbove() throws IOException {
+        NumericEntityEscaper nee = NumericEntityEscaper.above('F');
+
+        String input = "ADFGZ";
+        String result = nee.translate(input);
+        assertEquals("Failed to escape numeric entities via the above method", "ADF&#71;&#90;", result);
+    }
+}
---
+++ src/test/org/apache/commons/lang/text/translate/UnicodeEscaperTest.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.lang.text.translate;
+
+import junit.framework.TestCase;
+
+import java.io.StringWriter;
+import java.io.IOException;
+
+/**
+ * Unit tests for {@link org.apache.commons.lang.text.translate.UnicodeEscaper}.
+ */
+public class UnicodeEscaperTest extends TestCase {
+
+    public void testBelow() throws IOException {
+        UnicodeEscaper nee = UnicodeEscaper.below('F');
+
+        String input = "ADFGZ";
+        String result = nee.translate(input);
+        assertEquals("Failed to escape unicode characters via the below method", "\\u0041\\u0044FGZ", result);
+    }
+
+    public void testBetween() throws IOException {
+        UnicodeEscaper nee = UnicodeEscaper.between('F', 'L');
+
+        String input = "ADFGZ";
+        String result = nee.translate(input);
+        assertEquals("Failed to escape unicode characters via the between method", "AD\\u0046\\u0047Z", result);
+    }
+
+    public void testAbove() throws IOException {
+        UnicodeEscaper nee = UnicodeEscaper.above('F');
+
+        String input = "ADFGZ";
+        String result = nee.translate(input);
+        assertEquals("Failed to escape unicode characters via the above method", "ADF\\u0047\\u005A", result);
+    }
+}
