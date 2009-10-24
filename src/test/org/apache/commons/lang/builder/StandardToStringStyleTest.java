14224204d9670518034e129a73f54bfb115576ce
Stephen Colebourne <scolebourne@apache.org>
2009-10-24 11:44:07 +0000

    Fix test case tear down that causes other tests to break
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@829348 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang/builder/StandardToStringStyleTest.java
+++ src/test/org/apache/commons/lang/builder/StandardToStringStyleTest.java
@@ -75,7 +75,7 @@ public class StandardToStringStyleTest extends TestCase {
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
-        ToStringBuilder.setDefaultStyle(STYLE);
+        ToStringBuilder.setDefaultStyle(ToStringStyle.DEFAULT_STYLE);
     }
 
     //----------------------------------------------------------------
