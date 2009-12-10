cf429f5324849c2017a09478bb84063b4ec73f98
Henri Yandell <bayard@apache.org>
2009-12-10 12:04:56 +0000

    Moving back from lang3 to lang for the exception stack traces in these test documentation comments as they indicate historical cases. LANG-563
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@889222 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang3/builder/HashCodeBuilderTest.java
+++ src/test/org/apache/commons/lang3/builder/HashCodeBuilderTest.java
@@ -530,18 +530,18 @@ public class HashCodeBuilderTest extends TestCase {
         // java.lang.StackOverflowError
         // at java.lang.ClassLoader.getCallerClassLoader(Native Method)
         // at java.lang.Class.getDeclaredFields(Class.java:992)
-        // at org.apache.commons.lang3.builder.HashCodeBuilder.reflectionAppend(HashCodeBuilder.java:373)
-        // at org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode(HashCodeBuilder.java:349)
-        // at org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode(HashCodeBuilder.java:155)
+        // at org.apache.commons.lang.builder.HashCodeBuilder.reflectionAppend(HashCodeBuilder.java:373)
+        // at org.apache.commons.lang.builder.HashCodeBuilder.reflectionHashCode(HashCodeBuilder.java:349)
+        // at org.apache.commons.lang.builder.HashCodeBuilder.reflectionHashCode(HashCodeBuilder.java:155)
         // at
-        // org.apache.commons.lang3.builder.HashCodeBuilderTest$ReflectionTestCycleB.hashCode(HashCodeBuilderTest.java:53)
-        // at org.apache.commons.lang3.builder.HashCodeBuilder.append(HashCodeBuilder.java:422)
-        // at org.apache.commons.lang3.builder.HashCodeBuilder.reflectionAppend(HashCodeBuilder.java:383)
-        // at org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode(HashCodeBuilder.java:349)
-        // at org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode(HashCodeBuilder.java:155)
+        // org.apache.commons.lang.builder.HashCodeBuilderTest$ReflectionTestCycleB.hashCode(HashCodeBuilderTest.java:53)
+        // at org.apache.commons.lang.builder.HashCodeBuilder.append(HashCodeBuilder.java:422)
+        // at org.apache.commons.lang.builder.HashCodeBuilder.reflectionAppend(HashCodeBuilder.java:383)
+        // at org.apache.commons.lang.builder.HashCodeBuilder.reflectionHashCode(HashCodeBuilder.java:349)
+        // at org.apache.commons.lang.builder.HashCodeBuilder.reflectionHashCode(HashCodeBuilder.java:155)
         // at
-        // org.apache.commons.lang3.builder.HashCodeBuilderTest$ReflectionTestCycleA.hashCode(HashCodeBuilderTest.java:42)
-        // at org.apache.commons.lang3.builder.HashCodeBuilder.append(HashCodeBuilder.java:422)
+        // org.apache.commons.lang.builder.HashCodeBuilderTest$ReflectionTestCycleA.hashCode(HashCodeBuilderTest.java:42)
+        // at org.apache.commons.lang.builder.HashCodeBuilder.append(HashCodeBuilder.java:422)
 
         a.hashCode();
         b.hashCode();
