cbbb7b8f164e67cdcf3b6a55baba1498dade9a10
Sebastian Bazley <sebb@apache.org>
2009-12-14 17:54:26 +0000

    Fix generic map type
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@890413 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/lang3/text/StrSubstitutorTest.java
+++ src/test/org/apache/commons/lang3/text/StrSubstitutorTest.java
@@ -283,7 +283,7 @@ public class StrSubstitutorTest extends TestCase {
      */
     public void testResolveVariable() {
         final StrBuilder builder = new StrBuilder("Hi ${name}!");
-        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, String> map = new HashMap<String, String>();
         map.put("name", "commons");
         StrSubstitutor sub = new StrSubstitutor(map) {
             @Override
