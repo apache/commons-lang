a95e76eefb91bdd9cf1c22734874a3d3c95ed327
Henri Yandell <bayard@apache.org>
2009-12-17 06:04:28 +0000

    Fixing LANG-538 - you need to call getTime() on a calendar sometimes to get it in the right state, otherwise the timezone gets out of whack.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@891542 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang3/time/FastDateFormat.java
+++ src/java/org/apache/commons/lang3/time/FastDateFormat.java
@@ -869,6 +869,7 @@ public class FastDateFormat extends Format {
      */
     public StringBuffer format(Calendar calendar, StringBuffer buf) {
         if (mTimeZoneForced) {
+            calendar.getTime(); /// LANG-538
             calendar = (Calendar) calendar.clone();
             calendar.setTimeZone(mTimeZone);
         }
