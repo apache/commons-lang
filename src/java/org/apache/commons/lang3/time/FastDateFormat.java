e5ccf096569244af25820f7d8d94893fff00ce8c
Henri Yandell <bayard@apache.org>
2009-12-18 07:21:10 +0000

    Switching from getTime() to getTimeInMillis() per Sebb's report that it still fixes LANG-538 and is going to be lighter weight
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@892161 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/lang3/time/FastDateFormat.java
+++ src/java/org/apache/commons/lang3/time/FastDateFormat.java
@@ -869,7 +869,7 @@ public class FastDateFormat extends Format {
      */
     public StringBuffer format(Calendar calendar, StringBuffer buf) {
         if (mTimeZoneForced) {
-            calendar.getTime(); /// LANG-538
+            calendar.getTimeInMillis(); /// LANG-538
             calendar = (Calendar) calendar.clone();
             calendar.setTimeZone(mTimeZone);
         }
