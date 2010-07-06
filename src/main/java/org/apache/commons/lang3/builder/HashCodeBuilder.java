f5fb30817e2072958dc14a7ac2578125089df4e1
Henri Yandell <bayard@apache.org>
2010-07-06 07:36:38 +0000

    Updating javadoc per Sean Mickey's 2nd comment to LANG-628
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@960834 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/builder/HashCodeBuilder.java
+++ src/main/java/org/apache/commons/lang3/builder/HashCodeBuilder.java
@@ -615,11 +615,16 @@ public class HashCodeBuilder implements Builder<Integer> {
      * Append a <code>hashCode</code> for a <code>boolean</code>.
      * </p>
      * <p>
-     * This adds <code>1</code> when true, and <code>0</code> when false to the <code>hashCode</code> 
-     * and not a <code>1231</code> or
-     * <code>1237</code> as done in java.lang.Boolean (see java.lang.Boolean javadoc). This is in 
-     * accordance with the <quote>Effective Java</quote>
-     * design.
+     * This adds <code>1</code> when true, and <code>0</code> when false to the <code>hashCode</code>.
+     * </p>
+     * <p>
+     * This is in contrast to the standard <code>java.lang.Boolean.hashCode</code> handling, which computes
+     * a <code>hashCode</code> value of <code>1231</code> for <code>java.lang.Boolean</code> instances
+     * that represent <code>true</code> or <code>1237</code> for <code>java.lang.Boolean</code> instances
+     * that represent <code>false</code>.
+     * </p>
+     * <p>
+     * This is in accordance with the <quote>Effective Java</quote> design.
      * </p>
      * 
      * @param value
