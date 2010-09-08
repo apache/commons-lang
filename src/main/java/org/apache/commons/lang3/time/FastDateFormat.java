14a0cc2a9baf84a97348263975082ef3857daf97
Henri Yandell <bayard@apache.org>
2010-09-08 06:09:57 +0000

    Applying my fix to LANG-645, and Mikael's test case; fixing the FastDateFormat to properly include the locale when formatting a Date
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@993620 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/time/FastDateFormat.java
+++ src/main/java/org/apache/commons/lang3/time/FastDateFormat.java
@@ -817,7 +817,7 @@ public class FastDateFormat extends Format {
      * @return the formatted string
      */
     public String format(Date date) {
-        Calendar c = new GregorianCalendar(mTimeZone);
+        Calendar c = new GregorianCalendar(mTimeZone, mLocale);
         c.setTime(date);
         return applyRules(c, new StringBuffer(mMaxLengthEstimate)).toString();
     }
