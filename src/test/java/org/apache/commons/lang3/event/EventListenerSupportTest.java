4d6acd80b65b0da01bbcec66abb3f04dd20b13fb
Matthew Jason Benson <mbenson@apache.org>
2010-09-13 21:14:15 +0000

    unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@996691 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/lang3/event/EventListenerSupportTest.java
+++ src/test/java/org/apache/commons/lang3/event/EventListenerSupportTest.java
@@ -26,7 +26,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
