7f48b196fde0cc16fdddbeecbb30a38fb79acfb5
Niall Pemberton <niallp@apache.org>
2011-01-09 02:41:34 +0000

    Port LANG-645 to LANG 2.x Branch - fix the FastDateFormat to properly include the locale when formatting a Date
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/branches/LANG_2_X@1056869 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/lang/time/FastDateFormatTest.java
+++ src/test/java/org/apache/commons/lang/time/FastDateFormatTest.java
@@ -322,4 +322,16 @@ public class FastDateFormatTest extends TestCase {
         FastDateFormat format = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", TimeZone.getTimeZone("GMT"));
         assertEquals("dateTime", dateTime, format.format(cal));
     }
+
+    public void testLang645() {
+        Locale locale = new Locale("sv", "SE");
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2010, 0, 1, 12, 0, 0);
+        Date d = cal.getTime();
+
+        FastDateFormat fdf = FastDateFormat.getInstance("EEEE', week 'ww", locale);
+
+        assertEquals("fredag, week 53", fdf.format(d));
+    }
 }
