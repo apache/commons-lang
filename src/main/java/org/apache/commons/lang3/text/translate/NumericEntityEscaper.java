e4f94e085a7c068c7000def150a2ceb708232aee
Henri Yandell <bayard@apache.org>
2011-02-02 06:39:50 +0000

    Adding javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1066350 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/text/translate/NumericEntityEscaper.java
+++ src/main/java/org/apache/commons/lang3/text/translate/NumericEntityEscaper.java
@@ -32,28 +32,63 @@ public class NumericEntityEscaper extends CodePointTranslator {
     private final int above;
     private final boolean between;
 
+    /**
+     * <p>Constructs a <code>NumericEntityEscaper</code> for the specified range. This is 
+     * the underlying method for the other constructors/builders. The <code>below</code>
+     * and <code>above</code> boundaries are inclusive when <code>between</code> is 
+     * <code>true</code> and exclusive when it is <code>false</code>. </p>
+     *
+     * @param below int value representing the lowest codepoint boundary
+     * @param above int value representing the highest codepoint boundary
+     * @param between whether to escape between the boundaries or outside them
+     */
     private NumericEntityEscaper(int below, int above, boolean between) {
         this.below = below;
         this.above = above;
         this.between = between;
     }
 
+    /**
+     * <p>Constructs a <code>NumericEntityEscaper</code> for all characters. </p>
+     */
     public NumericEntityEscaper() { 
         this(0, Integer.MAX_VALUE, true);
     }
 
+    /**
+     * <p>Constructs a <code>NumericEntityEscaper</code> below the specified value (exclusive). </p>
+     *
+     * @param codepoint below which to escape
+     */
     public static NumericEntityEscaper below(int codepoint) {
         return outsideOf(codepoint, Integer.MAX_VALUE);
     }
 
+    /**
+     * <p>Constructs a <code>NumericEntityEscaper</code> above the specified value (exclusive). </p>
+     *
+     * @param codepoint above which to escape
+     */
     public static NumericEntityEscaper above(int codepoint) {
         return outsideOf(0, codepoint);
     }
 
+    /**
+     * <p>Constructs a <code>NumericEntityEscaper</code> between the specified values (inclusive). </p>
+     *
+     * @param codepointLow above which to escape
+     * @param codepointHigh below which to escape
+     */
     public static NumericEntityEscaper between(int codepointLow, int codepointHigh) {
         return new NumericEntityEscaper(codepointLow, codepointHigh, true);
     }
 
+    /**
+     * <p>Constructs a <code>NumericEntityEscaper</code> outside of the specified values (exclusive). </p>
+     *
+     * @param codepointLow below which to escape
+     * @param codepointHigh above which to escape
+     */
     public static NumericEntityEscaper outsideOf(int codepointLow, int codepointHigh) {
         return new NumericEntityEscaper(codepointLow, codepointHigh, false);
     }
