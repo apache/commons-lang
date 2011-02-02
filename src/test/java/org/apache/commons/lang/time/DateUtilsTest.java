62e687f06bec0edda85054517a9c26de9bd72d7c
Niall Pemberton <niallp@apache.org>
2011-02-02 22:02:32 +0000

    Port LANG-677 to 2.x branch - DateUtils isSameLocalTime() compares the hour using 12hour Calendar.HOUR instead of 24hour Calendar.HOUR_OF_DAY - thanks to Christian
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/branches/LANG_2_X@1066653 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/lang/time/DateUtilsTest.java
+++ src/test/java/org/apache/commons/lang/time/DateUtilsTest.java
@@ -226,6 +226,14 @@ public class DateUtilsTest extends TestCase {
         cal2.set(2004, 6, 9, 13, 45, 0);
         cal2.set(Calendar.MILLISECOND, 0);
         assertEquals(true, DateUtils.isSameLocalTime(cal1, cal2));
+
+        Calendar cal3 = Calendar.getInstance();
+        Calendar cal4 = Calendar.getInstance();
+        cal3.set(2004, 6, 9, 4,  0, 0);
+        cal4.set(2004, 6, 9, 16, 0, 0);
+        cal3.set(Calendar.MILLISECOND, 0);
+        cal4.set(Calendar.MILLISECOND, 0);
+        assertFalse("LANG-677", DateUtils.isSameLocalTime(cal3, cal4));
         
         cal2.set(2004, 6, 9, 11, 45, 0);
         assertEquals(false, DateUtils.isSameLocalTime(cal1, cal2));
