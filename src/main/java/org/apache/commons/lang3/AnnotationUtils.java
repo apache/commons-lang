a335e80f01870db86c6da463af97e63c74a808e3
Sebastian Bazley <sebb@apache.org>
2011-03-08 16:24:00 +0000

    Fix incorrect Javadoc re thrown exceptions; requires further change to private method
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1079417 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/AnnotationUtils.java
+++ src/main/java/org/apache/commons/lang3/AnnotationUtils.java
@@ -152,8 +152,9 @@ public class AnnotationUtils {
      * 
      * @param a the Annotation for a hash code calculation is desired, not null
      * @return the calculated hash code
-     * @throws IllegalAccessException if thrown during annotation access
-     * @throws InvocationTargetException if thrown during annotation access
+     * @throws RuntimeException if IllegalAccessException or InvocationTargetException 
+     * or any other Exception is thrown during annotation access
+     * @throws IllegalStateException if an annotation method invocation returns {@code null}
      */
     public static int hashCode(Annotation a) {
         int result = 0;
@@ -221,8 +222,7 @@ public class AnnotationUtils {
     }
 
     //besides modularity, this has the advantage of autoboxing primitives:
-    private static int hashMember(String name, Object value)
-            throws IllegalAccessException, InvocationTargetException {
+    private static int hashMember(String name, Object value) {
         int part1 = name.hashCode() * 127;
         if (value.getClass().isArray()) {
             return part1 ^ arrayMemberHash(value.getClass().getComponentType(), value);
