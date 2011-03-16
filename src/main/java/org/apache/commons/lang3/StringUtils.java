196067da9716e4ae4078743c0c56540ee64892f4
Henri Yandell <bayard@apache.org>
2011-03-16 04:39:17 +0000

    Removing CharSequenceUtils in favour of putting the code in StringUtils
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1082046 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -5021,7 +5021,7 @@ public class StringUtils {
         }
         return new StringBuilder(strLen)
             .append(Character.toTitleCase(cs.charAt(0)))
-            .append(CharSequenceUtils.subSequence(cs, 1))
+            .append(StringUtils.subSequence(cs, 1))
             .toString();
     }
 
@@ -5056,7 +5056,7 @@ public class StringUtils {
         }
         return new StringBuilder(strLen)
             .append(Character.toLowerCase(cs.charAt(0)))
-            .append(CharSequenceUtils.subSequence(cs, 1))
+            .append(StringUtils.subSequence(cs, 1))
             .toString();
     }
 
@@ -6384,4 +6384,24 @@ public class StringUtils {
         }
         return false;
     }
+
+    //-----------------------------------------------------------------------
+    /**
+     * <p>Returns a new {@code CharSequence} that is a subsequence of this
+     * sequence starting with the {@code char} value at the specified index.</p>
+     * 
+     * <p>This provides the {@code CharSequence} equivalent to {@link String#substring(int)}.
+     * The length (in {@code char}) of the returned sequence is {@code length() - start},
+     * so if {@code start == end} then an empty sequence is returned.</p>
+     *
+     * @param cs  the specified subsequence, null returns null
+     * @param start  the start index, inclusive, valid
+     * @return a new subsequence, may be null
+     * @throws IndexOutOfBoundsException if {@code start} is negative or if 
+     *  {@code start} is greater than {@code length()}
+     */
+    public static CharSequence subSequence(CharSequence cs, int start) {
+        return cs == null ? null : cs.subSequence(start, cs.length());
+    }
+
 }
