37a40f70ecd663f4472aed9eaa94f6181fc42301
Oliver Heger <oheger@apache.org>
2011-03-18 20:53:52 +0000

    Checkstyle and trailing spaces.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1083066 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/CharEncoding.java
+++ src/main/java/org/apache/commons/lang3/CharEncoding.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,13 +22,14 @@ import java.nio.charset.IllegalCharsetNameException;
 
 /**
  * <p>Character encoding names required of every implementation of the Java platform.</p>
- * 
+ *
  * <p>According to <a href="http://java.sun.com/j2se/1.3/docs/api/java/lang/package-summary.html#charenc">JRE character
  * encoding names</a>:</p>
- * 
- * <p><cite>Every implementation of the Java platform is required to support the following character encodings. Consult the
- * release documentation for your implementation to see if any other encodings are supported.</cite></p>
- * 
+ *
+ * <p><cite>Every implementation of the Java platform is required to support the following character encodings.
+ * Consult the release documentation for your implementation to see if any other encodings are supported.
+ * </cite></p>
+ *
  * @see <a href="http://download.oracle.com/javase/1.3/docs/guide/intl/encoding.doc.html">JRE character encoding names</a>
  * @author Apache Software Foundation
  * @since 2.1
@@ -38,7 +39,7 @@ public class CharEncoding {
 
     /**
      * <p>ISO Latin Alphabet #1, also known as ISO-LATIN-1.</p>
-     * 
+     *
      * <p>Every implementation of the Java platform is required to support this character encoding.</p>
      */
     public static final String ISO_8859_1 = "ISO-8859-1";
@@ -46,7 +47,7 @@ public class CharEncoding {
     /**
      * <p>Seven-bit ASCII, also known as ISO646-US, also known as the Basic Latin block
      * of the Unicode character set.</p>
-     * 
+     *
      * <p>Every implementation of the Java platform is required to support this character encoding.</p>
      */
     public static final String US_ASCII = "US-ASCII";
@@ -54,28 +55,28 @@ public class CharEncoding {
     /**
      * <p>Sixteen-bit Unicode Transformation Format, byte order specified by a mandatory initial
      * byte-order mark (either order accepted on input, big-endian used on output).</p>
-     * 
+     *
      * <p>Every implementation of the Java platform is required to support this character encoding.</p>
      */
     public static final String UTF_16 = "UTF-16";
 
     /**
      * <p>Sixteen-bit Unicode Transformation Format, big-endian byte order.</p>
-     * 
+     *
      * <p>Every implementation of the Java platform is required to support this character encoding.</p>
      */
     public static final String UTF_16BE = "UTF-16BE";
 
     /**
      * <p>Sixteen-bit Unicode Transformation Format, little-endian byte order.</p>
-     * 
+     *
      * <p>Every implementation of the Java platform is required to support this character encoding.</p>
      */
     public static final String UTF_16LE = "UTF-16LE";
 
     /**
      * <p>Eight-bit Unicode Transformation Format.</p>
-     * 
+     *
      * <p>Every implementation of the Java platform is required to support this character encoding.</p>
      */
     public static final String UTF_8 = "UTF-8";
@@ -83,11 +84,11 @@ public class CharEncoding {
     //-----------------------------------------------------------------------
     /**
      * <p>Returns whether the named charset is supported.</p>
-     * 
+     *
      * <p>This is similar to <a
      * href="http://download.oracle.com/javase/1.4.2/docs/api/java/nio/charset/Charset.html#isSupported%28java.lang.String%29">
      * java.nio.charset.Charset.isSupported(String)</a> but handles more formats</p>
-     * 
+     *
      * @param name  the name of the requested charset; may be either a canonical name or an alias, null returns false
      * @return {@code true} if the charset is available in the current Java virtual machine
      */
