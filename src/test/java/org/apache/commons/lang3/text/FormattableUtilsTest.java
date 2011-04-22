35dedafa35ddc45de42e4891b42520f69beed99d
Gary D. Gregory <ggregory@apache.org>
2011-04-22 21:53:14 +0000

    Move new FormattableUtils class to .text from .util.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1096058 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/lang3/text/FormattableUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/text/FormattableUtilsTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.lang3.util;
+package org.apache.commons.lang3.text;
 
 import static java.util.FormattableFlags.LEFT_JUSTIFY;
 import static org.junit.Assert.assertEquals;
