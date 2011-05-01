b1aa85b7480af1bbce9b651930a5fec8beca7973
Oliver Heger <oheger@apache.org>
2011-05-01 20:27:16 +0000

    Fixed a test failure which occurred under certain circumstances (Java 1.5 with default locale GERMAN).
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1098403 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/lang3/time/FastDateFormatTest.java
+++ src/test/java/org/apache/commons/lang3/time/FastDateFormatTest.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -96,7 +96,7 @@ public class FastDateFormatTest extends TestCase {
 
             assertTrue(format1 != format2); // -- junit 3.8 version -- assertFalse(format1 == format2);
             assertSame(format1, format3);
-            assertSame(Locale.GERMANY, format1.getLocale());
+            assertEquals(Locale.GERMANY, format1.getLocale());
 
         } finally {
             Locale.setDefault(realDefaultLocale);
@@ -217,13 +217,13 @@ public class FastDateFormatTest extends TestCase {
             fdf = FastDateFormat.getInstance(pattern);
             sdf = new SimpleDateFormat(pattern);
             assertEquals(sdf.format(date1), fdf.format(date1));
-            assertEquals(sdf.format(date2), fdf.format(date2));            
+            assertEquals(sdf.format(date2), fdf.format(date2));
         } finally {
             Locale.setDefault(realDefaultLocale);
             TimeZone.setDefault(realDefaultZone);
         }
     }
-    
+
     /**
      * Test case for {@link FastDateFormat#getDateInstance(int, java.util.Locale)}.
      */
