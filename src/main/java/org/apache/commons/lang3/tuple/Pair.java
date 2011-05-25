70e5580cdcaad6a24c07ddf0e4ff120260436924
Stephen Colebourne <scolebourne@apache.org>
2011-05-25 15:43:04 +0000

    Javadoc fix
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1127563 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/tuple/Pair.java
+++ src/main/java/org/apache/commons/lang3/tuple/Pair.java
@@ -163,11 +163,11 @@ public abstract class Pair<L, R> implements Map.Entry<L, R>, Comparable<Pair<L, 
      * <p>Formats the receiver using the given format.</p>
      * 
      * <p>This uses {@link Formattable} to perform the formatting. Two variables may
-     * be used to embed the left and right elements. Use {@code %1$} for the left
-     * element (key) and {@code %2$} for the right element (value).
+     * be used to embed the left and right elements. Use {@code %1$s} for the left
+     * element (key) and {@code %2$s} for the right element (value).
      * The default format used by {@code toString()} is {@code (%1$s,%2$s)}.</p>
      * 
-     * @param format  the format string, optionally containing {@code %1$} and {@code %2$}, not null
+     * @param format  the format string, optionally containing {@code %1$s} and {@code %2$s}, not null
      * @return the formatted string, not null
      */
     public String toString(String format) {
