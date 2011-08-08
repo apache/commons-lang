b0e216a2081d8febe70bffe3849437b9624ec24e
Henri Yandell <bayard@apache.org>
2011-08-08 05:26:12 +0000

    Fixing package names and adding excludes to the pom.xml for the parts of the collections framework that followed over
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1154844 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/lang3/compare/TestComparableComparator.java
+++ src/test/java/org/apache/commons/lang3/compare/TestComparableComparator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.collections.comparators;
+package org.apache.commons.lang3.compare;
 
 import java.util.Comparator;
 import java.util.LinkedList;
