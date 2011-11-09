f7059c744730e3ed02b6693f6b3a0576e92fb544
Sebastian Bazley <sebb@apache.org>
2011-11-09 13:11:07 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1199735 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/builder/CompareToBuilder.java
+++ src/main/java/org/apache/commons/lang3/builder/CompareToBuilder.java
@@ -997,19 +997,20 @@ public class CompareToBuilder implements Builder<Integer> {
      * side.
      * 
      * @return final comparison result
+     * @see #build()
      */
     public int toComparison() {
         return comparison;
     }
 
     /**
-     * Returns a negative integer, a positive integer, or zero as
+     * Returns a negative Integer, a positive Integer, or zero as
      * the <code>builder</code> has judged the "left-hand" side
      * as less than, greater than, or equal to the "right-hand"
      * side.
      * 
-     * @return final comparison result
-     * 
+     * @return final comparison result as an Integer
+     * @see #toComparison()
      * @since 3.0
      */
     public Integer build() {
