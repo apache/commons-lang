0efe1c6171b99b960ea9b37638b0591af7bc2d7a
Joerg Schaible <joehni@apache.org>
2012-09-27 22:35:09 +0000

    "import java.lang.*" is always implicit.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1391246 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/lang3/ConversionTest.java
+++ src/test/java/org/apache/commons/lang3/ConversionTest.java
@@ -22,8 +22,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.fail;
 
-import java.lang.Throwable;
-
 import org.junit.Test;
 
 
