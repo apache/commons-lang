2c9c8753165dc7ce5dd1d5a6d741b445b33302c2
Sebastian Bazley <sebb@apache.org>
2013-05-19 10:22:13 +0000

    LANG-693 Method createNumber from NumberUtils doesn't work for floating point numbers other than Float
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1484263 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -482,6 +482,7 @@ public class NumberUtils {
         // if both e and E are present, this is caught by the checks on expPos (which prevent IOOBE)
         // and the parsing which will detect if e or E appear in a number due to using the wrong offset
 
+        int numDecimals = 0; // Check required precision (LANG-693)
         if (decPos > -1) { // there is a decimal point
 
             if (expPos > -1) { // there is an exponent
@@ -493,6 +494,7 @@ public class NumberUtils {
                 dec = str.substring(decPos + 1);
             }
             mant = str.substring(0, decPos);
+            numDecimals = dec.length(); // gets number of digits past the decimal to ensure no loss of precision for floating point numbers.
         } else {
             if (expPos > -1) {
                 if (expPos > str.length()) { // prevents double exponent causing IOOBE
@@ -588,17 +590,21 @@ public class NumberUtils {
         //Must be a Float, Double, BigDecimal
         final boolean allZeros = isAllZeros(mant) && isAllZeros(exp);
         try {
-            final Float f = createFloat(str);
-            if (!(f.isInfinite() || (f.floatValue() == 0.0F && !allZeros))) {
-                return f;
+            if(numDecimals <= 7){// If number has 7 or fewer digits past the decimal point then make it a float
+                final Float f = createFloat(str);
+                if (!(f.isInfinite() || (f.floatValue() == 0.0F && !allZeros))) {
+                    return f;
+                }
             }
         } catch (final NumberFormatException nfe) { // NOPMD
             // ignore the bad number
         }
         try {
-            final Double d = createDouble(str);
-            if (!(d.isInfinite() || (d.doubleValue() == 0.0D && !allZeros))) {
-                return d;
+            if(numDecimals <= 16){// If number has between 8 and 16 digits past the decimal point then make it a double
+                final Double d = createDouble(str);
+                if (!(d.isInfinite() || (d.doubleValue() == 0.0D && !allZeros))) {
+                    return d;
+                }
             }
         } catch (final NumberFormatException nfe) { // NOPMD
             // ignore the bad number
