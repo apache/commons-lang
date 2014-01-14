0fb96b2456f0f6b233948d8664ed784bdada8ca9
patrick-a-cavanaugh <me@patcavanaugh.info>
2014-01-14 15:07:31 -0500

    Update ImmutablePair to be not final
    The sibling class MutablePair is not final, and the JavaDoc for ImmutablePair also states it is not final: "The class is also not {@code final}, so a subclass could add undesirable behaviour".

--- src/main/java/org/apache/commons/lang3/tuple/ImmutablePair.java
+++ src/main/java/org/apache/commons/lang3/tuple/ImmutablePair.java
@@ -32,7 +32,7 @@ package org.apache.commons.lang3.tuple;
  * @since Lang 3.0
  * @version $Id$
  */
-public final class ImmutablePair<L, R> extends Pair<L, R> {
+public class ImmutablePair<L, R> extends Pair<L, R> {
 
     /** Serialization version */
     private static final long serialVersionUID = 4954918890077093841L;
