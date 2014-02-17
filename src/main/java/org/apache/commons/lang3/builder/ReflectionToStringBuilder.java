600432075b9e07e160d5a9ba4ee615a2e9f20c6c
Bruno P. Kinoshita <kinow@apache.org>
2014-02-17 15:45:17 +0000

    LANG-621: ReflectionToStringBuilder.toString does not debug 3rd party object fields within 3rd party object. Suggested by Philip Hodges. Final patch from Thomas Neidhart.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1569028 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/builder/ReflectionToStringBuilder.java
+++ src/main/java/org/apache/commons/lang3/builder/ReflectionToStringBuilder.java
@@ -81,7 +81,12 @@ import org.apache.commons.lang3.ClassUtils;
  * <p>
  * The exact format of the <code>toString</code> is determined by the {@link ToStringStyle} passed into the constructor.
  * </p>
- * 
+ *
+ * <p>
+ * <b>Note:</b> the default {@link ToStringStyle} will only do a "shallow" formatting, i.e. composed objects are not
+ * further traversed. To get "deep" formatting, use an instance of {@link RecursiveToStringStyle}.
+ * </p>
+ *
  * @since 2.0
  * @version $Id$
  */
