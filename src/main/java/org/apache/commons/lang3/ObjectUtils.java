94583a1b02fadda66b55f89cc0ab795d96d4ea25
Niall Pemberton <niallp@apache.org>
2014-04-01 20:54:53 +0000

    Fix JavaDoc issues with JDK 1.8
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1583781 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/ObjectUtils.java
+++ src/main/java/org/apache/commons/lang3/ObjectUtils.java
@@ -482,7 +482,7 @@ public class ObjectUtils {
      * @param <T> type of the values processed by this method
      * @param c1  the first comparable, may be null
      * @param c2  the second comparable, may be null
-     * @return a negative value if c1 &gt; c2, zero if c1 = c2
+     * @return a negative value if c1 &lt; c2, zero if c1 = c2
      *  and a positive value if c1 &gt; c2
      */
     public static <T extends Comparable<? super T>> int compare(final T c1, final T c2) {
