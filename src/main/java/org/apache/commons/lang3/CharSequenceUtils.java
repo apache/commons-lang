04df7d430896075061b50dafc346d57623b79363
Gary D. Gregory <ggregory@apache.org>
2014-04-10 13:12:12 +0000

    Statement unnecessarily nested within else clause.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1586290 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/CharSequenceUtils.java
+++ src/main/java/org/apache/commons/lang3/CharSequenceUtils.java
@@ -115,21 +115,20 @@ public class CharSequenceUtils {
     static int lastIndexOf(final CharSequence cs, final int searchChar, int start) {
         if (cs instanceof String) {
             return ((String) cs).lastIndexOf(searchChar, start);
-        } else {
-            final int sz = cs.length();
-            if (start < 0) {
-                return -1;
-            }
-            if (start >= sz) {
-                start = sz - 1;
-            }
-            for (int i = start; i >= 0; --i) {
-                if (cs.charAt(i) == searchChar) {
-                    return i;
-                }
-            }
+        }
+        final int sz = cs.length();
+        if (start < 0) {
             return -1;
         }
+        if (start >= sz) {
+            start = sz - 1;
+        }
+        for (int i = start; i >= 0; --i) {
+            if (cs.charAt(i) == searchChar) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     /**
