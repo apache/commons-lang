b4b51a8fc9eed7274a045b2f3192d77782b0e4fa
Gary D. Gregory <ggregory@apache.org>
2014-04-30 21:49:35 +0000

    <action issue="LANG-1001" type="fix" dev="ggregory" due-to="Michael Osipov">ISO 8601 misspelled throughout the Javadocs</action>
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1591488 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/lang3/time/DurationFormatUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/time/DurationFormatUtilsTest.java
@@ -351,7 +351,7 @@ public class DurationFormatUtilsTest {
             new DurationFormatUtils.Token(DurationFormatUtils.s, 1),
             new DurationFormatUtils.Token(DurationFormatUtils.S, 1)}, DurationFormatUtils.lexx("yMdHmsS"));
 
-        // tests the ISO8601-like
+        // tests the ISO 8601-like
         assertArrayEquals(new DurationFormatUtils.Token[]{
             new DurationFormatUtils.Token(DurationFormatUtils.H, 1),
             new DurationFormatUtils.Token(new StringBuilder(":"), 1),
