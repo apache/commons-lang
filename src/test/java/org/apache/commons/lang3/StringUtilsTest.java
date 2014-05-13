08314e45e8f64c46faa02d476960b77957a47cad
thiagoh <thiagoh@gmail.com>
2014-05-13 16:21:47 -0300

    StringUtils add truncate method

--- src/test/java/org/apache/commons/lang3/StringUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/StringUtilsTest.java
@@ -1859,7 +1859,74 @@ public class StringUtilsTest {
         assertEquals("a..f", StringUtils.abbreviateMiddle("abcdef", "..", 4));
         assertEquals("ab.ef", StringUtils.abbreviateMiddle("abcdef", ".", 5));
     }
+    
+    @Test
+    public void testTruncate_StringInt() {
+        assertNull(StringUtils.truncate(null,  12));
+        assertEquals("", StringUtils.truncate("", 10));
+        assertEquals("", StringUtils.truncate("", 10));
+        assertEquals("abc", StringUtils.truncate("abcdefghij", 3));
+        assertEquals("abcdef", StringUtils.truncate("abcdefghij", 6));
+        
+        try {
+            @SuppressWarnings("unused")
+            final
+            String res = StringUtils.truncate("abcdefghij", -1);
+            fail("StringUtils.truncate expecting IllegalArgumentException");
+        } catch (final IllegalArgumentException ex) {
+            // empty
+        }
+        
+        assertEquals("abcdefghij", StringUtils.truncate("abcdefghijklmno", 10));
+        assertEquals("abcdefghijklmno", StringUtils.truncate("abcdefghijklmno", Integer.MAX_VALUE));
+        assertEquals("abcde", StringUtils.truncate("abcdefghijklmno", 5));
+        assertEquals("abc", StringUtils.truncate("abcdefghijklmno", 3));
+    }
 
+    @Test
+    public void testTruncate_StringIntInt() {
+        assertNull(StringUtils.truncate(null, 0, 12));
+        assertNull(StringUtils.truncate(null, 10, 12));
+        assertEquals("", StringUtils.truncate("", 0, 10));
+        assertEquals("", StringUtils.truncate("", 2, 10));
+        assertEquals("abc", StringUtils.truncate("abcdefghij", 0, 3));
+        assertEquals("fghij", StringUtils.truncate("abcdefghij", 5, 6));
+        
+        try {
+            @SuppressWarnings("unused")
+            final
+            String res = StringUtils.truncate("abcdefghij", 0, -1);
+            fail("StringUtils.truncate expecting IllegalArgumentException");
+        } catch (final IllegalArgumentException ex) {
+                // empty
+        }
+        
+        final String raspberry = "raspberry peach";
+        assertEquals("peach", StringUtils.truncate(raspberry, 10, 15));
+
+        assertEquals("abcdefghij", StringUtils.truncate("abcdefghijklmno", -1, 10));
+        assertEquals("abcdefghij", StringUtils.truncate("abcdefghijklmno", 0, 10));
+        assertEquals("abcdefghij", StringUtils.truncate("abcdefghijklmno", Integer.MIN_VALUE, 10));
+        assertEquals("abcdefghijklmno", StringUtils.truncate("abcdefghijklmno", Integer.MIN_VALUE, Integer.MAX_VALUE));
+        assertEquals("abcdefghijklmno", StringUtils.truncate("abcdefghijklmno", 0, Integer.MAX_VALUE));
+        assertEquals("bcdefghijk", StringUtils.truncate("abcdefghijklmno", 1, 10));
+        assertEquals("cdefghijkl", StringUtils.truncate("abcdefghijklmno", 2, 10));
+        assertEquals("defghijklm", StringUtils.truncate("abcdefghijklmno", 3, 10));
+        assertEquals("efghijklmn", StringUtils.truncate("abcdefghijklmno", 4, 10));
+        assertEquals("fghijklmno", StringUtils.truncate("abcdefghijklmno", 5, 10));
+        assertEquals("fghij", StringUtils.truncate("abcdefghijklmno", 5, 5));
+        assertEquals("fgh", StringUtils.truncate("abcdefghijklmno", 5, 3));
+        assertEquals("klm", StringUtils.truncate("abcdefghijklmno", 10, 3));
+        assertEquals("klmno", StringUtils.truncate("abcdefghijklmno", 10, Integer.MAX_VALUE));
+        assertEquals("n", StringUtils.truncate("abcdefghijklmno", 13, 1));
+        assertEquals("no", StringUtils.truncate("abcdefghijklmno", 13, Integer.MAX_VALUE));
+        assertEquals("o", StringUtils.truncate("abcdefghijklmno", 14, 1));
+        assertEquals("o", StringUtils.truncate("abcdefghijklmno", 14, Integer.MAX_VALUE));
+        assertEquals("", StringUtils.truncate("abcdefghijklmno", 15, 1));
+        assertEquals("", StringUtils.truncate("abcdefghijklmno", 15, Integer.MAX_VALUE));
+        assertEquals("", StringUtils.truncate("abcdefghijklmno", Integer.MAX_VALUE, Integer.MAX_VALUE));
+    }
+    
     //-----------------------------------------------------------------------
     @Test
     public void testDifference_StringString() {
