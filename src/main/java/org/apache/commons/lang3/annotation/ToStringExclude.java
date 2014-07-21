805b4ad229aa7ca6cc98a4fa16911dd28784364f
cad_fadorno <cad_fadorno@uolinc.com>
2014-07-21 11:41:17 -0300

    New Annotation @ToStringExclude use this annotation to builds a String excluding the annotated field

--- src/main/java/org/apache/commons/lang3/annotation/ToStringExclude.java
+++ src/main/java/org/apache/commons/lang3/annotation/ToStringExclude.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Use this annotation to builds a String excluding the given field.
+ * Use this annotation to builds a String excluding the annotated field.
  */
 @Retention(RUNTIME)
 @Target(ElementType.FIELD)
