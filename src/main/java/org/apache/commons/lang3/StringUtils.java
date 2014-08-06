04e1efff25b51355019c160c5c8dfcd7049dcb1e
Gary D. Gregory <ggregory@apache.org>
2014-08-06 23:46:30 +0000

    [LANG-1033] Add StringUtils.countMatches(CharSequence, char)
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1616372 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -5822,6 +5822,39 @@ public class StringUtils {
         return count;
     }
 
+    /**
+     * <p>Counts how many times the char appears in the given string.</p>
+     *
+     * <p>A {@code null} or empty ("") String input returns {@code 0}.</p>
+     *
+     * <pre>
+     * StringUtils.countMatches(null, *)       = 0
+     * StringUtils.countMatches("", *)         = 0
+     * StringUtils.countMatches("abba", 0)  = 0
+     * StringUtils.countMatches("abba", 'a')   = 2
+     * StringUtils.countMatches("abba", 'b')  = 2
+     * StringUtils.countMatches("abba", 'x') = 0
+     * </pre>
+     *
+     * @param str  the CharSequence to check, may be null
+     * @param ch  the char to count
+     * @return the number of occurrences, 0 if the CharSequence is {@code null}
+     * @since 3.4
+     */
+    public static int countMatches(final CharSequence str, final char ch) {
+        if (isEmpty(str)) {
+            return 0;
+        }
+        int count = 0;
+        // We could also call str.toCharArray() for faster look ups but that would generate more garbage.
+        for (int i = 0; i < str.length(); i++) {
+            if (ch == str.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     // Character Tests
     //-----------------------------------------------------------------------
     /**
