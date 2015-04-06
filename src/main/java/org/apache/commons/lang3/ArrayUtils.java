ef51b068aef14fa347710497cfb67aa9c6e65c0d
Benedikt Ritter <britter@apache.org>
2015-04-06 18:02:50 +0000

    Simplify isEmpty be reusing getLength
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/lang/trunk@1671623 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/lang3/ArrayUtils.java
+++ src/main/java/org/apache/commons/lang3/ArrayUtils.java
@@ -3453,7 +3453,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final Object[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     /**
@@ -3464,7 +3464,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final long[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     /**
@@ -3475,7 +3475,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final int[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     /**
@@ -3486,7 +3486,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final short[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     /**
@@ -3497,7 +3497,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final char[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     /**
@@ -3508,7 +3508,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final byte[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     /**
@@ -3519,7 +3519,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final double[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     /**
@@ -3530,7 +3530,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final float[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     /**
@@ -3541,7 +3541,7 @@ public class ArrayUtils {
      * @since 2.1
      */
     public static boolean isEmpty(final boolean[] array) {
-        return array == null || array.length == 0;
+        return getLength(array) == 0;
     }
 
     // ----------------------------------------------------------------------
