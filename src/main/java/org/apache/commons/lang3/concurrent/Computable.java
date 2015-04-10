d5d9ba6c3897125466f4f2d6034a429a50e5858e
jamessawle <jamessawle@hotmail.com>
2015-04-10 16:53:09 +0100

    LANG-740 - Initial implementation of the Memoizer class.

---
+++ src/main/java/org/apache/commons/lang3/concurrent/Computable.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.lang3.concurrent;
+
+/**
+ * <p>Definition of an interface for a wrapper around a calculation that takes a single parameter and returns a result.</p>
+ * <p/>
+ * <p>This interface allows for wrapping a calculation into a class so that it maybe passed around an application.</p>
+ *
+ * @param <A> the type of the input to the calculation
+ * @param <V> the type of the output of the calculation
+ */
+public interface Computable<A, V> {
+
+	/**
+	 * This method carries out the given operation with the provided argument.
+	 *
+	 * @param arg the argument for the calculation
+	 * @return the result of the calculation
+	 * @throws InterruptedException thrown if the calculation is interrupted
+	 */
+	V compute(final A arg) throws InterruptedException;
+}
