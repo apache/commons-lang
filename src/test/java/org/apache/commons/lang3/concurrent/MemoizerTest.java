b65aaca6c6131e08511ad06e5c07347475b869e8
jamessawle <jamessawle@hotmail.com>
2015-04-15 11:26:05 +0100

    LANG-740 - Add tests for core flow through the Memoizer class

---
+++ src/test/java/org/apache/commons/lang3/concurrent/MemoizerTest.java
@@ -0,0 +1,90 @@
+package org.apache.commons.lang3.concurrent;
+
+import org.easymock.EasyMockRunner;
+import org.easymock.Mock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(EasyMockRunner.class)
+public class MemoizerTest {
+
+	@Mock
+	private Computable<Integer, Integer> computable;
+
+	@Test
+	public void testOnlyCallComputableOnceIfDoesNotThrowException() throws Exception {
+		Integer input = 1;
+		Memoizer<Integer, Integer> memoizer = new Memoizer<Integer, Integer>(computable);
+		expect(computable.compute(input)).andReturn(input);
+		replay(computable);
+
+		assertEquals("Should call computable first time", input, memoizer.compute(input));
+		assertEquals("Should not call the computable the second time", input, memoizer.compute(input));
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testDefaultBehaviourNotToRecalculateExecutionExceptions() throws Exception {
+		Integer input = 1;
+		Integer answer = 3;
+		Memoizer<Integer, Integer> memoizer = new Memoizer<Integer, Integer>(computable);
+		InterruptedException interruptedException = new InterruptedException();
+		expect(computable.compute(input)).andThrow(interruptedException);
+		replay(computable);
+
+		try {
+			memoizer.compute(input);
+			fail();
+		}
+		catch (Throwable ex) {
+			//Should always be thrown the first time
+		}
+
+		memoizer.compute(input);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testDoesNotRecalculateWhenSetToFalse() throws Exception {
+		Integer input = 1;
+		Integer answer = 3;
+		Memoizer<Integer, Integer> memoizer = new Memoizer<Integer, Integer>(computable, false);
+		InterruptedException interruptedException = new InterruptedException();
+		expect(computable.compute(input)).andThrow(interruptedException);
+		replay(computable);
+
+		try {
+			memoizer.compute(input);
+			fail();
+		}
+		catch (Throwable ex) {
+			//Should always be thrown the first time
+		}
+
+		memoizer.compute(input);
+	}
+
+	@Test
+	public void testDoesRecalculateWhenSetToTrue() throws Exception {
+		Integer input = 1;
+		Integer answer = 3;
+		Memoizer<Integer, Integer> memoizer = new Memoizer<Integer, Integer>(computable, true);
+		InterruptedException interruptedException = new InterruptedException();
+		expect(computable.compute(input)).andThrow(interruptedException).andReturn(answer);
+		replay(computable);
+
+		try {
+			memoizer.compute(input);
+			fail();
+		}
+		catch (Throwable ex) {
+			//Should always be thrown the first time
+		}
+
+		assertEquals(answer, memoizer.compute(input));
+	}
+
+}
