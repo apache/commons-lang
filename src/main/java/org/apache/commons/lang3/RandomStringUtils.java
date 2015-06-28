dfef87d0936447f9348b4a050b2ddaf17664608a
Caleb Cushing <xenoterracide@gmail.com>
2015-06-28 02:08:08 -0500

    consistently refer to Java Regular Expression classes

--- src/main/java/org/apache/commons/lang3/RandomStringUtils.java
+++ src/main/java/org/apache/commons/lang3/RandomStringUtils.java
@@ -154,7 +154,7 @@ public class RandomStringUtils {
     /**
      * <p>Creates a random string whose length is the number of characters specified.</p>
      *
-     * <p>Characters will be chosen from the set of characters which match the POSIX [:graph:] regular expression.</p>
+     * <p>Characters will be chosen from the set of \p{Graph} characters.</p>
      *
      * @param count  the length of random string to create
      * @return the random string
@@ -206,7 +206,7 @@ public class RandomStringUtils {
     /**
      * <p>Creates a random string whose length is the number of characters specified.</p>
      *
-     * <p>Characters will be chosen from the set of characters which match the POSIX [:print:] regular expression.</p>
+     * <p>Characters will be chosen from the set of \p{Print} characters.</p>
      *
      * @param count  the length of random string to create
      * @return the random string
