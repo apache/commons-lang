0a203baf42fa8d7399ddf9be4beae4254108624a
kaching88 <waved@o2.pl>
2015-07-14 01:54:35 +0200

    [LANG-1120] Bugfix: StringUtils#stripAccents Ł and ł.

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -753,10 +753,23 @@ public class StringUtils {
             return null;
         }
         final Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");//$NON-NLS-1$
-        final String decomposed = Normalizer.normalize(input, Normalizer.Form.NFD);
+        StringBuilder decomposed = new StringBuilder(Normalizer.normalize(input, Normalizer.Form.NFD));
+        convertRemainingAccentCharacters(decomposed);     
         // Note that this doesn't correctly remove ligatures...
         return pattern.matcher(decomposed).replaceAll("");//$NON-NLS-1$
     }
+    
+	private static void convertRemainingAccentCharacters(StringBuilder decomposed) {
+		for (int i = 0; i < decomposed.length(); i++) {
+        	if (decomposed.charAt(i) == '\u0141') {
+        		decomposed.deleteCharAt(i);
+        		decomposed.insert(i, 'L');
+        	} else if (decomposed.charAt(i) == '\u0142') {
+        		decomposed.deleteCharAt(i);
+        		decomposed.insert(i, 'l');
+        	}
+		}
+	}
 
     // Equals
     //-----------------------------------------------------------------------
