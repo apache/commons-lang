6849dfc8a660bf8c95354520a2fa1283c9b3aa43
Sebb <sebb@apache.org>
2015-08-10 22:42:21 +0100

    LANG-1162 StringUtils#equals fails with Index OOBE on non-Strings with identical leading prefix

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -788,10 +788,13 @@ public class StringUtils {
         if (cs1 == null || cs2 == null) {
             return false;
         }
+        if (cs1.length() != cs2.length()) {
+            return false;
+        }
         if (cs1 instanceof String && cs2 instanceof String) {
             return cs1.equals(cs2);
         }
-        return CharSequenceUtils.regionMatches(cs1, false, 0, cs2, 0, Math.max(cs1.length(), cs2.length()));
+        return CharSequenceUtils.regionMatches(cs1, false, 0, cs2, 0, cs1.length());
     }
 
     /**
