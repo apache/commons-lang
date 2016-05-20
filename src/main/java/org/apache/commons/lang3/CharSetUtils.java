afedbae8aca69a092d65766640361801bf2b5d9b
pascalschumacher <pascalschumacher@gmx.net>
2016-05-20 20:04:30 +0200

    LANG-1206: Improve CharSetUtils.squeeze() performance (closes #147)
    patch by Mohammed Alfallaj

--- src/main/java/org/apache/commons/lang3/CharSetUtils.java
+++ src/main/java/org/apache/commons/lang3/CharSetUtils.java
@@ -68,13 +68,26 @@ public class CharSetUtils {
         final StringBuilder buffer = new StringBuilder(str.length());
         final char[] chrs = str.toCharArray();
         final int sz = chrs.length;
-        char lastChar = ' ';
+        char lastChar = chrs[0];
         char ch = ' ';
-        for (int i = 0; i < sz; i++) {
+        Character inChars = null;
+        Character notInChars = null;
+        buffer.append(lastChar);
+        for (int i = 1; i < sz; i++) {
             ch = chrs[i];
-            // Compare with contains() last for performance.
-            if (ch == lastChar && i != 0 && chars.contains(ch)) {
-                continue;
+            if (ch == lastChar) {
+                if ((inChars != null) && (ch == inChars)) {
+                    continue;
+                } else {
+                    if ((notInChars == null) || (ch != notInChars)) {
+                        if (chars.contains(ch)) {
+                            inChars = ch;
+                            continue;
+                        } else {
+                            notInChars = ch;
+                        }
+                    }
+                }
             }
             buffer.append(ch);
             lastChar = ch;
