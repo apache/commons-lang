2cd3f0f10a797b744509903b8eca4fd2fd4ed6f6
Gary Gregory <garydgregory@gmail.com>
2016-11-28 13:27:25 -0800

    Re-implement to avoid previous license.
    Rewrite all Javadocs to be much simpler but we are open to more text.

--- src/main/java/org/apache/commons/lang3/concurrent/annotation/Immutable.java
+++ src/main/java/org/apache/commons/lang3/concurrent/annotation/Immutable.java
@@ -22,31 +22,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/*
- * Copyright (c) 2005 Brian Goetz
- * Released under the Creative Commons Attribution License
- *   (http://creativecommons.org/licenses/by/2.5)
- * Official home: http://www.jcip.net
- *
- * Any republication or derived work distributed in source code form
- * must include this copyright and license notice.
- */
-
 /**
- * Immutable
- * 
- * The class to which this annotation is applied is immutable. This means that
- * its state cannot be seen to change by callers. Of necessity this means that
- * all public fields are final, and that all public final reference fields refer
- * to other immutable objects, and that methods do not publish references to any
- * internal state which is mutable by implementation even if not by design.
- * Immutable objects may still have internal mutable state for purposes of
- * performance optimization; some state variables may be lazily computed, so
- * long as they are computed from immutable state and that callers cannot tell
- * the difference.
- * 
- * Immutable objects are inherently thread-safe; they may be passed between
- * threads or published without synchronization.
+ * Describes a type as immutable.
  */
 @Documented
 @Target(ElementType.TYPE)
--- src/main/java/org/apache/commons/lang3/concurrent/annotation/NotThreadSafe.java
+++ src/main/java/org/apache/commons/lang3/concurrent/annotation/NotThreadSafe.java
@@ -22,23 +22,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/*
- * Copyright (c) 2005 Brian Goetz
- * Released under the Creative Commons Attribution License
- *   (http://creativecommons.org/licenses/by/2.5)
- * Official home: http://www.jcip.net
- *
- * Any republication or derived work distributed in source code form
- * must include this copyright and license notice.
- */
-
 /**
- * NotThreadSafe
- * 
- * The class to which this annotation is applied is not thread-safe. This
- * annotation primarily exists for clarifying the non-thread-safety of a class
- * that might otherwise be assumed to be thread-safe, despite the fact that it
- * is a bad idea to assume a class is thread-safe without good reason.
+ * Describes this type as not thread-safe.
  * 
  * @see ThreadSafe
  */
