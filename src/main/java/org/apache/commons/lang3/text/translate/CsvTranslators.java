99073efdf33d0ece00ed4b5c2aa07b70ea5ae334
Jaroslaw Strzelecki <jarst@users.noreply.github.com>
2017-01-04 23:47:44 +0100

    CSV translators - minor tweaks

--- src/main/java/org/apache/commons/lang3/text/translate/CsvTranslators.java
+++ src/main/java/org/apache/commons/lang3/text/translate/CsvTranslators.java
@@ -14,10 +14,11 @@ public class CsvTranslators {
     private static final char CSV_DELIMITER = ',';
     private static final char CSV_QUOTE = '"';
     private static final String CSV_QUOTE_STR = String.valueOf(CSV_QUOTE);
+    private static final String CSV_ESCAPED_QUOTE_STR = CSV_QUOTE_STR + CSV_QUOTE_STR;
     private static final char[] CSV_SEARCH_CHARS =
             new char[] {CSV_DELIMITER, CSV_QUOTE, CharUtils.CR, CharUtils.LF};
 
-    private CsvTranslators() {}
+    private CsvTranslators() { }
 
     /**
      * Translator for escaping Comma Separated Values.
@@ -26,11 +27,13 @@ public class CsvTranslators {
 
         @Override
         void translateWhole(final CharSequence input, final Writer out) throws IOException {
-            if (StringUtils.containsNone(input.toString(), CSV_SEARCH_CHARS)) {
-                out.write(input.toString());
+            final String inputSting = input.toString();
+            if (StringUtils.containsNone(inputSting, CSV_SEARCH_CHARS)) {
+                out.write(inputSting);
             } else {
+                // input needs quoting
                 out.write(CSV_QUOTE);
-                out.write(StringUtils.replace(input.toString(), CSV_QUOTE_STR, CSV_QUOTE_STR + CSV_QUOTE_STR));
+                out.write(StringUtils.replace(inputSting, CSV_QUOTE_STR, CSV_ESCAPED_QUOTE_STR));
                 out.write(CSV_QUOTE);
             }
         }
@@ -43,6 +46,7 @@ public class CsvTranslators {
 
         @Override
         void translateWhole(final CharSequence input, final Writer out) throws IOException {
+            // is input not quoted?
             if (input.charAt(0) != CSV_QUOTE || input.charAt(input.length() - 1) != CSV_QUOTE) {
                 out.write(input.toString());
                 return;
@@ -53,7 +57,7 @@ public class CsvTranslators {
 
             if (StringUtils.containsAny(quoteless, CSV_SEARCH_CHARS)) {
                 // deal with escaped quotes; ie) ""
-                out.write(StringUtils.replace(quoteless, CSV_QUOTE_STR + CSV_QUOTE_STR, CSV_QUOTE_STR));
+                out.write(StringUtils.replace(quoteless, CSV_ESCAPED_QUOTE_STR, CSV_QUOTE_STR));
             } else {
                 out.write(input.toString());
             }
