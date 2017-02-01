ab25f67348d4885620df86497889c1826f013a73
peleghila <peleghila@users.noreply.github.com>
2017-02-01 14:01:18 +0200

    Fix copy-paste error in CharUtilsTest#testIsAsciiPrintable_char (closes #230)
    Line 238 has copy paste error from testIsAscii_char

--- src/test/java/org/apache/commons/lang3/CharUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/CharUtilsTest.java
@@ -235,7 +235,7 @@ public class CharUtilsTest {
         assertTrue(CharUtils.isAsciiPrintable('3'));
         assertTrue(CharUtils.isAsciiPrintable('-'));
         assertFalse(CharUtils.isAsciiPrintable('\n'));
-        assertFalse(CharUtils.isAscii(CHAR_COPY));
+        assertFalse(CharUtils.isAsciiPrintable(CHAR_COPY));
        
         for (int i = 0; i < 196; i++) {
             if (i >= 32 && i <= 126) {
