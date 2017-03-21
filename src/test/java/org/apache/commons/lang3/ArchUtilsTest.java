a9c6f59b005c8e36980165b98f5dedbc4329438e
Tomschi <tomschi@users.noreply.github.com>
2017-03-21 08:24:00 +0100

    #LANG-1313 remove wildcard import

--- src/test/java/org/apache/commons/lang3/ArchUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/ArchUtilsTest.java
@@ -19,7 +19,10 @@ package org.apache.commons.lang3;
 import org.apache.commons.lang3.arch.Processor;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Test class for {@link ArchUtils}.
