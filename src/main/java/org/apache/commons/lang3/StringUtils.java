b5260f809940b0626d986889ca3bcbc5ffc68cad
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2017-07-09 22:20:39 +0100

    Simplify boolean expression in StringUtils
    This eliminates a warning reported by IntelliJ IDEA when running with
    default settings.
    
    There is no need to check that `startIndex != -1` because `startIndex`
    is always initialized to 0 and is never mutated afterwards.

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -9212,7 +9212,7 @@ public class StringUtils {
         if (str.charAt(0) == wrapChar && str.charAt(str.length() - 1) == wrapChar) {
             final int startIndex = 0;
             final int endIndex = str.length() - 1;
-            if (startIndex != -1 && endIndex != -1) {
+            if (endIndex != -1) {
                 return str.substring(startIndex + 1, endIndex);
             }
         }
