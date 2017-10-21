apache/commons-lang

commit 6ea2fc8d38e035bafaa92c7d3b007be38c2e9000

[LANG-1360] Add methods to ObjectUtils to get various forms of class names in a null-safe manner

src/main/java/org/apache/commons/lang3/ObjectUtils.java
@@ -1033,4 +1033,36 @@ public class ObjectUtils {
         return v;
     }
 
+    /**
+     * Gets the class name of the given object.
+     * 
+     * @param object the object to query, may be null
+     * @return the given object's class name or null if the object is null
+     * @since 3.7
+     */
+    public static String getClassName(final Object object) {
+        return object == null ? null : object.getClass().getName();
+    }
+
+    /**
+     * Gets the class simple name of the given object.
+     * 
+     * @param object the object to query, may be null
+     * @return the given object's class simple name or null if the object is null
+     * @since 3.7
+     */
+    public static String getClassSimpleName(final Object object) {
+        return object == null ? null : object.getClass().getSimpleName();
+    }
+
+    /**
+     * Gets the class canonical name of the given object.
+     * 
+     * @param object the object to query, may be null
+     * @return the given object's class canonical name or null if the object is null
+     * @since 3.7
+     */
+    public static String getClassCanonicalName(final Object object) {
+        return object == null ? null : object.getClass().getCanonicalName();
+    }
 }

src/test/java/org/apache/commons/lang3/ObjectUtilsTest.java
@@ -38,6 +38,7 @@ import java.util.List;
 import org.apache.commons.lang3.exception.CloneFailedException;
 import org.apache.commons.lang3.mutable.MutableObject;
 import org.apache.commons.lang3.text.StrBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -665,5 +666,37 @@ public class ObjectUtilsTest {
             return o1.toString().compareTo(o2.toString());
         }
 
+    }
+
+    /**
+     * @since 3.7
+     */
+    @Test
+    public void testGetClassName() {
+        Assert.assertNull(ObjectUtils.getClassName(null));
+        Assert.assertEquals("java.lang.String", ObjectUtils.getClassName(new String()));
+        Assert.assertEquals("org.apache.commons.lang3.ObjectUtilsTest$CloneableString",
+                ObjectUtils.getClassName(new CloneableString("test")));
+    }
+
+    /**
+     * @since 3.7
+     */
+    @Test
+    public void testGetSimpleName() {
+        Assert.assertNull(ObjectUtils.getClassSimpleName(null));
+        Assert.assertEquals("String", ObjectUtils.getClassSimpleName(new String()));
+        Assert.assertEquals("CloneableString", ObjectUtils.getClassSimpleName(new CloneableString("test")));
+    }
+
+    /**
+     * @since 3.7
+     */
+    @Test
+    public void testGetCanonicalName() {
+        Assert.assertNull(ObjectUtils.getClassCanonicalName(null));
+        Assert.assertEquals("java.lang.String", ObjectUtils.getClassCanonicalName(new String()));
+        Assert.assertEquals("org.apache.commons.lang3.ObjectUtilsTest.CloneableString",
+                ObjectUtils.getClassCanonicalName(new CloneableString("test")));
     }
 }

commit f0930aa1512a408635d2286c01d49523b1234db7

Remove ObjectUtils' trailing white spaces
Commit 6ea2fc8 inadvertently introduced trailing white spaces in
ObjectUtils' code, thus breaking the Checkstyle validation.

This patch removes these redundant TWS in order to allow the build to
pass.

src/main/java/org/apache/commons/lang3/ObjectUtils.java
@@ -1035,7 +1035,7 @@ public class ObjectUtils {
 
     /**
      * Gets the class name of the given object.
-     * 
+     *
      * @param object the object to query, may be null
      * @return the given object's class name or null if the object is null
      * @since 3.7
@@ -1046,7 +1046,7 @@ public class ObjectUtils {
 
     /**
      * Gets the class simple name of the given object.
-     * 
+     *
      * @param object the object to query, may be null
      * @return the given object's class simple name or null if the object is null
      * @since 3.7
@@ -1057,7 +1057,7 @@ public class ObjectUtils {
 
     /**
      * Gets the class canonical name of the given object.
-     * 
+     *
      * @param object the object to query, may be null
      * @return the given object's class canonical name or null if the object is null
      * @since 3.7