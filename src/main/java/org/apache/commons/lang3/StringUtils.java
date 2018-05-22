96cb498f9bb88b534e49f9b8301c9fe7113b2dfe
Gary Gregory <garydgregory@gmail.com>
2018-05-22 08:55:54 -0600

    [LANG-1290] StringUtils.join() with support for List<?> with configurable start/end indices. Reuse constant.

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -4614,7 +4614,7 @@ public class StringUtils {
         }
         final Object first = iterator.next();
         if (!iterator.hasNext()) {
-            return Objects.toString(first, "");
+            return Objects.toString(first, EMPTY);
         }
 
         // two or more elements
