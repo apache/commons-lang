96f3ab841096b6a480b4cae99bb05aebce1ff92d
MarkDacek <mark.dacek@richmond.edu>
2018-07-08 19:20:48 -0400

    LANG-1402: comment proofread

--- src/main/java/org/apache/commons/lang3/ArrayUtils.java
+++ src/main/java/org/apache/commons/lang3/ArrayUtils.java
@@ -8675,7 +8675,7 @@ public class ArrayUtils {
 
     /**
      * Gets an element from the array if the array is non-null and appropriately long, otherwise returns null
-     * @param <T> the component type of the array
+     * @param <T> the component type of the array, may be null
      * @param array   the array holding the desired element
      * @param index  the index of the element in the array
      * @return The element in the array at the index, or null if it is ill-formatted
@@ -8688,10 +8688,10 @@ public class ArrayUtils {
     /**
      * Gets an element from the array if the array is non-null and appropriately long, otherwise returns the specified value
      * @param <T> the component type of the array
-     * @param array   the array holding the desired element
+     * @param array   the array holding the desired element, may be null
      * @param index  the index of the element in the array
      * @param defaultReturn the object to be returned if the array is null or shorter than the index
-     * @return The element in the array at the specified index, or the given argument if it is ill-formatted
+     * @return The element in the array at the specified index, or the given argument if it the array is not sufficiently long for the index. May return null if the array contains null
      * @since 3.8
      */
     public static <T> T get(T[] array, int index, T defaultReturn){
@@ -8709,8 +8709,8 @@ public class ArrayUtils {
     /**
      * Returns whether a given array can safely be accessed at the given index.
      * @param <T> the component type of the array
-     * @param array   the array holding the desired element
-     * @param index  the index of the element in the array
+     * @param array the array to inspect
+     * @param index the index of the array to be inspected
      * @return Whether the given index is safely-accessible in the given array
      * @since 3.8
      */
