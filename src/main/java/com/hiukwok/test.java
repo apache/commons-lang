068a250848de261dec57ffcb50224666f36a3d59
Hiu Kwok <hiufkwok@gmail.com>
2018-09-15 14:11:39 +0800

    Update example

--- src/main/java/com/hiukwok/test.java
+++ src/main/java/com/hiukwok/test.java
@@ -9,12 +9,13 @@ public class test {
 
     public static void main(String[] args) {
 
-        String text = "\u0130x\u0130x";
-        String searchString = "i";
+        String text = "\u0130x";
+        String searchString = "I";
         String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
 //        As u0130 got normalized to two char, which one of it is lower case of [i].
 //        Which would lead to mismatch happen.
         System.out.println("Before: " + normalized);
+        System.out.println(normalized + " -> [" + normalized.charAt(0) + "," + normalized.charAt(1) +"," + normalized.charAt(2) + "]"  );
 //        Expected: Stay un-change, it doesn't
         System.out.println("After: " + StringUtils.replaceIgnoreCase(text, searchString, ""));
     }
