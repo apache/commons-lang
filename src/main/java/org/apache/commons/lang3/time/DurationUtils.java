8b43fb4de1cfd35cc576807533536a8a7465d666
Aleksander Sciborek <aleksandersciborek@gmail.com>
2018-10-14 19:05:51 +0200

    [LANG_1407] add the license headers to DurationUtils and DurationUtilsTests

--- src/main/java/org/apache/commons/lang3/time/DurationUtils.java
+++ src/main/java/org/apache/commons/lang3/time/DurationUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.commons.lang3.time;
 
 import java.time.Duration;
@@ -104,7 +120,7 @@ public class DurationUtils {
      * <p>For the duration of 1h it should return 1(hour) and the duration of 1h 10 min  it should return 2(hours)</p>
      *
      * @param duration the given duration from which the quantity of hours will be calculated
-     * @return the rounded up quantity of hours 
+     * @return the rounded up quantity of hours
      * @throws IllegalArgumentException if the given duration is null
      * @since 3.9
      */
