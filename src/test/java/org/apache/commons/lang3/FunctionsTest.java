95926eb5d97e9f89718993bbed0a0f70da58a68f
Allon Mureinik <mureinik@gmail.com>
2019-02-08 19:32:39 +0200

    FunctionsTest imports
    Clean up FunctionsTest's imports as per the project's Checkstyle
    rules.

--- src/test/java/org/apache/commons/lang3/FunctionsTest.java
+++ src/test/java/org/apache/commons/lang3/FunctionsTest.java
@@ -16,15 +16,18 @@
  */
 package org.apache.commons.lang3;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.lang.reflect.UndeclaredThrowableException;
 
-import org.apache.commons.lang3.Functions.FailableBiConsumer;
 import org.apache.commons.lang3.Functions.FailableConsumer;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class FunctionsTest {
 	public static class SomeException extends Exception {
