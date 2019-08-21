fc4eeb94ce4f9a558a2815bb1198e339af66e0fc
yuyu.zhao <yuyu.zhao@ipinyou.com>
2019-08-21 18:22:36 +0800

    add if '{}'s

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -8025,8 +8025,9 @@ public class StringUtils {
         private int size;
 
         private static int hugeCapacity(int minCapacity) {
-            if (minCapacity < 0) // overflow
+            if (minCapacity < 0) { // overflow
                 throw new OutOfMemoryError();
+            }
             return (minCapacity > MAX_ARRAY_SIZE)
                     ? Integer.MAX_VALUE
                     : MAX_ARRAY_SIZE;
@@ -8055,8 +8056,9 @@ public class StringUtils {
          * which helps when add(E) is called in a C1-compiled loop.
          */
         private void add(String e, Object[] elementData, int s) {
-            if (s == elementData.length)
+            if (s == elementData.length) {
                 elementData = grow(size + 1);
+            }
             elementData[s] = e;
             size = s + 1;
         }
@@ -8087,10 +8089,12 @@ public class StringUtils {
             int oldCapacity = elementData.length;
             int newCapacity = oldCapacity + (oldCapacity >> 1);
             if (newCapacity - minCapacity <= 0) {
-                if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA)
+                if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
                     return Math.max(DEFAULT_CAPACITY, minCapacity);
-                if (minCapacity < 0) // overflow
+                }
+                if (minCapacity < 0) {// overflow
                     throw new OutOfMemoryError();
+                }
                 return minCapacity;
             }
             return (newCapacity - MAX_ARRAY_SIZE <= 0)
