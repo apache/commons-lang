9a3f74709c56c74593bc8077176d559a586e7572
Gary Gregory <gardgregory@gmail.com>
2019-09-11 14:55:33 -0400

    Remove trailing white spaces on all lines.

--- src/test/java/org/apache/commons/lang3/StringUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/StringUtilsTest.java
@@ -1687,7 +1687,7 @@ public class StringUtilsTest {
 
         // StringUtils.removeIgnoreCase("\u0130x", "x") = "\u0130"
         assertEquals("\u0130", StringUtils.removeIgnoreCase("\u0130x", "x"));
-        
+
         // LANG-1453
         StringUtils.removeIgnoreCase("İa", "a");
     }
