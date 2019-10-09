00f56ea420e65e55c03b428daa1512bd2408923c
Peter_Verhas <peter@verhas.com>
2019-10-09 15:59:46 +0200

    LANG-1480 imports rearranged

--- src/test/java/org/apache/commons/lang3/ClassUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/ClassUtilsTest.java
@@ -39,7 +39,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.asser tNotSame;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
