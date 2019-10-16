7e78be392aaddc8b6f7179697b620c61511efacc
Peter_Verhas <peter@verhas.com>
2019-10-16 13:06:44 +0200

    ArrayUtils change reverted

--- src/main/java/org/apache/commons/lang3/ArrayUtils.java
+++ src/main/java/org/apache/commons/lang3/ArrayUtils.java
@@ -4803,7 +4803,7 @@ public class ArrayUtils {
         }
         final boolean[] result = new boolean[array.length];
         for (int i = 0; i < array.length; i++) {
-            result[i] = array[i];
+            result[i] = array[i].booleanValue();
         }
         return result;
     }
