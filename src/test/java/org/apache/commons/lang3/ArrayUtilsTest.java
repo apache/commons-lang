a4c11de8f179503cec8eb2781bdaadb0b2f84864
Apirom Na Nakorn <apirom9@gmail.com>
2019-10-25 17:14:43 +0700

    Remove unused import statement

--- src/test/java/org/apache/commons/lang3/ArrayUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/ArrayUtilsTest.java
@@ -33,7 +33,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
