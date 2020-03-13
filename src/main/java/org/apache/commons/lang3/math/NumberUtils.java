d08fb27f1afe2fb5bf3e94259a591a0b76aa35da
Pengyu Nie <prodigy.sov@gmail.com>
2020-03-13 18:41:45 -0500

    LANG-1527: Remove an redundant argument check in NumberUtils.

--- src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -987,13 +987,6 @@ public class NumberUtils {
         if (StringUtils.isBlank(str)) {
             throw new NumberFormatException("A blank string is not a valid number");
         }
-        if (str.trim().startsWith("--")) {
-            // this is protection for poorness in java.lang.BigDecimal.
-            // it accepts this as a legal value, but it does not appear
-            // to be in specification of class. OS X Java parses it to
-            // a wrong value.
-            throw new NumberFormatException(str + " is not a valid number.");
-        }
         return new BigDecimal(str);
     }
 
