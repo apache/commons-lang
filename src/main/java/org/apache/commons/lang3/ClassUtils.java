550da9ce4e41432945d3abdac3ea982942200539
Isira Seneviratne <isirasen96@gmail.com>
2020-04-25 14:53:43 +0530

    Combine two if conditions in isAssignable().

--- src/main/java/org/apache/commons/lang3/ClassUtils.java
+++ src/main/java/org/apache/commons/lang3/ClassUtils.java
@@ -902,22 +902,13 @@ public class ClassUtils {
                 return Float.TYPE.equals(toClass)
                     || Double.TYPE.equals(toClass);
             }
-            if (Boolean.TYPE.equals(cls)) {
-                return false;
-            }
-            if (Double.TYPE.equals(cls)) {
+            if (Boolean.TYPE.equals(cls) || Double.TYPE.equals(cls)) {
                 return false;
             }
             if (Float.TYPE.equals(cls)) {
                 return Double.TYPE.equals(toClass);
             }
-            if (Character.TYPE.equals(cls)) {
-                return Integer.TYPE.equals(toClass)
-                    || Long.TYPE.equals(toClass)
-                    || Float.TYPE.equals(toClass)
-                    || Double.TYPE.equals(toClass);
-            }
-            if (Short.TYPE.equals(cls)) {
+            if (Character.TYPE.equals(cls) || Short.TYPE.equals(cls)) {
                 return Integer.TYPE.equals(toClass)
                     || Long.TYPE.equals(toClass)
                     || Float.TYPE.equals(toClass)
