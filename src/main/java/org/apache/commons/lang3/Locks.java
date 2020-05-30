apache/commons-lang

commit c141bc961cb5cb84c47a21c3a5b0f15ab0035728

Adding the Locks class.

src/main/java/org/apache/commons/lang3/Locks.java
@@ -1,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.lang3;
+
+import java.util.Objects;
+import java.util.concurrent.locks.StampedLock;
+
+import org.apache.commons.lang3.Functions.FailableConsumer;
+import org.apache.commons.lang3.Functions.FailableFunction;
+
+
+/** Utility class for working with {@link java.util.concurrent.locks.Lock locked objects}. Locked objects are an
+ * alternative to synchronization.
+ * 
+ * Locking is preferable, if there is a distinction between read access (multiple threads may have read
+ * access concurrently), and write access (only one thread may have write access at any given time.
+ * In comparison, synchronization doesn't support read access, because synchronized access is exclusive.
+ * 
+ * Using this class is fairly straightforward:
+ * <ol>
+ *   <li>While still in single thread mode, create an instance of {@link Locks.Lock} by calling
+ *     {@link #lock(Object)}, passing the object, which needs to be locked. Discard all
+ *     references to the locked object. Instead, use references to the lock.</li>
+ *   <li>If you want to access the locked object, create a {@link FailableConsumer}. The consumer
+ *     will receive the locked object as a parameter. For convenience, the consumer may be
+ *     implemented as a Lamba. Then invoke {@link Locks.Lock#runReadLocked(FailableConsumer)},
+ *     or {@link Locks.Lock#runWriteLocked(FailableConsumer)}, passing the consumer.</li>
+ *   <li>As an alternative, if you need to produce a result object, you may use a
+ *     {@link FailableFunction}. This function may also be implemented as a Lambda. To
+ *     have the function executed, invoke {@link Locks.Lock#callReadLocked(FailableFunction)}, or
+ *     {@link Locks.Lock#callWriteLocked(FailableFunction)}.</li>
+ * </ol>
+ *
+ * Example: A thread safe logger class.
+ * <pre>
+ *   public class SimpleLogger {
+ *     private final Lock&lt;PrintStream&gt; lock;
+ *
+ *     public SimpleLogger(OutputStream out) {
+ *         PrintStream ps = new Printstream(out);
+ *         lock = Locks.lock(ps);
+ *     }
+ * 
+ *     public void log(String message) {
+ *         lock.runWriteLocked((ps) -&gt; ps.println(message));
+ *     }
+ *
+ *     public void log(byte[] buffer) {
+ *         lock.runWriteLocked((ps) -&gt; { ps.write(buffer); ps.println(); });
+ *     }
+ * </pre> 
+ */
+public class Locks {
+	public static class Lock<O extends Object> {
+		private final O lockedObject;
+		private final StampedLock lock = new StampedLock();
+
+		public Lock(O lockedObject) {
+			this.lockedObject = Objects.requireNonNull(lockedObject, "Locked Object");
+		}
+
+		public void runReadLocked(FailableConsumer<O,?> consumer) {
+			runLocked(lock.readLock(), consumer);
+		}
+
+		public void runWriteLocked(FailableConsumer<O,?> consumer) {
+			runLocked(lock.writeLock(), consumer);
+		}
+
+		public <T> T callReadLocked(FailableFunction<O,T,?> function) {
+			return callLocked(lock.readLock(), function);
+		}
+
+		public <T> T callWriteLocked(FailableFunction<O,T,?> function) {
+			return callLocked(lock.writeLock(), function);
+		}
+
+		protected void runLocked(long stamp, FailableConsumer<O,?> consumer) {
+			try {
+				consumer.accept(lockedObject);
+			} catch (Throwable t) {
+				throw Functions.rethrow(t);
+			} finally {
+				lock.unlock(stamp);
+			}
+		}
+
+		protected <T> T callLocked(long stamp, FailableFunction<O,T,?> function) {
+			try {
+				return function.apply(lockedObject);
+			} catch (Throwable t) {
+				throw Functions.rethrow(t);
+			} finally {
+				lock.unlock(stamp);
+			}
+		}
+	}
+
+	public static <O extends Object> Locks.Lock<O> lock(O object) {
+		return new Locks.Lock<O>(object);
+	}
+}

src/test/java/org/apache/commons/lang3/LocksTest.java
@@ -1,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.lang3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.apache.commons.lang3.Functions.FailableConsumer;
+import org.apache.commons.lang3.Locks.Lock;
+import org.junit.jupiter.api.Test;
+
+class LocksTest {
+	@Test
+	void testReadLock() throws Exception {
+		final long DELAY=3000;
+		final boolean[] booleanValues = new boolean[10];
+		final Lock<boolean[]> lock = Locks.lock(booleanValues);
+		final boolean[] runningValues = new boolean[10];
+
+		final long startTime = System.currentTimeMillis();
+		for (int i = 0;  i < booleanValues.length;  i++) {
+			final int index = i;
+			final FailableConsumer<boolean[],?> consumer = (b) -> {
+				b[index] = false;
+				Thread.sleep(DELAY);
+				b[index] = true;
+				modify(runningValues, index, false);
+			};
+			final Thread t = new Thread(() -> lock.runReadLocked(consumer));
+			modify(runningValues, i, true);
+			t.start();
+		}
+		while (someValueIsTrue(runningValues)) {
+			Thread.sleep(100);
+		}
+		final long endTime = System.currentTimeMillis();
+		for (int i = 0;  i < booleanValues.length;  i++) {
+			assertTrue(booleanValues[i]);
+		}
+		// If our threads would be running in exclusive mode, then we'd need
+		// at least DELAY milliseconds for each.
+		assertTrue((endTime-startTime) < booleanValues.length*DELAY);
+	}
+
+	protected void modify(boolean[] booleanArray, int offset, boolean value) {
+		synchronized(booleanArray) {
+			booleanArray[offset] = value;
+		}
+	}
+	protected boolean someValueIsTrue(boolean[] booleanArray) {
+		synchronized(booleanArray) {
+			for (int i = 0;  i < booleanArray.length;  i++) {
+				if (booleanArray[i]) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}

commit 23c3ee61f1114997b35ad8702d72cf5f102e46d4

reformat Locks and LocksTest for passing Checkstyle Hi. commit https://github.com/apache/commons-lang/commit/c141bc961cb5cb84c47a21c3a5b0f15ab0035728 broke the Checkstyle, thus making travis-ci cannot pass. thus I made a commit to fix it.

src/main/java/org/apache/commons/lang3/Locks.java
@@ -16,20 +16,20 @@
  */
 package org.apache.commons.lang3;
 
-import java.util.Objects;
-import java.util.concurrent.locks.StampedLock;
-
 import org.apache.commons.lang3.Functions.FailableConsumer;
 import org.apache.commons.lang3.Functions.FailableFunction;
+
+import java.util.Objects;
+import java.util.concurrent.locks.StampedLock;
 
 
 /** Utility class for working with {@link java.util.concurrent.locks.Lock locked objects}. Locked objects are an
  * alternative to synchronization.
- * 
+ *
  * Locking is preferable, if there is a distinction between read access (multiple threads may have read
  * access concurrently), and write access (only one thread may have write access at any given time.
  * In comparison, synchronization doesn't support read access, because synchronized access is exclusive.
- * 
+ *
  * Using this class is fairly straightforward:
  * <ol>
  *   <li>While still in single thread mode, create an instance of {@link Locks.Lock} by calling
@@ -54,7 +54,7 @@ import org.apache.commons.lang3.Functions.FailableFunction;
  *         PrintStream ps = new Printstream(out);
  *         lock = Locks.lock(ps);
  *     }
- * 
+ *
  *     public void log(String message) {
  *         lock.runWriteLocked((ps) -&gt; ps.println(message));
  *     }
@@ -62,55 +62,55 @@ import org.apache.commons.lang3.Functions.FailableFunction;
  *     public void log(byte[] buffer) {
  *         lock.runWriteLocked((ps) -&gt; { ps.write(buffer); ps.println(); });
  *     }
- * </pre> 
+ * </pre>
  */
 public class Locks {
-	public static class Lock<O extends Object> {
-		private final O lockedObject;
-		private final StampedLock lock = new StampedLock();
+    public static class Lock<O extends Object> {
+        private final O lockedObject;
+        private final StampedLock lock = new StampedLock();
 
-		public Lock(O lockedObject) {
-			this.lockedObject = Objects.requireNonNull(lockedObject, "Locked Object");
-		}
+        public Lock(O lockedObject) {
+            this.lockedObject = Objects.requireNonNull(lockedObject, "Locked Object");
+        }
 
-		public void runReadLocked(FailableConsumer<O,?> consumer) {
-			runLocked(lock.readLock(), consumer);
-		}
+        public void runReadLocked(FailableConsumer<O, ?> consumer) {
+            runLocked(lock.readLock(), consumer);
+        }
 
-		public void runWriteLocked(FailableConsumer<O,?> consumer) {
-			runLocked(lock.writeLock(), consumer);
-		}
+        public void runWriteLocked(FailableConsumer<O, ?> consumer) {
+            runLocked(lock.writeLock(), consumer);
+        }
 
-		public <T> T callReadLocked(FailableFunction<O,T,?> function) {
-			return callLocked(lock.readLock(), function);
-		}
+        public <T> T callReadLocked(FailableFunction<O, T, ?> function) {
+            return callLocked(lock.readLock(), function);
+        }
 
-		public <T> T callWriteLocked(FailableFunction<O,T,?> function) {
-			return callLocked(lock.writeLock(), function);
-		}
+        public <T> T callWriteLocked(FailableFunction<O, T, ?> function) {
+            return callLocked(lock.writeLock(), function);
+        }
 
-		protected void runLocked(long stamp, FailableConsumer<O,?> consumer) {
-			try {
-				consumer.accept(lockedObject);
-			} catch (Throwable t) {
-				throw Functions.rethrow(t);
-			} finally {
-				lock.unlock(stamp);
-			}
-		}
+        protected void runLocked(long stamp, FailableConsumer<O, ?> consumer) {
+            try {
+                consumer.accept(lockedObject);
+            } catch (Throwable t) {
+                throw Functions.rethrow(t);
+            } finally {
+                lock.unlock(stamp);
+            }
+        }
 
-		protected <T> T callLocked(long stamp, FailableFunction<O,T,?> function) {
-			try {
-				return function.apply(lockedObject);
-			} catch (Throwable t) {
-				throw Functions.rethrow(t);
-			} finally {
-				lock.unlock(stamp);
-			}
-		}
-	}
+        protected <T> T callLocked(long stamp, FailableFunction<O, T, ?> function) {
+            try {
+                return function.apply(lockedObject);
+            } catch (Throwable t) {
+                throw Functions.rethrow(t);
+            } finally {
+                lock.unlock(stamp);
+            }
+        }
+    }
 
-	public static <O extends Object> Locks.Lock<O> lock(O object) {
-		return new Locks.Lock<O>(object);
-	}
+    public static <O extends Object> Locks.Lock<O> lock(O object) {
+        return new Locks.Lock<O>(object);
+    }
 }

src/test/java/org/apache/commons/lang3/LocksTest.java
@@ -16,58 +16,59 @@
  */
 package org.apache.commons.lang3;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.apache.commons.lang3.Functions.FailableConsumer;
 import org.apache.commons.lang3.Locks.Lock;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class LocksTest {
-	@Test
-	void testReadLock() throws Exception {
-		final long DELAY=3000;
-		final boolean[] booleanValues = new boolean[10];
-		final Lock<boolean[]> lock = Locks.lock(booleanValues);
-		final boolean[] runningValues = new boolean[10];
+    @Test
+    void testReadLock() throws Exception {
+        final long DELAY = 3000;
+        final boolean[] booleanValues = new boolean[10];
+        final Lock<boolean[]> lock = Locks.lock(booleanValues);
+        final boolean[] runningValues = new boolean[10];
 
-		final long startTime = System.currentTimeMillis();
-		for (int i = 0;  i < booleanValues.length;  i++) {
-			final int index = i;
-			final FailableConsumer<boolean[],?> consumer = (b) -> {
-				b[index] = false;
-				Thread.sleep(DELAY);
-				b[index] = true;
-				modify(runningValues, index, false);
-			};
-			final Thread t = new Thread(() -> lock.runReadLocked(consumer));
-			modify(runningValues, i, true);
-			t.start();
-		}
-		while (someValueIsTrue(runningValues)) {
-			Thread.sleep(100);
-		}
-		final long endTime = System.currentTimeMillis();
-		for (int i = 0;  i < booleanValues.length;  i++) {
-			assertTrue(booleanValues[i]);
-		}
-		// If our threads would be running in exclusive mode, then we'd need
-		// at least DELAY milliseconds for each.
-		assertTrue((endTime-startTime) < booleanValues.length*DELAY);
-	}
+        final long startTime = System.currentTimeMillis();
+        for (int i = 0; i < booleanValues.length; i++) {
+            final int index = i;
+            final FailableConsumer<boolean[], ?> consumer = (b) -> {
+                b[index] = false;
+                Thread.sleep(DELAY);
+                b[index] = true;
+                modify(runningValues, index, false);
+            };
+            final Thread t = new Thread(() -> lock.runReadLocked(consumer));
+            modify(runningValues, i, true);
+            t.start();
+        }
+        while (someValueIsTrue(runningValues)) {
+            Thread.sleep(100);
+        }
+        final long endTime = System.currentTimeMillis();
+        for (int i = 0; i < booleanValues.length; i++) {
+            assertTrue(booleanValues[i]);
+        }
+        // If our threads would be running in exclusive mode, then we'd need
+        // at least DELAY milliseconds for each.
+        assertTrue((endTime - startTime) < booleanValues.length * DELAY);
+    }
 
-	protected void modify(boolean[] booleanArray, int offset, boolean value) {
-		synchronized(booleanArray) {
-			booleanArray[offset] = value;
-		}
-	}
-	protected boolean someValueIsTrue(boolean[] booleanArray) {
-		synchronized(booleanArray) {
-			for (int i = 0;  i < booleanArray.length;  i++) {
-				if (booleanArray[i]) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
+    protected void modify(boolean[] booleanArray, int offset, boolean value) {
+        synchronized (booleanArray) {
+            booleanArray[offset] = value;
+        }
+    }
+
+    protected boolean someValueIsTrue(boolean[] booleanArray) {
+        synchronized (booleanArray) {
+            for (int i = 0; i < booleanArray.length; i++) {
+                if (booleanArray[i]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }