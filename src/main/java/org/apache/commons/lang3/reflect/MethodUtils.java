f6e7a21963cab263d02eba0e50e0ddc6faac0aa3
XenoAmess <xenoamess@gmail.com>
2020-06-01 16:10:38 +0800

    Update MethodUtils.java

--- src/main/java/org/apache/commons/lang3/reflect/MethodUtils.java
+++ src/main/java/org/apache/commons/lang3/reflect/MethodUtils.java
@@ -25,7 +25,6 @@ import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
