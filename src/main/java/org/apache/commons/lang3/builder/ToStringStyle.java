364342b1b7490c81dccf05929e6e871dab0ee9d9
XenoAmess <xenoamess@gmail.com>
2020-06-01 16:23:54 +0800

    change_removeLastFieldSeparator_to_use_endsWith

--- src/main/java/org/apache/commons/lang3/builder/ToStringStyle.java
+++ src/main/java/org/apache/commons/lang3/builder/ToStringStyle.java
@@ -437,19 +437,8 @@ public abstract class ToStringStyle implements Serializable {
      * @since 2.0
      */
     protected void removeLastFieldSeparator(final StringBuffer buffer) {
-        final int len = buffer.length();
-        final int sepLen = fieldSeparator.length();
-        if (len > 0 && sepLen > 0 && len >= sepLen) {
-            boolean match = true;
-            for (int i = 0; i < sepLen; i++) {
-                if (buffer.charAt(len - 1 - i) != fieldSeparator.charAt(sepLen - 1 - i)) {
-                    match = false;
-                    break;
-                }
-            }
-            if (match) {
-                buffer.setLength(len - sepLen);
-            }
+        if (StringUtils.endsWith(buffer,fieldSeparator)) {
+            buffer.setLength(buffer.length() - fieldSeparator.length());
         }
     }
 
