9be3ce419ebb414cf651ebbddc4289e437f0e8c1
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2020-10-27 21:31:34 +0100

    Test cases for StringUtils.join array of doubles with String separator

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -4152,6 +4152,11 @@ public class StringUtils {
      * @since 3.2
      */
     public static String join(final double[] array, final char delimiter, final int startIndex, final int endIndex) {
+        return join(array, Character.toString(separator), startIndex, endIndex);
+    }
+
+    // TODO: JavaDoc
+    public static String join(final double[] array, final String separator, final int startIndex, final int endIndex) {
         if (array == null) {
             return null;
         }
@@ -4874,6 +4879,11 @@ public class StringUtils {
 
     // TODO: JavaDoc
     public static String join(final short[] array, final String separator) {
+        return join(array, separator, 0, array.length);
+    }
+
+    // TODO: JavaDoc
+    public static String join(final double[] array, final String separator) {
         return join(array, separator, 0, array.length);
     }
 
--- src/test/java/org/apache/commons/lang3/StringUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/StringUtilsTest.java
@@ -1197,7 +1197,12 @@ public class StringUtilsTest {
         assertEquals(StringUtils.EMPTY, StringUtils.join(DOUBLE_PRIM_LIST, SEPARATOR_CHAR, 0, 0));
         assertEquals(StringUtils.EMPTY, StringUtils.join(DOUBLE_PRIM_LIST, SEPARATOR_CHAR, 1, 0));
 
-        assertEquals("1,2", StringUtils.join(DOUBLE_PRIM_LIST, SEPARATOR));
+        assertNull(StringUtils.join((double[]) null, ','));
+        assertEquals("1.0,2.0", StringUtils.join(DOUBLE_PRIM_LIST, SEPARATOR));
+        assertEquals("2.0", StringUtils.join(DOUBLE_PRIM_LIST, SEPARATOR, 1, 2));
+        assertNull(StringUtils.join((double[]) null, SEPARATOR, 0, 1));
+        assertEquals(StringUtils.EMPTY, StringUtils.join(DOUBLE_PRIM_LIST, SEPARATOR, 0, 0));
+        assertEquals(StringUtils.EMPTY, StringUtils.join(DOUBLE_PRIM_LIST, SEPARATOR, 1, 0));
     }
 
     @Test
