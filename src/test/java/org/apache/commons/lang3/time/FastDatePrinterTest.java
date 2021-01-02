fd4bb4ab89678ce29fd18d8fc20bcfb57f146a8f
Uri Gonen <uri.gonen@datorama.com>
2021-01-02 18:13:08 +0200

    LANG-1637: added test cases for Y & YYY patterns

--- src/test/java/org/apache/commons/lang3/time/FastDatePrinterTest.java
+++ src/test/java/org/apache/commons/lang3/time/FastDatePrinterTest.java
@@ -440,12 +440,12 @@ FastDatePrinterTest {
     public void testWeekYear() {
         final GregorianCalendar cal = new GregorianCalendar(2020, 12, 31, 0, 0, 0);
         final DatePrinter printer4Digits = getInstance("YYYY");
-        final DatePrinter printer3Digits = getInstance("YYY");
+        final DatePrinter printer4DigitsFallback = getInstance("YYY");
         final DatePrinter printer2Digits = getInstance("YY");
-        final DatePrinter printer1Digit = getInstance("Y");
+        final DatePrinter printer4DigitAnotherFallback = getInstance("Y");
         assertEquals("2021", printer4Digits.format(cal));
-        assertEquals("2021", printer3Digits.format(cal));
-        assertEquals("2021", printer1Digit.format(cal));
+        assertEquals("2021", printer4DigitsFallback.format(cal));
+        assertEquals("2021", printer4DigitAnotherFallback.format(cal));
         assertEquals("21", printer2Digits.format(cal));
     }
 }
