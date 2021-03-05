73f2b7381dc0e68e1fb8ba311434b6af1ebdd7b3
abernal <arturobernalg@gmail.com>
2021-03-05 16:00:41 +0100

    LANG-1647 - Check if an throwable is a checked exception

--- src/main/java/org/apache/commons/lang3/concurrent/ConcurrentUtils.java
+++ src/main/java/org/apache/commons/lang3/concurrent/ConcurrentUtils.java
@@ -22,6 +22,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 /**
  * <p>
@@ -142,8 +143,7 @@ public class ConcurrentUtils {
      * checked exception
      */
     static Throwable checkedException(final Throwable ex) {
-        Validate.isTrue(ex != null && !(ex instanceof RuntimeException)
-                && !(ex instanceof Error), "Not a checked exception: " + ex);
+        Validate.isTrue(ExceptionUtils.isChecked(ex), "Not a checked exception: " + ex);
 
         return ex;
     }
