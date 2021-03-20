4ec9abf894aeab727cb18f53d34cd6fc057c7b95
Ciprian Bodnarescu <ciprian.bodnarescu@netcentric.biz>
2021-03-20 15:00:19 +0200

    Use Set instead of List for checking the contains() method

--- src/main/java/org/apache/commons/lang3/LocaleUtils.java
+++ src/main/java/org/apache/commons/lang3/LocaleUtils.java
@@ -117,13 +117,13 @@ public class LocaleUtils {
     }
 
     /**
-     * <p>Checks if the locale specified is in the list of available locales.</p>
+     * <p>Checks if the locale specified is in the set of available locales.</p>
      *
      * @param locale the Locale object to check if it is available
      * @return true if the locale is a known locale
      */
     public static boolean isAvailableLocale(final Locale locale) {
-        return availableLocaleList().contains(locale);
+        return availableLocaleSet().contains(locale);
     }
 
     /**
