f345ce7e12006f4ea07d661f2a2f77e7b4b0e487
LeeKyoungIl <leekyoungil@gmail.com>
2021-05-16 22:55:31 +0900

    Add nullSafeEquals method.

--- src/main/java/org/apache/commons/lang3/ObjectUtils.java
+++ src/main/java/org/apache/commons/lang3/ObjectUtils.java
@@ -19,9 +19,11 @@ package org.apache.commons.lang3;
 import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -710,6 +712,94 @@ public class ObjectUtils {
             }
         }
         return null;
+    }
+
+    /**
+     * <p>Compare two objects for equality. Even if the object to be compared is null,
+     * it safely returns false and compares nulls.</p>
+     *
+     * <pre>
+     * class Baz {
+     *     String testBazString = "test";
+     * }
+     *
+     * class Bar {
+     *     String testBarString = "test";
+     *     Baz baz = new Baz();
+     *     Baz nullBaz;
+     * }
+     *
+     * class Foo {
+     *     Bar bar = new Bar();
+     *     Bar nullBar;
+     *     int primitiveInt = 1;
+     *     String testFooString = "test";
+     *
+     *     public static final String PUBLIC_STATIC_FINAL_FOO = "foo";
+     *     public static String PUBLIC_STATIC_NULL_FOO;
+     * }
+     *
+     * Foo foo = new Foo();
+     *
+     * ObjectUtils.nullSafeEquals(foo, "nullBar", null)                          = true
+     * ObjectUtils.nullSafeEquals(foo, "nullBar.testBarString", "test")          = false
+     * ObjectUtils.nullSafeEquals(foo, "bar.testBarString", "test")              = true
+     * ObjectUtils.nullSafeEquals(foo, "bar.nullBaz.testBazString", "test")      = false
+     * ObjectUtils.nullSafeEquals(foo, "primitiveInt", 1)                        = true
+     * ObjectUtils.nullSafeEquals(foo, "primitiveInt", 2)                        = false
+     * ObjectUtils.nullSafeEquals(foo, "PUBLIC_STATIC_FINAL_FOO", "foo")         = true
+     * ObjectUtils.nullSafeEquals(foo, "PUBLIC_STATIC_NULL_FOO", "foo")          = false
+     * </pre>
+     *
+     * @param source the first object.
+     * @param targetPath object target path to be checked.
+     * @param value value to compare.
+     * @return {@code true} if the objects are same, {@code false} otherwise
+     * @throws IllegalAccessException, IllegalArgumentException if an Access or Argument error occurs.
+     */
+    public static boolean nullSafeEquals(final Object source, String targetPath, Object value) throws IllegalAccessException, IllegalArgumentException {
+        if (source == null || StringUtils.isBlank(targetPath)) {
+            return false;
+        }
+        String[] targetValues = targetPath.split("\\.");
+        if (targetValues.length == 0) {
+            return false;
+        }
+        if (source.getClass().getSimpleName().equals(targetValues[0]) && targetPath.contains(".")) {
+            targetValues = Arrays.copyOfRange(targetValues, 1, targetValues.length);
+        }
+        final Field[] fields = source.getClass().getDeclaredFields();
+        if (targetValues.length == 1) {
+            for (Field field : fields) {
+                if (!field.getName().equalsIgnoreCase(targetValues[0])) {
+                    continue;
+                }
+
+                field.setAccessible(true);
+                final Object compareValue = field.get(source);
+                if (value == null) {
+                    return compareValue == null;
+                } else {
+                    return value.equals(compareValue);
+                }
+            }
+        } else {
+            for (int i=0; i<fields.length; i++) {
+                final Field field = fields[i];
+                if (!field.getName().equalsIgnoreCase(targetValues[0])) {
+                    continue;
+                }
+
+                field.setAccessible(true);
+                final Object nextSource = field.get(source);
+                if (nextSource == null) {
+                    return false;
+                } else if (targetValues.length >= (i+1)) {
+                    return nullSafeEquals(nextSource, targetPath.substring(targetValues[0].length()+1), value);
+                }
+            }
+        }
+        return false;
     }
 
     /**
