d85d59e0b40276be6185ba2b5cf4b9eceb6bf7fc
韩金明 <hanjm@highzap.com>
2021-11-14 14:12:03 +0800

    feat: list2tree method add(import optimize)

--- src/main/java/org/apache/commons/lang3/TreeUtils.java
+++ src/main/java/org/apache/commons/lang3/TreeUtils.java
@@ -17,7 +17,14 @@
 package org.apache.commons.lang3;
 
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
