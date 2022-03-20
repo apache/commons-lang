5648bc42e2f618bd230e815eb36964d2fd62d303
Gary Gregory <garydgregory@gmail.com>
2022-03-20 14:06:39 -0400

    Better concurrency with the Java 8 API ConcurrentMap#computeIfAbsent().

--- src/main/java/org/apache/commons/lang3/time/FastDateParser.java
+++ src/main/java/org/apache/commons/lang3/time/FastDateParser.java
@@ -653,16 +653,9 @@ public class FastDateParser implements DateParser, Serializable {
      */
     private Strategy getLocaleSpecificStrategy(final int field, final Calendar definingCalendar) {
         final ConcurrentMap<Locale, Strategy> cache = getCache(field);
-        Strategy strategy = cache.get(locale);
-        if (strategy == null) {
-            strategy = field == Calendar.ZONE_OFFSET ? new TimeZoneStrategy(locale)
-                : new CaseInsensitiveTextStrategy(field, definingCalendar, locale);
-            final Strategy inCache = cache.putIfAbsent(locale, strategy);
-            if (inCache != null) {
-                return inCache;
-            }
-        }
-        return strategy;
+        return cache.computeIfAbsent(locale, k -> {
+            return field == Calendar.ZONE_OFFSET ? new TimeZoneStrategy(locale) : new CaseInsensitiveTextStrategy(field, definingCalendar, locale);
+        });
     }
 
     /**
