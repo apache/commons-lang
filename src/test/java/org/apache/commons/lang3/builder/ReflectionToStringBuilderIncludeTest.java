36daf0433141ed69efed6bda594a71e5b24482eb
GutoVeronezi <daniel@scclouds.com.br>
2022-04-20 21:05:08 -0300

    Remove unnused import

--- src/test/java/org/apache/commons/lang3/builder/ReflectionToStringBuilderIncludeTest.java
+++ src/test/java/org/apache/commons/lang3/builder/ReflectionToStringBuilderIncludeTest.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
