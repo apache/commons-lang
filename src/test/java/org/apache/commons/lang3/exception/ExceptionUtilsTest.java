702d661907c4dbed6caeaffa89c17efb550c4f8b
Gary Gregory <garydgregory@gmail.com>
2022-05-30 09:14:43 -0400

    Add synchronized like super

--- src/test/java/org/apache/commons/lang3/exception/ExceptionUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/exception/ExceptionUtilsTest.java
@@ -67,7 +67,7 @@ public class ExceptionUtilsTest {
         }
 
         @Override
-        public Throwable getCause() {
+        public synchronized Throwable getCause() {
             return cause;
         }
 
