0c1a79ff3456a52703b6b04ff412287802baac36
Michael Ernst <mernst@cs.washington.edu>
2023-04-10 11:50:10 -0700

    Update URL for JDK bug

--- src/main/java/org/apache/commons/lang3/time/FastDateParser.java
+++ src/main/java/org/apache/commons/lang3/time/FastDateParser.java
@@ -59,7 +59,7 @@ import org.apache.commons.lang3.LocaleUtils;
  * This class is especially useful in multi-threaded server environments.
  * {@link SimpleDateFormat} is not thread-safe in any JDK version,
  * nor will it be as Sun has closed the
- * <a href="https://bugs.java.com/bugdatabase/view_bug.do?bug_id=4228335">bug</a>/RFE.
+ * <a href="https://bugs.openjdk.org/browse/JDK-4228335">bug</a>/RFE.
  * </p>
  *
  * <p>Only parsing is supported by this class, but all patterns are compatible with
