e5030d6571d76839592fd29530da51a0c58156a4
xumengqi <2663479778@qq.com>
2023-04-17 20:42:15 +0800

    Fix the comment of nop

--- src/main/java/org/apache/commons/lang3/function/TriFunction.java
+++ src/main/java/org/apache/commons/lang3/function/TriFunction.java
@@ -46,6 +46,10 @@ public interface TriFunction<T, U, V, R> {
     /**
      * Returns The NOP singleton.
      *
+     * @param <T> the type of the first argument to the function
+     * @param <U> the type of the second argument to the function
+     * @param <V> the type of the third argument to the function
+     * @param <R> the type of the result of the function
      * @return The NOP singleton.
      */
     static <T, U, V, R> TriFunction<T, U, V, R> nop() {
