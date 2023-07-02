b7738998544b6ccdc349e5c4eafca5953ab70cf6
Gary Gregory <garydgregory@gmail.com>
2023-07-02 10:32:25 -0400

    Inline single-use local variable

--- src/main/java/org/apache/commons/lang3/ArchUtils.java
+++ src/main/java/org/apache/commons/lang3/ArchUtils.java
@@ -51,38 +51,31 @@ public class ArchUtils {
     }
 
     private static void init_Aarch_64Bit() {
-        final Processor processor = new Processor(Processor.Arch.BIT_64, Processor.Type.AARCH_64);
-        addProcessors(processor, "aarch64");
+        addProcessors(new Processor(Processor.Arch.BIT_64, Processor.Type.AARCH_64), "aarch64");
     }
 
     private static void init_X86_32Bit() {
-        final Processor processor = new Processor(Processor.Arch.BIT_32, Processor.Type.X86);
-        addProcessors(processor, "x86", "i386", "i486", "i586", "i686", "pentium");
+        addProcessors(new Processor(Processor.Arch.BIT_32, Processor.Type.X86), "x86", "i386", "i486", "i586", "i686", "pentium");
     }
 
     private static void init_X86_64Bit() {
-        final Processor processor = new Processor(Processor.Arch.BIT_64, Processor.Type.X86);
-        addProcessors(processor, "x86_64", "amd64", "em64t", "universal");
+        addProcessors(new Processor(Processor.Arch.BIT_64, Processor.Type.X86), "x86_64", "amd64", "em64t", "universal");
     }
 
     private static void init_IA64_32Bit() {
-        final Processor processor = new Processor(Processor.Arch.BIT_32, Processor.Type.IA_64);
-        addProcessors(processor, "ia64_32", "ia64n");
+        addProcessors(new Processor(Processor.Arch.BIT_32, Processor.Type.IA_64), "ia64_32", "ia64n");
     }
 
     private static void init_IA64_64Bit() {
-        final Processor processor = new Processor(Processor.Arch.BIT_64, Processor.Type.IA_64);
-        addProcessors(processor, "ia64", "ia64w");
+        addProcessors(new Processor(Processor.Arch.BIT_64, Processor.Type.IA_64), "ia64", "ia64w");
     }
 
     private static void init_PPC_32Bit() {
-        final Processor processor = new Processor(Processor.Arch.BIT_32, Processor.Type.PPC);
-        addProcessors(processor, "ppc", "power", "powerpc", "power_pc", "power_rs");
+        addProcessors(new Processor(Processor.Arch.BIT_32, Processor.Type.PPC), "ppc", "power", "powerpc", "power_pc", "power_rs");
     }
 
     private static void init_PPC_64Bit() {
-        final Processor processor = new Processor(Processor.Arch.BIT_64, Processor.Type.PPC);
-        addProcessors(processor, "ppc64", "power64", "powerpc64", "power_pc64", "power_rs64");
+        addProcessors(new Processor(Processor.Arch.BIT_64, Processor.Type.PPC), "ppc64", "power64", "powerpc64", "power_pc64", "power_rs64");
     }
 
     /**
