0dee4cda62eada1f07c269843d125c899b36f8fe
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2023-07-29 07:26:30 -0400

    Remove 2.1 specific comment
    Putting release notes in Javadoc goes out of date fast

--- src/main/java/org/apache/commons/lang3/time/package-info.java
+++ src/main/java/org/apache/commons/lang3/time/package-info.java
@@ -21,8 +21,6 @@
  * <p>The time package contains some basic utilities for manipulating time (a delorean, police box and grandfather clock?).
  * These include a {@link org.apache.commons.lang3.time.StopWatch} for simple performance measurements and an optimised {@link org.apache.commons.lang3.time.FastDateFormat} class.</p>
  *
- * <p>New in Lang 2.1 is the {@link org.apache.commons.lang3.time.DurationFormatUtils} class, which provides various methods for formatting durations.</p>
- *
  * @since 2.0
  */
 package org.apache.commons.lang3.time;
