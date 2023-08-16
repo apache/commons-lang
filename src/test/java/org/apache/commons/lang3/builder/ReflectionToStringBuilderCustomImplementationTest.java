cb61a266bd13e6d31d48cb2433bed6db2ef82db2
Shashank Sharma <shashankrnr32@gmail.com>
2023-08-16 20:30:41 +0530

    Remove unused import

--- src/test/java/org/apache/commons/lang3/builder/ReflectionToStringBuilderCustomImplementationTest.java
+++ src/test/java/org/apache/commons/lang3/builder/ReflectionToStringBuilderCustomImplementationTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Regression test to ensure the {@link ReflectionToStringBuilder} always
