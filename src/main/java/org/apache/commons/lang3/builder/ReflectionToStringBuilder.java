15ff8f999780b566d21b53550a6c3e0c5cbe8994
Gary Gregory <garydgregory@gmail.com>
2023-08-27 07:44:42 -0400

    [LANG-1710] ReflectionToStringBuilder changes in version 3.13.0 has broken the logic for overriding classes

--- src/main/java/org/apache/commons/lang3/builder/ReflectionToStringBuilder.java
+++ src/main/java/org/apache/commons/lang3/builder/ReflectionToStringBuilder.java
@@ -657,10 +657,17 @@ public class ReflectionToStringBuilder extends ToStringBuilder {
         for (final Field field : fields) {
             final String fieldName = field.getName();
             if (this.accept(field)) {
-                // Warning: Field.get(Object) creates wrappers objects for primitive types.
-                final Object fieldValue = Reflection.getUnchecked(field, getObject());
-                if (!excludeNullValues || fieldValue != null) {
-                    this.append(fieldName, fieldValue, !field.isAnnotationPresent(ToStringSummary.class));
+                try {
+                    // Warning: Field.get(Object) creates wrappers objects
+                    // for primitive types.
+                    final Object fieldValue = this.getValue(field);
+                    if (!excludeNullValues || fieldValue != null) {
+                        this.append(fieldName, fieldValue, !field.isAnnotationPresent(ToStringSummary.class));
+                    }
+                } catch (final IllegalAccessException e) {
+                    // this can't happen. Would get a Security exception instead throw a runtime exception in case the
+                    // impossible happens.
+                    throw new IllegalStateException(e);
                 }
             }
         }
---
+++ src/test/java/org/apache/commons/lang3/builder/ReflectionToStringBuilderCustomImplementationTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.lang3.builder;
+
+import org.apache.commons.lang3.AbstractLangTest;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests {@link ReflectionToStringBuilder} always uses {@link ReflectionToStringBuilder#getValue(Field)} to get the
+ * value of every field in the class.
+ */
+public class ReflectionToStringBuilderCustomImplementationTest extends AbstractLangTest {
+
+    public static class CustomReflectionToStringBuilder extends ReflectionToStringBuilder {
+
+        private static final String CUSTOM_PREFIX = "prefix:";
+
+        public CustomReflectionToStringBuilder(Object object, ToStringStyle toStringStyle) {
+            super(object, toStringStyle);
+        }
+
+        @Override
+        protected Object getValue(Field field) throws IllegalAccessException {
+            return CUSTOM_PREFIX + super.getValue(field);
+        }
+    }
+
+    @SuppressWarnings("unused") // Used indirectly by ReflectionToStringBuilder
+    private final String stringField = "string";
+
+    @Test
+    public void testBuild() {
+        assertEquals("[stringField=prefix:string]",
+                new CustomReflectionToStringBuilder(this, ToStringStyle.NO_CLASS_NAME_STYLE).build());
+    }
+
+}
