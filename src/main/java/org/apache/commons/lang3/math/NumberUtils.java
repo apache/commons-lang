2236e397a6b3c6f058e65f51bf6d71e5d973b3c7
Gary Gregory <garydgregory@users.noreply.github.com>
2023-12-09 09:18:00 -0500

    Don't change the formatting

--- src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -316,7 +316,6 @@ public class NumberUtils {
         if (str == null) {
             return null;
         }
-
         if (StringUtils.isBlank(str)) {
             throw new NumberFormatException("A blank string is not a valid number");
         }
