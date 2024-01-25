c6f1bfeb1a0cc3879d39ff09cce89180a526a0a1
Gary Gregory <garydgregory@gmail.com>
2024-01-25 09:15:25 -0500

    Depreacte BooleanUtils 0-argument contructor

--- src/main/java/org/apache/commons/lang3/BooleanUtils.java
+++ src/main/java/org/apache/commons/lang3/BooleanUtils.java
@@ -1212,8 +1212,12 @@ public class BooleanUtils {
      *
      * <p>This constructor is public to permit tools that require a JavaBean instance
      * to operate.</p>
+     *
+     * @deprecated TODO Make private in 4.0.
      */
+    @Deprecated
     public BooleanUtils() {
+        // empty
     }
 
 }
