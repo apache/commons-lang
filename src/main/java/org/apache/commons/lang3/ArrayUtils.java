db6f3ed367f7d7ff854b63adcc1ac5a006a7ff13
Gary Gregory <garydgregory@gmail.com>
2024-01-25 09:14:46 -0500

    Depreacte ArrayUtils 0-argument contructor

--- src/main/java/org/apache/commons/lang3/ArrayUtils.java
+++ src/main/java/org/apache/commons/lang3/ArrayUtils.java
@@ -9634,7 +9634,11 @@ public class ArrayUtils {
      * This constructor is public to permit tools that require a JavaBean instance
      * to operate.
      * </p>
+     *
+     * @deprecated TODO Make private in 4.0.
      */
+    @Deprecated
     public ArrayUtils() {
+        // empty
     }
 }
