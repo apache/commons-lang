21d0b562b706e774d75df3cf5d7b2df967a21f89
Gary Gregory <garydgregory@gmail.com>
2024-01-25 09:36:41 -0500

    Depreacte DateUtils 0-argument contructor

--- src/main/java/org/apache/commons/lang3/time/DateUtils.java
+++ src/main/java/org/apache/commons/lang3/time/DateUtils.java
@@ -1770,8 +1770,12 @@ public class DateUtils {
      *
      * <p>This constructor is public to permit tools that require a JavaBean
      * instance to operate.</p>
+     *
+     * @deprecated TODO Make private in 4.0.
      */
+    @Deprecated
     public DateUtils() {
+        // empty
     }
 
 }
