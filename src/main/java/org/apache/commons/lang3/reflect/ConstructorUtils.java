4822446e98c6e1d437b19ef371acd5a4f29a6a63
Gary Gregory <garydgregory@gmail.com>
2024-01-25 09:31:12 -0500

    Depreacte ConstructorUtils 0-argument contructor

--- src/main/java/org/apache/commons/lang3/reflect/ConstructorUtils.java
+++ src/main/java/org/apache/commons/lang3/reflect/ConstructorUtils.java
@@ -288,8 +288,12 @@ public class ConstructorUtils {
      *
      * <p>This constructor is {@code public} to permit tools that require a JavaBean
      * instance to operate.</p>
+     *
+     * @deprecated TODO Make private in 4.0.
      */
+    @Deprecated
     public ConstructorUtils() {
+        // empty
     }
 
 }
