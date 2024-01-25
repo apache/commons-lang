8926ed1c63f3ca4b442af947045c72ad2ec1f049
Gary Gregory <garydgregory@gmail.com>
2024-01-25 09:33:49 -0500

    Depreacte TypeUtils 0-argument contructor

--- src/main/java/org/apache/commons/lang3/reflect/TypeUtils.java
+++ src/main/java/org/apache/commons/lang3/reflect/TypeUtils.java
@@ -1938,8 +1938,12 @@ public class TypeUtils {
      * This constructor is public to permit tools that require a JavaBean instance
      * to operate.
      * </p>
+     *
+     * @deprecated TODO Make private in 4.0.
      */
+    @Deprecated
     public TypeUtils() {
+        // empty
     }
 
 }
