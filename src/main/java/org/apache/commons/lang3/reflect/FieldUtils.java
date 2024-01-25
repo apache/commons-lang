212d2ae3542f13dd200bacf8c24ea1e32a8f6957
Gary Gregory <garydgregory@gmail.com>
2024-01-25 09:31:37 -0500

    Depreacte FieldUtils 0-argument contructor

--- src/main/java/org/apache/commons/lang3/reflect/FieldUtils.java
+++ src/main/java/org/apache/commons/lang3/reflect/FieldUtils.java
@@ -873,7 +873,11 @@ public class FieldUtils {
      * <p>
      * This constructor is {@code public} to permit tools that require a JavaBean instance to operate.
      * </p>
+     *
+     * @deprecated TODO Make private in 4.0.
      */
+    @Deprecated
     public FieldUtils() {
+        // empty
     }
 }
