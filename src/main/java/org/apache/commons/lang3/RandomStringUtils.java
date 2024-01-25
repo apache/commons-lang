61a0ac00852f22aaf258790a4ce8ccd99d0f6eb7
Gary Gregory <garydgregory@gmail.com>
2024-01-25 09:20:47 -0500

    Depreacte RandomStringUtils 0-argument contructor

--- src/main/java/org/apache/commons/lang3/RandomStringUtils.java
+++ src/main/java/org/apache/commons/lang3/RandomStringUtils.java
@@ -477,8 +477,12 @@ public class RandomStringUtils {
      *
      * <p>This constructor is public to permit tools that require a JavaBean instance
      * to operate.</p>
+     *
+     * @deprecated TODO Make private in 4.0.
      */
+    @Deprecated
     public RandomStringUtils() {
+        // empty
     }
 
 }
