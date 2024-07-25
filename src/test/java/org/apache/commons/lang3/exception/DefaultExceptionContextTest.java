a4f38e95eee8bfa6fd79b11bea289ca7c8c2a325
Gary Gregory <garydgregory@gmail.com>
2024-07-25 08:46:19 -0400

    Add DefaultExceptionContextTest.testFormattedExceptionMessageExceptionHandling()

--- src/test/java/org/apache/commons/lang3/exception/DefaultExceptionContextTest.java
+++ src/test/java/org/apache/commons/lang3/exception/DefaultExceptionContextTest.java
@@ -16,6 +16,10 @@
  */
 package org.apache.commons.lang3.exception;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.apache.commons.lang3.ObjectToStringRuntimeException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -32,9 +36,22 @@ public class DefaultExceptionContextTest extends AbstractExceptionContextTest<De
     }
 
     @Test
+    public void testFormattedExceptionMessageExceptionHandling() {
+        exceptionContext = new DefaultExceptionContext();
+        final String label1 = "throws 1";
+        final String label2 = "throws 2";
+        exceptionContext.addContextValue(label1, new ObjectToStringRuntimeException(label1));
+        exceptionContext.addContextValue(label2, new ObjectToStringRuntimeException(label2));
+        final String message = exceptionContext.getFormattedExceptionMessage(TEST_MESSAGE);
+        assertTrue(message.startsWith(TEST_MESSAGE));
+        assertTrue(message.contains(label1));
+        assertTrue(message.contains(label2));
+    }
+
+    @Test
     public void testFormattedExceptionMessageNull() {
         exceptionContext = new DefaultExceptionContext();
-        exceptionContext.getFormattedExceptionMessage(null);
+        assertEquals("", exceptionContext.getFormattedExceptionMessage(null));
     }
 
 }
