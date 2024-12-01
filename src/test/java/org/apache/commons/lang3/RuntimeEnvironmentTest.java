ce28ddedcae8972af1614b66b9c13b465b25d288
maxxedev <5051664+maxxedev@users.noreply.github.com>
2024-12-01 15:42:31 -0800

    Fix checkstyle import order

--- src/test/java/org/apache/commons/lang3/RuntimeEnvironmentTest.java
+++ src/test/java/org/apache/commons/lang3/RuntimeEnvironmentTest.java
@@ -17,10 +17,7 @@
 
 package org.apache.commons.lang3;
 
-import org.junit.jupiter.api.io.TempDir;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -28,7 +25,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 
 /**
  * Tests {@link RuntimeEnvironment}.
