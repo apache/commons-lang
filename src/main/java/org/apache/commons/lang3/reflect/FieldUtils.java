5ba76850bd009dfb3638a91e0b673d455c173e08
Gary Gregory <garydgregory@gmail.com>
2025-01-28 09:52:14 -0500

    No need to break string up

--- src/main/java/org/apache/commons/lang3/reflect/FieldUtils.java
+++ src/main/java/org/apache/commons/lang3/reflect/FieldUtils.java
@@ -217,8 +217,8 @@ public class FieldUtils {
         for (final Class<?> class1 : ClassUtils.getAllInterfaces(cls)) {
             try {
                 final Field test = class1.getField(fieldName);
-                Validate.isTrue(match == null, "Reference to field %s is ambiguous relative to %s"
-                        + "; a matching field exists on two or more implemented interfaces.", fieldName, cls);
+                Validate.isTrue(match == null,
+                        "Reference to field %s is ambiguous relative to %s; a matching field exists on two or more implemented interfaces.", fieldName, cls);
                 match = test;
             } catch (final NoSuchFieldException ignored) {
                 // ignore
