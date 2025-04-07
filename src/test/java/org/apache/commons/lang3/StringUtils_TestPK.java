15dac093d43c8f0ef17f2c9949a7def2663edd46
Jyotprabhs <jyot.prabhs@gmail.com>
2025-04-07 00:34:52 -0400

    updating

--- src/test/java/org/apache/commons/lang3/StringUtils_TestPK.java
+++ src/test/java/org/apache/commons/lang3/StringUtils_TestPK.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache;
+package org.apache.commons.lang3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
