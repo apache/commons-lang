76d30201faf8f5c116c3130b880d8b494a5bfc8f
Gary Gregory <garydgregory@gmail.com>
2025-04-15 15:43:54 -0400

    Reimplement StringUtils.capitalize(String) to use java.lang.CharSequence.codePoints()

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -517,27 +517,18 @@ public class StringUtils {
      * @since 2.0
      */
     public static String capitalize(final String str) {
-        final int strLen = length(str);
-        if (strLen == 0) {
+        if (isEmpty(str)) {
             return str;
         }
-
         final int firstCodepoint = str.codePointAt(0);
         final int newCodePoint = Character.toTitleCase(firstCodepoint);
         if (firstCodepoint == newCodePoint) {
             // already capitalized
             return str;
         }
-
-        final int[] newCodePoints = new int[strLen]; // cannot be longer than the char array
-        int outOffset = 0;
-        newCodePoints[outOffset++] = newCodePoint; // copy the first code point
-        for (int inOffset = Character.charCount(firstCodepoint); inOffset < strLen; ) {
-            final int codePoint = str.codePointAt(inOffset);
-            newCodePoints[outOffset++] = codePoint; // copy the remaining ones
-            inOffset += Character.charCount(codePoint);
-         }
-        return new String(newCodePoints, 0, outOffset);
+        final int[] newCodePoints = str.codePoints().toArray();
+        newCodePoints[0] = newCodePoint; // copy the first code point
+        return new String(newCodePoints, 0, newCodePoints.length);
     }
 
     /**
