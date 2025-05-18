0d479ab9f059a747ddecf7d9d26fba63e03487d3
Finger <53509788+yuyu1025@users.noreply.github.com>
2025-05-18 08:53:04 +0800

    style: Optimize readability
    Co-authored-by: Piotr P. Karwasz <piotr@github.copernik.eu>

--- src/main/java/org/apache/commons/lang3/time/DateUtils.java
+++ src/main/java/org/apache/commons/lang3/time/DateUtils.java
@@ -1648,8 +1648,9 @@ public class DateUtils {
      * @since 3.18
      */
     public static LocalDateTime toLocalDateTime(final Date date, final TimeZone tz) {
-        final LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(Objects.requireNonNull(date, "date").getTime()),
-                Objects.requireNonNull(tz, "tz").toZoneId());
+        Objects.requireNonNull(date, "date");
+        Objects.requireNonNull(tz, "tz");
+        final LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(date.getTime()), tz.toZoneId());
         if (date instanceof java.sql.Timestamp) {
             return localDateTime.withNano(((Timestamp) date).getNanos());
         }
