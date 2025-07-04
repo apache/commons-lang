f71459b6bf6c5bf67685c1befc87fc52f9c7e09b
Gary D. Gregory <garydgregory@gmail.com>
2025-07-04 13:52:00 -0400

    Don't mangle class and method names

--- src/test/java/org/apache/commons/lang3/compare/ComparableUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/compare/ComparableUtilsTest.java
@@ -26,12 +26,9 @@ import java.time.Instant;
 
 import org.apache.commons.lang3.AbstractLangTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ComparableUtilsTest extends AbstractLangTest {
 
     @Nested
