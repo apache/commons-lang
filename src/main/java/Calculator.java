633c38891a18b1fb09c50898f0b58736cb72a3aa
Afifa Sultana <afifasultana637@gmail.com>
2025-09-20 20:29:36 +0600

    added Calculator.java in master

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public Calculator() {}
 
     public double add(double a, double b) {
-        return a + b;
+        return a + b + 100;
     }
 
     public double subtract(double a, double b) {
