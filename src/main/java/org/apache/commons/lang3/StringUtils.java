e97b3583c4629fd4dd539b5f4ac9486a593a18f6
jher235 <tim668666@gmail.com>
2025-12-18 02:14:10 +0900

    [LANG-1805] Fix inaccurate capacity calculation in join(boolean[])
    The previous implementation of join(boolean[], ...) calculated the initial StringBuilder capacity based on 'array.length' instead of the actual number of elements to be joined ('endIndex - startIndex').
    
    This caused excessive memory allocation when joining a small range of a large array (e.g., joining 5 elements from an array of 10,000).
    
    This commit fixes the calculation to use 'noOfItems', ensuring precise memory allocation.
    
    Signed-off-by: jher235 <tim668666@gmail.com>

--- src/main/java/org/apache/commons/lang3/StringUtils.java
+++ src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -3857,10 +3857,11 @@ public class StringUtils {
         if (array == null) {
             return null;
         }
-        if (endIndex - startIndex <= 0) {
+        final int noOfItems = endIndex - startIndex;
+        if (noOfItems <= 0) {
             return EMPTY;
         }
-        final StringBuilder stringBuilder = new StringBuilder(array.length * 5 + array.length - 1);
+        final StringBuilder stringBuilder = new StringBuilder(noOfItems * 5 + noOfItems - 1);
         for (int i = startIndex; i < endIndex; i++) {
             stringBuilder
                     .append(array[i])
