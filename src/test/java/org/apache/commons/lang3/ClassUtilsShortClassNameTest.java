b686df82a7f66eaa195bf4495f8472ed561641d5
Ivan <iponomarev@mail.ru>
2026-01-27 18:16:49 +0000

    fix lang-1818

---
+++ src/test/java/org/apache/commons/lang3/ClassUtilsShortClassNameTest.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.lang3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class $trange {
+
+}
+
+class Pa$$word {
+
+}
+
+/**
+ * Tests for https://issues.apache.org/jira/browse/LANG-1818
+ */
+public class ClassUtilsShortClassNameTest {
+
+    class $Inner {
+
+    }
+
+    class Inner {
+        class Ne$ted {
+
+        }
+    }
+
+    @Test
+    void testDollarSignImmediatelyAfterPackage() {
+        String result = ClassUtils.getShortClassName($trange.class);
+        assertEquals("$trange", result);
+    }
+
+    @Test
+    void testDollarSignWithinName() {
+        String result = ClassUtils.getShortClassName(Pa$$word.class);
+        assertEquals("Pa$$word", result);
+    }
+
+    @Test
+    void testMultipleDollarSigns() {
+        String result = ClassUtils.getShortClassName($Inner.class);
+        assertEquals(getClass().getSimpleName() + ".$Inner", result);
+    }
+
+    @Test
+    void testInnerClassName() {
+        String result = ClassUtils.getShortClassName(Inner.class);
+        assertEquals(getClass().getSimpleName() + ".Inner", result);
+    }
+
+    @Test
+    void testNe$tedClassName() {
+        String result = ClassUtils.getShortClassName(Inner.Ne$ted.class);
+        assertEquals(getClass().getSimpleName() + ".Inner.Ne$ted", result);
+    }
+}
