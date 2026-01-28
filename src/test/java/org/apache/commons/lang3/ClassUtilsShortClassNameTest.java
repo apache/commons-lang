75de25ead6212fbc7ffd901140a2732d50e5a00d
Gary Gregory <garydgregory@users.noreply.github.com>
2026-01-28 15:13:23 -0500

    Checkstyle

--- src/test/java/org/apache/commons/lang3/ClassUtilsShortClassNameTest.java
+++ src/test/java/org/apache/commons/lang3/ClassUtilsShortClassNameTest.java
@@ -17,9 +17,9 @@
 
 package org.apache.commons.lang3;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class $trange {
 
