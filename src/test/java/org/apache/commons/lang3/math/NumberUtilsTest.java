5be0c0e20eb6bb8f083aafc98c6490d0309e4b18
Gary Gregory <garydgregory@users.noreply.github.com>
2026-04-08 14:16:57 -0400

    Fix more Checkstyle errors.

--- src/test/java/org/apache/commons/lang3/math/NumberUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/math/NumberUtilsTest.java
@@ -889,7 +889,7 @@ class NumberUtilsTest extends AbstractLangTest {
         compareIsCreatableWithCreateNumber(".D", false); // LANG-1646
         compareIsCreatableWithCreateNumber(".e10", false); // LANG-1646
         compareIsCreatableWithCreateNumber(".e10D", false); // LANG-1646
-        
+
         compareIsCreatableWithCreateNumber("1E2147483647", true);
         compareIsCreatableWithCreateNumber("1E-2147483647", true);
         compareIsCreatableWithCreateNumber("1E-2147483648", false);
@@ -897,13 +897,12 @@ class NumberUtilsTest extends AbstractLangTest {
         compareIsCreatableWithCreateNumber("1E+2147483648", SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_21));
         compareIsCreatableWithCreateNumber("1E+2147483649", false);
         compareIsCreatableWithCreateNumber("1E-2147483649", false);
-        
-        compareIsCreatableWithCreateNumber("1E2147483648D",SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_21));
+
+        compareIsCreatableWithCreateNumber("1E2147483648D", SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_21));
         compareIsCreatableWithCreateNumber("1E-2147483648D", false);
 
-        compareIsCreatableWithCreateNumber("1.0E2147483648",SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_21));
+        compareIsCreatableWithCreateNumber("1.0E2147483648", SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_21));
         compareIsCreatableWithCreateNumber("1.0E-2147483648", false);
-
     }
 
     @Test
