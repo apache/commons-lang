2d2e68560fb001c7fa8f8d6f7475f3b800acd995
Gary Gregory <garydgregory@gmail.com>
2026-06-02 07:28:06 -0400

    NumericEntityUnescaperTest: Use less vertical space

--- src/test/java/org/apache/commons/lang3/text/translate/NumericEntityUnescaperTest.java
+++ src/test/java/org/apache/commons/lang3/text/translate/NumericEntityUnescaperTest.java
@@ -52,7 +52,6 @@ class NumericEntityUnescaperTest extends AbstractLangTest {
         final NumericEntityUnescaper neu = new NumericEntityUnescaper();
         final String input = "&#68642;";
         final String expected = "\uD803\uDC22";
-
         final String result = neu.translate(input);
         assertEquals(expected, result, "Failed to unescape numeric entities supplementary characters");
     }
@@ -63,21 +62,16 @@ class NumericEntityUnescaperTest extends AbstractLangTest {
         NumericEntityUnescaper neu = new NumericEntityUnescaper(NumericEntityUnescaper.OPTION.semiColonOptional);
         String input = "Test &#x30 not test";
         String expected = "Test \u0030 not test";
-
         String result = neu.translate(input);
         assertEquals(expected, result, "Failed to support unfinished entities (i.e. missing semicolon)");
-
         // ignore it
         neu = new NumericEntityUnescaper();
         input = "Test &#x30 not test";
         expected = input;
-
         result = neu.translate(input);
         assertEquals(expected, result, "Failed to ignore unfinished entities (i.e. missing semicolon)");
-
         // fail it
-        final NumericEntityUnescaper failingNeu =
-                new NumericEntityUnescaper(NumericEntityUnescaper.OPTION.errorIfNoSemiColon);
+        final NumericEntityUnescaper failingNeu = new NumericEntityUnescaper(NumericEntityUnescaper.OPTION.errorIfNoSemiColon);
         final String failingInput = "Test &#x30 not test";
         assertIllegalArgumentException(() -> failingNeu.translate(failingInput));
     }
