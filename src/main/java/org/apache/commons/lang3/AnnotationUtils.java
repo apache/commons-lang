b611203cdba63e48a03a18cbfa0d780795f2a69a
NusaibaMahnejPapry <bsse1617@iit.du.ac.bd>
2026-06-23 03:26:26 +0600

    refactor: improve code structure and readability of AnnotationUtils.java Class

--- src/main/java/org/apache/commons/lang3/AnnotationUtils.java
+++ src/main/java/org/apache/commons/lang3/AnnotationUtils.java
@@ -81,21 +81,50 @@ public class AnnotationUtils {
          */
         @Override
         protected String getShortClassName(final Class<?> cls) {
-            // formatter:off
-            return ClassUtils.getAllInterfaces(cls).stream().filter(Annotation.class::isAssignableFrom).findFirst()
-                .map(iface -> "@" + iface.getName())
-                .orElse(StringUtils.EMPTY);
-            // formatter:on
+            return ClassUtils.getAllInterfaces(cls).stream()
+                    .filter(Annotation.class::isAssignableFrom)
+                    .findFirst()
+                    .map(iface -> "@" + iface.getName())
+                    .orElse(StringUtils.EMPTY);
         }
 
     };
 
+    // ---------------------- Helper dispatch methods ----------------------
+
+    private static boolean isPrimitiveArrayType(Class<?> type) {
+        return type.equals(Byte.TYPE) || type.equals(Short.TYPE)
+                || type.equals(Integer.TYPE) || type.equals(Character.TYPE)
+                || type.equals(Long.TYPE) || type.equals(Float.TYPE)
+                || type.equals(Double.TYPE) || type.equals(Boolean.TYPE);
+    }
+
+    private static boolean primitiveArrayEquals(Class<?> type, Object o1, Object o2) {
+        if (type.equals(Byte.TYPE)) return Arrays.equals((byte[]) o1, (byte[]) o2);
+        if (type.equals(Short.TYPE)) return Arrays.equals((short[]) o1, (short[]) o2);
+        if (type.equals(Integer.TYPE)) return Arrays.equals((int[]) o1, (int[]) o2);
+        if (type.equals(Character.TYPE)) return Arrays.equals((char[]) o1, (char[]) o2);
+        if (type.equals(Long.TYPE)) return Arrays.equals((long[]) o1, (long[]) o2);
+        if (type.equals(Float.TYPE)) return Arrays.equals((float[]) o1, (float[]) o2);
+        if (type.equals(Double.TYPE)) return Arrays.equals((double[]) o1, (double[]) o2);
+        if (type.equals(Boolean.TYPE)) return Arrays.equals((boolean[]) o1, (boolean[]) o2);
+        return false;
+    }
+
+    private static int primitiveArrayHash(Class<?> type, Object o) {
+        if (type.equals(Byte.TYPE)) return Arrays.hashCode((byte[]) o);
+        if (type.equals(Short.TYPE)) return Arrays.hashCode((short[]) o);
+        if (type.equals(Integer.TYPE)) return Arrays.hashCode((int[]) o);
+        if (type.equals(Character.TYPE)) return Arrays.hashCode((char[]) o);
+        if (type.equals(Long.TYPE)) return Arrays.hashCode((long[]) o);
+        if (type.equals(Float.TYPE)) return Arrays.hashCode((float[]) o);
+        if (type.equals(Double.TYPE)) return Arrays.hashCode((double[]) o);
+        if (type.equals(Boolean.TYPE)) return Arrays.hashCode((boolean[]) o);
+        return 0;
+    }
+
     /**
      * Helper method for comparing two arrays of annotations.
-     *
-     * @param a1 the first array
-     * @param a2 the second array
-     * @return a flag whether these arrays are equal
      */
     private static boolean annotationArrayMemberEquals(final Annotation[] a1, final Annotation[] a2) {
         if (a1.length != a2.length) {
@@ -111,74 +140,23 @@ public class AnnotationUtils {
 
     /**
      * Helper method for comparing two objects of an array type.
-     *
-     * @param componentType the component type of the array
-     * @param o1 the first object
-     * @param o2 the second object
-     * @return a flag whether these objects are equal
      */
     private static boolean arrayMemberEquals(final Class<?> componentType, final Object o1, final Object o2) {
         if (componentType.isAnnotation()) {
             return annotationArrayMemberEquals((Annotation[]) o1, (Annotation[]) o2);
         }
-        if (componentType.equals(Byte.TYPE)) {
-            return Arrays.equals((byte[]) o1, (byte[]) o2);
-        }
-        if (componentType.equals(Short.TYPE)) {
-            return Arrays.equals((short[]) o1, (short[]) o2);
-        }
-        if (componentType.equals(Integer.TYPE)) {
-            return Arrays.equals((int[]) o1, (int[]) o2);
-        }
-        if (componentType.equals(Character.TYPE)) {
-            return Arrays.equals((char[]) o1, (char[]) o2);
-        }
-        if (componentType.equals(Long.TYPE)) {
-            return Arrays.equals((long[]) o1, (long[]) o2);
-        }
-        if (componentType.equals(Float.TYPE)) {
-            return Arrays.equals((float[]) o1, (float[]) o2);
-        }
-        if (componentType.equals(Double.TYPE)) {
-            return Arrays.equals((double[]) o1, (double[]) o2);
-        }
-        if (componentType.equals(Boolean.TYPE)) {
-            return Arrays.equals((boolean[]) o1, (boolean[]) o2);
+        if (isPrimitiveArrayType(componentType)) {
+            return primitiveArrayEquals(componentType, o1, o2);
         }
         return Arrays.equals((Object[]) o1, (Object[]) o2);
     }
 
     /**
      * Helper method for generating a hash code for an array.
-     *
-     * @param componentType the component type of the array
-     * @param o the array
-     * @return a hash code for the specified array
      */
     private static int arrayMemberHash(final Class<?> componentType, final Object o) {
-        if (componentType.equals(Byte.TYPE)) {
-            return Arrays.hashCode((byte[]) o);
-        }
-        if (componentType.equals(Short.TYPE)) {
-            return Arrays.hashCode((short[]) o);
-        }
-        if (componentType.equals(Integer.TYPE)) {
-            return Arrays.hashCode((int[]) o);
-        }
-        if (componentType.equals(Character.TYPE)) {
-            return Arrays.hashCode((char[]) o);
-        }
-        if (componentType.equals(Long.TYPE)) {
-            return Arrays.hashCode((long[]) o);
-        }
-        if (componentType.equals(Float.TYPE)) {
-            return Arrays.hashCode((float[]) o);
-        }
-        if (componentType.equals(Double.TYPE)) {
-            return Arrays.hashCode((double[]) o);
-        }
-        if (componentType.equals(Boolean.TYPE)) {
-            return Arrays.hashCode((boolean[]) o);
+        if (isPrimitiveArrayType(componentType)) {
+            return primitiveArrayHash(componentType, o);
         }
         return Arrays.hashCode((Object[]) o);
     }
@@ -186,13 +164,6 @@ public class AnnotationUtils {
     /**
      * Checks if two annotations are equal using the criteria for equality
      * presented in the {@link Annotation#equals(Object)} API docs.
-     *
-     * @param a1 the first Annotation to compare, {@code null} returns
-     * {@code false} unless both are {@code null}
-     * @param a2 the second Annotation to compare, {@code null} returns
-     * {@code false} unless both are {@code null}
-     * @return {@code true} if the two annotations are {@code equal} or both
-     * {@code null}
      */
     public static boolean equals(final Annotation a1, final Annotation a2) {
         if (a1 == a2) {
@@ -201,19 +172,25 @@ public class AnnotationUtils {
         if (a1 == null || a2 == null) {
             return false;
         }
+
         final Class<? extends Annotation> type1 = a1.annotationType();
         final Class<? extends Annotation> type2 = a2.annotationType();
+
         Validate.notNull(type1, "Annotation %s with null annotationType()", a1);
         Validate.notNull(type2, "Annotation %s with null annotationType()", a2);
+
         if (!type1.equals(type2)) {
             return false;
         }
+
         try {
             for (final Method m : type1.getDeclaredMethods()) {
                 if (m.getParameterTypes().length == 0
                         && isValidAnnotationMemberType(m.getReturnType())) {
+
                     final Object v1 = m.invoke(a1);
                     final Object v2 = m.invoke(a2);
+
                     if (!memberEquals(m.getReturnType(), v1, v2)) {
                         return false;
                     }
@@ -228,23 +205,16 @@ public class AnnotationUtils {
     /**
      * Generate a hash code for the given annotation using the algorithm
      * presented in the {@link Annotation#hashCode()} API docs.
-     *
-     * @param a the Annotation for a hash code calculation is desired, not
-     * {@code null}
-     * @return the calculated hash code
-     * @throws RuntimeException if an {@link Exception} is encountered during
-     * annotation member access
-     * @throws IllegalStateException if an annotation method invocation returns
-     * {@code null}
      */
     public static int hashCode(final Annotation a) {
         int result = 0;
         final Class<? extends Annotation> type = a.annotationType();
+
         for (final Method m : type.getDeclaredMethods()) {
             try {
                 final Object value = m.invoke(a);
                 if (value == null) {
-                    throw new IllegalStateException(String.format("Annotation method %s returned null", m));
+                    throw new IllegalStateException("Annotation method returned null: " + m);
                 }
                 result += hashMember(m.getName(), value);
             } catch (final ReflectiveOperationException ex) {
@@ -254,16 +224,9 @@ public class AnnotationUtils {
         return result;
     }
 
-    //besides modularity, this has the advantage of autoboxing primitives:
-    /**
-     * Helper method for generating a hash code for a member of an annotation.
-     *
-     * @param name the name of the member
-     * @param value the value of the member
-     * @return a hash code for this member
-     */
     private static int hashMember(final String name, final Object value) {
         final int part1 = name.hashCode() * 127;
+
         if (ObjectUtils.isArray(value)) {
             return part1 ^ arrayMemberHash(value.getClass().getComponentType(), value);
         }
@@ -275,14 +238,6 @@ public class AnnotationUtils {
 
     /**
      * Checks if the specified type is permitted as an annotation member.
-     *
-     * <p>The Java language specification only permits certain types to be used
-     * in annotations. These include {@link String}, {@link Class}, primitive
-     * types, {@link Annotation}, {@link Enum}, and single-dimensional arrays of
-     * these types.</p>
-     *
-     * @param type the type to check, {@code null}
-     * @return {@code true} if the type is a valid type to use in an annotation
      */
     public static boolean isValidAnnotationMemberType(Class<?> type) {
         if (type == null) {
@@ -291,19 +246,16 @@ public class AnnotationUtils {
         if (type.isArray()) {
             type = type.getComponentType();
         }
-        return type.isPrimitive() || type.isEnum() || type.isAnnotation()
-                || String.class.equals(type) || Class.class.equals(type);
+        return type.isPrimitive()
+                || type.isEnum()
+                || type.isAnnotation()
+                || String.class.equals(type)
+                || Class.class.equals(type);
     }
 
     /**
      * Helper method for checking whether two objects of the given type are
-     * equal. This method is used to compare the parameters of two annotation
-     * instances.
-     *
-     * @param type the type of the objects to be compared
-     * @param o1 the first object
-     * @param o2 the second object
-     * @return a flag whether these objects are equal
+     * equal.
      */
     private static boolean memberEquals(final Class<?> type, final Object o1, final Object o2) {
         if (o1 == o2) {
@@ -324,16 +276,13 @@ public class AnnotationUtils {
     /**
      * Generate a string representation of an Annotation, as suggested by
      * {@link Annotation#toString()}.
-     *
-     * @param a the annotation of which a string representation is desired
-     * @return the standard string representation of an annotation, not
-     * {@code null}
      */
     public static String toString(final Annotation a) {
         final ToStringBuilder builder = new ToStringBuilder(a, TO_STRING_STYLE);
+
         for (final Method m : a.annotationType().getDeclaredMethods()) {
             if (m.getParameterTypes().length > 0) {
-                continue; // what?
+                continue;
             }
             try {
                 builder.append(m.getName(), m.invoke(a));
@@ -346,12 +295,7 @@ public class AnnotationUtils {
 
     /**
      * {@link AnnotationUtils} instances should NOT be constructed in
-     * standard programming. Instead, the class should be used statically.
-     *
-     * <p>This constructor is public to permit tools that require a JavaBean
-     * instance to operate.</p>
-     *
-     * @deprecated TODO Make private in 4.0.
+     * standard programming.
      */
     @Deprecated
     public AnnotationUtils() {
