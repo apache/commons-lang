0ebe074afd9b8fbdc40479df5022aec230286efd
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:56:13 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/tuple/Triple.java
+++ src/main/java/org/apache/commons/lang3/tuple/Triple.java
@@ -90,7 +90,7 @@ public abstract class Triple<L, M, R> implements Comparable<Triple<L, M, R>>, Se
      * @param middle  The middle element, may not be null.
      * @param right  The right element, may not be null.
      * @return An immutable triple formed from the three parameters, not null.
-     * @throws NullPointerException if any input is null.
+     * @throws NullPointerException Thrown if any input is null.
      * @since 3.13.0
      */
     public static <L, M, R> Triple<L, M, R> ofNonNull(final L left, final M middle, final R right) {
