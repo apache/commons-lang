85cce504543de92ac93e6905af810a791553d33d
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:55:59 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/function/BooleanConsumer.java
+++ src/main/java/org/apache/commons/lang3/function/BooleanConsumer.java
@@ -56,7 +56,7 @@ public interface BooleanConsumer {
      * @param after The operation to perform after this operation
      * @return A composed {@link BooleanConsumer} that performs in sequence this operation followed by the {@code after}
      *         operation
-     * @throws NullPointerException if {@code after} is null
+     * @throws NullPointerException Thrown if {@code after} is null.
      */
     default BooleanConsumer andThen(final BooleanConsumer after) {
         Objects.requireNonNull(after, "after");
