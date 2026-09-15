eecfaa8978472de611ce539dbb7bfc6ca2e1909c
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:56:01 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/function/FailableDoubleBinaryOperator.java
+++ src/main/java/org/apache/commons/lang3/function/FailableDoubleBinaryOperator.java
@@ -34,7 +34,7 @@ public interface FailableDoubleBinaryOperator<E extends Throwable> {
      * @param left The first operand
      * @param right The second operand
      * @return The operator result
-     * @throws E if the operation fails
+     * @throws E Thrown if the operation fails.
      */
     double applyAsDouble(double left, double right) throws E;
 }
