cf4b52fe9c945bea5dfbfb4b8882aa4caadbac58
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:56:10 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/text/translate/OctalUnescaper.java
+++ src/main/java/org/apache/commons/lang3/text/translate/OctalUnescaper.java
@@ -45,7 +45,7 @@ public class OctalUnescaper extends CharSequenceTranslator {
     }
 
     /**
-     * Checks if the given char is the character representation of one of the digit from 0 to 3.
+     * Tests whether the given char is the character representation of one of the digits from 0 to 3.
      *
      * @param ch The char to check.
      * @return true if the given char is the character representation of one of the digits from 0 to 3.
