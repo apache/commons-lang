3aa42b7697c59978811e70f60c5ef66841ed1a08
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:56:04 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/function/FailableShortSupplier.java
+++ src/main/java/org/apache/commons/lang3/function/FailableShortSupplier.java
@@ -29,10 +29,10 @@ import java.util.function.IntSupplier;
 public interface FailableShortSupplier<E extends Throwable> {
 
     /**
-     * Supplies an int.
+     * Gets an int.
      *
      * @return A result
-     * @throws E if the supplier fails
+     * @throws E Thrown if the supplier fails.
      */
     short getAsShort() throws E;
 }
