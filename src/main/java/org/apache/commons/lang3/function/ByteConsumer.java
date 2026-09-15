5f8ac5f28dc02e0b8a17ade9730a7ee889395b62
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:55:59 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/function/ByteConsumer.java
+++ src/main/java/org/apache/commons/lang3/function/ByteConsumer.java
@@ -56,7 +56,7 @@ public interface ByteConsumer {
      *
      * @param after The operation to perform after this operation
      * @return A composed {@link ByteConsumer} that performs in sequence this operation followed by the {@code after} operation
-     * @throws NullPointerException if {@code after} is null
+     * @throws NullPointerException Thrown if {@code after} is null.
      */
     default ByteConsumer andThen(final ByteConsumer after) {
         Objects.requireNonNull(after, "after");
