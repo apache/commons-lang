b60696637b3848cb776581f5d2a87bd7cea18f0d
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:56:16 -0700

    Javadoc, comments, and site documentation

--- src/test/java/org/apache/commons/lang3/concurrent/AtomicSafeInitializerTest.java
+++ src/test/java/org/apache/commons/lang3/concurrent/AtomicSafeInitializerTest.java
@@ -122,8 +122,8 @@ class AtomicSafeInitializerTest extends AbstractConcurrentInitializerTest<Object
     /**
      * Tests that initialize() is called only once.
      *
-     * @throws org.apache.commons.lang3.concurrent.ConcurrentException because {@link #testGetConcurrent()} may throw it.
-     * @throws InterruptedException                                    because {@link #testGetConcurrent()} may throw it.
+     * @throws org.apache.commons.lang3.concurrent.ConcurrentException Thrown if {@link #testGetConcurrent()} throws an exception.
+     * @throws InterruptedException Thrown if {@link #testGetConcurrent()} throws an exception.
      */
     @Test
     void testNumberOfInitializeInvocations() throws ConcurrentException, InterruptedException {
