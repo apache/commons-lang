15893fd84e8afa04465c2f371f0522ece6b54d94
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:56:08 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/stream/Streams.java
+++ src/main/java/org/apache/commons/lang3/stream/Streams.java
@@ -228,7 +228,7 @@ public class Streams {
         /**
          * Throws IllegalStateException if this stream is already terminated.
          *
-         * @throws IllegalStateException if this stream is already terminated.
+         * @throws IllegalStateException Thrown if this stream is already terminated.
          */
         protected void assertNotTerminated() {
             if (terminated) {
@@ -388,7 +388,7 @@ public class Streams {
         /**
          * Marks this stream as terminated.
          *
-         * @throws IllegalStateException if this stream is already terminated.
+         * @throws IllegalStateException Thrown if this stream is already terminated.
          */
         protected void makeTerminated() {
             assertNotTerminated();
