c38cc2a5979f8960379510bbf23439f5758b4e1d
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:56:03 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/function/FailableLongBinaryOperator.java
+++ src/main/java/org/apache/commons/lang3/function/FailableLongBinaryOperator.java
@@ -34,7 +34,7 @@ public interface FailableLongBinaryOperator<E extends Throwable> {
      * @param left The first operand
      * @param right The second operand
      * @return The operator result
-     * @throws E if the operation fails
+     * @throws E Thrown if the operation fails.
      */
     long applyAsLong(long left, long right) throws E;
 }
