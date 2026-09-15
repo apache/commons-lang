019a138b1c4f636cd796f7ce3a3b7e8a661155a3
Gary Gregory <garydgregory@gmail.com>
2026-09-15 09:56:00 -0700

    Javadoc, comments, and site documentation

--- src/main/java/org/apache/commons/lang3/function/FailableBiFunction.java
+++ src/main/java/org/apache/commons/lang3/function/FailableBiFunction.java
@@ -57,7 +57,7 @@ public interface FailableBiFunction<T, U, R, E extends Throwable> {
      * @param <V> The output type of the {@code after} function, and of the composed function.
      * @param after The operation to perform after this one.
      * @return A composed {@link FailableBiFunction} that like {@link BiFunction#andThen(Function)}.
-     * @throws NullPointerException when {@code after} is null.
+     * @throws NullPointerException Thrown when {@code after} is null.
      */
     default <V> FailableBiFunction<T, U, V, E> andThen(final FailableFunction<? super R, ? extends V, E> after) {
         Objects.requireNonNull(after, "after");
